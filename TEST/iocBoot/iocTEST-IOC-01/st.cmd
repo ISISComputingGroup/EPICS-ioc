f5fb2bc2ef953047eb9162f0439b3731d76baa1d
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2014-03-03 13:13:35 +0000

    Add test IOC
    Former-commit-id: 719ca9795636a4aab864e9b5335aa341e244c5fe

---
+++ TEST/iocBoot/iocTEST-IOC-01/st.cmd
@@ -0,0 +1,35 @@
+#!../../bin/windows-x64-debug/TEST-IOC-01
+
+## You may have to change TEST-IOC-01 to something else
+## everywhere it appears in this file
+
+< envPaths
+
+cd ${TOP}
+
+## Register all support components
+dbLoadDatabase "dbd/TEST-IOC-01.dbd"
+TEST_IOC_01_registerRecordDeviceDriver pdbbase
+
+##ISIS## Run IOC initialisation 
+< $(IOCSTARTUP)/init.cmd
+
+## Load record instances
+
+##ISIS## Load common DB records 
+< $(IOCSTARTUP)/dbload.cmd
+
+## Load our record instances
+dbLoadRecords("db/TEST-IOC-01.db","P=$(MYPVPREFIX)$(IOCNAME):, RANDDEV=Random Async, RANDSEED=324235")
+
+##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
+< $(IOCSTARTUP)/preiocinit.cmd
+
+cd ${TOP}/iocBoot/${IOC}
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=faa59Host"
+
+##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
+< $(IOCSTARTUP)/postiocinit.cmd
