7e13429a0bdf65006d9c8774638ab3b8a1cdad28
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2014-03-07 18:29:09 +0000

    Add new location for PSCTRL
    Former-commit-id: 79eb724b631db697d50984904bf1b28b39cf1ac8

---
+++ PSCTRL/iocBoot/iocPSCTRL-IOC-01/st.cmd
@@ -0,0 +1,38 @@
+#!../../bin/windows-x64-debug/PSCTRL
+
+## You may have to change PSCTRL to something else
+## everywhere it appears in this file
+
+< envPaths
+
+cd ${TOP}
+
+## Register all support components
+dbLoadDatabase "dbd/PSCTRL.dbd"
+PSCTRL_registerRecordDeviceDriver pdbbase
+
+##ISIS## Run IOC initialisation 
+< $(IOCSTARTUP)/init.cmd
+
+## Load record instances
+
+##ISIS## Load common DB records 
+< $(IOCSTARTUP)/dbload.cmd
+
+## Load our record instances
+#dbLoadRecords("db/xxx.db","user=faa59Host")
+< $(IOCSTARTUP)/psc1.cmd
+
+##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
+< $(IOCSTARTUP)/preiocinit.cmd
+
+cd ${TOP}/iocBoot/${IOC}
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=faa59Host"
+
+##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
+< $(IOCSTARTUP)/postiocinit.cmd
+
+< $(IOCSTARTUP)/psc2.cmd
