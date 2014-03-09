d89757568f5360b378e7ec601f1ff2bf77316368
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2014-03-09 23:34:43 +0000

    Copy DLLs over
    Former-commit-id: ea6db94ee0f2535ea02700cb0102eb039025b9db

---
+++ ISISDAE/postbuild/update_dlls.bat
@@ -0,0 +1,19 @@
+@echo off
+set MYDIR=%~dp0
+REM set ICPDIR=c:\development\instrumentcontrol\icp\labview modules\dae\service\x64
+set ICPDIR=\\olympic\babylon5\public\freddie\isisdae\x64
+echo Checking %ICPDIR% for updated DAE DLLs 
+xcopy /i /q /d /y "%ICPDIR%\debug\*.dll" "%MYDIR%..\bin\windows-x64-debug"
+xcopy /i /q /d /y "%ICPDIR%\debug\*.manifest" "%MYDIR%..\bin\windows-x64-debug"
+xcopy /i /q /d /y "%ICPDIR%\debug\*.pdb" "%MYDIR%..\bin\windows-x64-debug"
+xcopy /i /q /d /y "%ICPDIR%\release\*.dll" "%MYDIR%..\bin\windows-x64"
+xcopy /i /q /d /y "%ICPDIR%\release\*.manifest" "%MYDIR%..\bin\windows-x64"
+xcopy /i /q /d /y "%ICPDIR%\release\*.pdb" "%MYDIR%..\bin\windows-x64"
+
+xcopy /i /q /d /y "%ICPDIR%\crt_manifest_for_dae3\*.*" "%MYDIR%..\bin\windows-x64-debug"
+
+REM need visa64.dll, visaConfMgr.dll and visaUtilities.dll  
+if not exist "c:\windows\system32\visa64.dll" (
+    xcopy /i /q /d /y "%ICPDIR%\visa\*.dll" "%MYDIR%..\bin\windows-x64-debug"
+    xcopy /i /q /d /y "%ICPDIR%\visa\*.dll" "%MYDIR%..\bin\windows-x64"
+)
