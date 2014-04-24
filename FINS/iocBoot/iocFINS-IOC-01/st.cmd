004b89345fe70e0aa0885f20f983c515c384ecb2
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2014-04-24 16:27:36 +0000

    Larmor PLC files with conversion records
    Former-commit-id: a549a48885504042929567aea31d3509fbb83be3

--- FINS/iocBoot/iocFINS-IOC-01/st.cmd
+++ FINS/iocBoot/iocFINS-IOC-01/st.cmd
@@ -14,8 +14,25 @@ FINS_IOC_01_registerRecordDeviceDriver pdbbase
 ##ISIS## Run IOC initialisation 
 < $(IOCSTARTUP)/init.cmd
 
-#finsUDPInit("LN2", "172.23.231.192")
-finsUDPInit("LN2", "130.246.55.167", "TCP")
+# this is larmor
+#finsUDPInit("PLC", "130.246.36.139", "TCP")
+# this is R78
+finsUDPInit("PLC", "130.246.55.167", "TCP")
+
+# ASYN_TRACE_ERROR     0x0001
+# ASYN_TRACEIO_DEVICE  0x0002
+# ASYN_TRACEIO_FILTER  0x0004
+# ASYN_TRACEIO_DRIVER  0x0008
+# ASYN_TRACE_FLOW      0x0010
+
+# ASYN_TRACEIO_NODATA  0x0000
+# ASYN_TRACEIO_ASCII   0x0001
+# ASYN_TRACEIO_ESCAPE  0x0002
+# ASYN_TRACEIO_HEX     0x0004
+
+# need to set trace mask per address
+#asynSetTraceMask  ("PLC", 1, 0x01f)
+#asynSetTraceIOMask("PLC", 1, 0x01)
 
 ## Load record instances
 
@@ -23,7 +40,7 @@ finsUDPInit("LN2", "130.246.55.167", "TCP")
 < $(IOCSTARTUP)/dbload.cmd
 
 ## Load our record instances
-dbLoadRecords("db/larmor-air.db","P=$(MYPVPREFIX)")
+dbLoadRecords("db/larmor-air.db","P=$(MYPVPREFIX),Q=BENCH:")
 
 ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
 < $(IOCSTARTUP)/preiocinit.cmd
