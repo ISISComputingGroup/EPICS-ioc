a70ee2ef5cf491fe0ce32085517711845d3dd2fa
Gareth Howells <gareth.howells@stfc.ac.uk>
2014-07-07 13:14:40 +0000

    Updated motor configuration for LARMOR bench
    Former-commit-id: c28282ab091f45b526ca62efd00725a6ab5bd114

--- GALIL/iocBoot/iocGALIL-IOC-01/galil4.cmd
+++ GALIL/iocBoot/iocGALIL-IOC-01/galil4.cmd
@@ -10,7 +10,7 @@ epicsEnvSet("GALILCRATE","04")
 
 G21X3Config($(GCID),$(GALILADDR$(GALILCRATE)),8,2100,2000) 
 
-G21X3NameConfig($(GCID),"A",2,0,2,-1,0,1,"",0,0,"",1,0)
+G21X3NameConfig($(GCID),"A",2,0,2,-1,0,0,"",0,0,"",1,0)
 G21X3NameConfig($(GCID),"B",3,0,-1,-1,0,1,"",0,0,"",1,0)
 G21X3NameConfig($(GCID),"C",3,0,-1,-1,0,1,"",0,0,"",1,0)
 G21X3NameConfig($(GCID),"D",3,0,-1,-1,0,1,"",0,0,"",1,0)
@@ -19,4 +19,5 @@ G21X3NameConfig($(GCID),"F",0,0,0,0,0,1,"",0,0,"",1,0)
 G21X3NameConfig($(GCID),"G",0,0,0,0,0,1,"",0,0,"",1,0)
 G21X3NameConfig($(GCID),"H",0,0,0,0,0,1,"",0,0,"",1,0)
 
-G21X3StartCard($(GCID),"",0,0)
+#G21X3StartCard($(GCID),"",0,0)
+G21X3StartCard($(GCID),"$(GALIL)/db/galil_Default_Header.gmc;$(GALIL)/db/galil_Home_Home.gmc!$(GALIL)/db/galil_Home_Home.gmc!$(GALIL)/db/galil_Home_Home.gmc!$(GALIL)/db/galil_Home_Home.gmc!$(GALIL)/db/galil_Home_Home.gmc!$(GALIL)/db/galil_Home_RevLimit.gmc!$(GALIL)/db/galil_Home_RevLimit.gmc!$(GALIL)/db/galil_Home_Home.gmc;$(GALIL)/db/galil_Default_Footer.gmc",0,0)
