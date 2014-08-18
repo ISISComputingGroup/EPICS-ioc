58f45acd00d302c6629e5b10f64ee3818bd971ac
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2014-08-18 13:34:03 +0000

    Fix truncated messages
    Former-commit-id: 1b07c9a37d1266742b5b29701a79b5bb27ffe5e1

--- ISISDAE/iocBoot/iocISISDAE-IOC-01/st.cmd
+++ ISISDAE/iocBoot/iocISISDAE-IOC-01/st.cmd
@@ -3,6 +3,9 @@
 ## You may have to change ISISDAE-IOC-01 to something else
 ## everywhere it appears in this file
 
+# Increase this if you get <<TRUNCATED>> or discarded messages warnings in your errlog output
+errlogInit2(65536, 4096)
+
 < envPaths
 
 cd ${TOP}
