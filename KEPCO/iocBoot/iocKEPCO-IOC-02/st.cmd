b7f728d4671b023dc2ea6658db2ea4d399718511
Dominic Oram <dominic.oram@stfc.ac.uk>
2014-10-10 10:42:48 +0000

    Moved KEPCO IOC
    Former-commit-id: 0f75a011d319f0e948018899f589494f51f71108

---
+++ KEPCO/iocBoot/iocKEPCO-IOC-02/st.cmd
@@ -0,0 +1,36 @@
+#!../../bin/windows-x64/kepco
+
+## You may have to change kepco to something else
+## everywhere it appears in this file
+
+< envPaths
+
+epicsEnvSet "STREAM_PROTOCOL_PATH" "$(SUPPORT)/kepco/kepcoApp/protocol"
+epicsEnvSet "TTY" "$(TTY=\\\\\\\\.\\\\COM19)"
+
+cd ${TOP}
+
+## Register all support components
+dbLoadDatabase "dbd/KEPCO-IOC-02.dbd"
+KEPCO_IOC_02_registerRecordDeviceDriver pdbbase
+
+< $(IOCSTARTUP)/init.cmd
+
+drvAsynSerialPortConfigure("L0", "$(TTY)", 0, 0, 0, 0)
+asynSetOption("L0", -1, "baud", "9600")
+asynSetOption("L0", -1, "bits", "8")
+asynSetOption("L0", -1, "parity", "none")
+asynSetOption("L0", -1, "stop", "1")
+
+< $(IOCSTARTUP)/dbload.cmd
+
+## Load record instances
+dbLoadRecords("$(TOP)/../../db/kepco.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0, RESET=NO")
+
+< $(IOCSTARTUP)/preiocinit.cmd
+
+cd ${TOP}/iocBoot/${IOC}
+iocInit
+
+< $(IOCSTARTUP)/postiocinit.cmd
+
