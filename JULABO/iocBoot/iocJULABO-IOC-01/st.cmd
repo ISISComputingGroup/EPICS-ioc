64d09015be921ff044d1c0c4a5e335cf2882451f
Matt Clarke <matt.clarke@stfc.ac.uk>
2014-10-10 06:49:57 +0000

    Refs #508. Added julabo to IOC directory
    Former-commit-id: 2803a8b419b144cf9d41ce19ded593aebe59f54f

---
+++ JULABO/iocBoot/iocJULABO-IOC-01/st.cmd
@@ -0,0 +1,47 @@
+#!../../bin/windows-x64-debug/JULABO-IOC-01
+
+## You may have to change JULABO-IOC-01 to something else
+## everywhere it appears in this file
+
+# Increase this if you get <<TRUNCATED>> or discarded messages warnings in your errlog output
+errlogInit2(65536, 256)
+
+< envPaths
+
+cd ${TOP}
+
+epicsEnvSet "STREAM_PROTOCOL_PATH" "$(SUPPORT)/julabo/julaboApp/protocol"
+epicsEnvSet "TTY" "$(TTY=\\\\\\\\.\\\\COM18)"
+
+## Register all support components
+dbLoadDatabase "dbd/JULABO-IOC-01.dbd"
+JULABO_IOC_01_registerRecordDeviceDriver pdbbase
+
+##ISIS## Run IOC initialisation 
+< $(IOCSTARTUP)/init.cmd
+
+drvAsynSerialPortConfigure("L0", "$(TTY)", 0, 0, 0, 0)
+asynSetOption("L0", -1, "baud", "4800")
+asynSetOption("L0", -1, "bits", "7")
+asynSetOption("L0", -1, "parity", "even")
+asynSetOption("L0", -1, "stop", "1")
+
+## Load record instances
+
+##ISIS## Load common DB records 
+< $(IOCSTARTUP)/dbload.cmd
+
+## Load our record instances
+dbLoadRecords("db/FL300.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0")
+
+##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
+< $(IOCSTARTUP)/preiocinit.cmd
+
+cd ${TOP}/iocBoot/${IOC}
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=mjc23Host"
+
+##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
+< $(IOCSTARTUP)/postiocinit.cmd
