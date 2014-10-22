e79581aebdfe2b130f2a5dddf28cd4ba59b73fea
Matt Clarke <matt.clarke@stfc.ac.uk>
2014-10-22 12:52:59 +0000

    Refs #250. Started moving Tektronix_MSO_4104B to ioc directory.
    Former-commit-id: a3d1295c1dd49d31df26b25cc2458f5e7b0e55c1

---
+++ TEKMSO4104B/iocBoot/iocTEKMSO4104B-IOC-02/st.cmd
@@ -0,0 +1,43 @@
+#!../../bin/windows-x64-debug/TEKMSO4104B-IOC-02
+
+## You may have to change TEKMSO4104B-IOC-02 to something else
+## everywhere it appears in this file
+
+# Increase this if you get <<TRUNCATED>> or discarded messages warnings in your errlog output
+errlogInit2(65536, 256)
+
+< envPaths
+
+epicsEnvSet "STREAM_PROTOCOL_PATH" "$(SUPPORT)/Tektronix_MSO_4104B/1-0/Tektronix_MSO_4104BSup"
+epicsEnvSet "EPICS_CA_MAX_ARRAY_BYTES" "100000"
+
+cd ${TOP}
+
+## Register all support components
+dbLoadDatabase "dbd/TEKMSO4104B-IOC-02.dbd"
+TEKMSO4104B_IOC_02_registerRecordDeviceDriver pdbbase
+
+##ISIS## Run IOC initialisation 
+< $(IOCSTARTUP)/init.cmd
+
+vxi11Configure("IP", "130.246.49.179", 0, 0.0,"inst0", 0, 0)
+
+## Load record instances
+
+##ISIS## Load common DB records 
+< $(IOCSTARTUP)/dbload.cmd
+
+## Load our record instances
+dbLoadRecords("db/devTektronix_MSO_4104B.db","P=$(MYPVPREFIX)$(IOCNAME), PORT=IP")
+
+##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
+< $(IOCSTARTUP)/preiocinit.cmd
+
+cd ${TOP}/iocBoot/${IOC}
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=mjc23Host"
+
+##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
+< $(IOCSTARTUP)/postiocinit.cmd
