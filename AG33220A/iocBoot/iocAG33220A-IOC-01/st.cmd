83c0eaab3653a649a6363f8064e0a15d6998820d
Matt Clarke <matt.clarke@stfc.ac.uk>
2014-10-23 13:11:16 +0000

    Refs #250. ioc for AG33220A
    Former-commit-id: 406cc57e4c77bf78489df1ba2ce4b1fda22be3be

---
+++ AG33220A/iocBoot/iocAG33220A-IOC-01/st.cmd
@@ -0,0 +1,42 @@
+#!../../bin/windows-x64-debug/AG33220A-IOC-01
+
+## You may have to change AG33220A-IOC-01 to something else
+## everywhere it appears in this file
+
+# Increase this if you get <<TRUNCATED>> or discarded messages warnings in your errlog output
+errlogInit2(65536, 256)
+
+< envPaths
+
+epicsEnvSet "STREAM_PROTOCOL_PATH" "$(SUPPORT)/agilent33220A/1-4-1/agilent33220AApp/protocol"
+
+cd ${TOP}
+
+## Register all support components
+dbLoadDatabase "dbd/AG33220A-IOC-01.dbd"
+AG33220A_IOC_01_registerRecordDeviceDriver pdbbase
+
+##ISIS## Run IOC initialisation 
+< $(IOCSTARTUP)/init.cmd
+
+drvAsynIPPortConfigure ("IP", "130.246.49.196:5025")
+
+## Load record instances
+
+##ISIS## Load common DB records 
+< $(IOCSTARTUP)/dbload.cmd
+
+## Load our record instances
+dbLoadRecords("db/agilent33220A.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=IP")
+
+##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
+< $(IOCSTARTUP)/preiocinit.cmd
+
+cd ${TOP}/iocBoot/${IOC}
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=mjc23Host"
+
+##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
+< $(IOCSTARTUP)/postiocinit.cmd
