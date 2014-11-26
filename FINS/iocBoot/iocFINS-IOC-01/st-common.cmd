7daeac0e7afe435d21ad34282d83abb284a7cb76
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2014-11-26 14:04:04 +0000

    Load config file based on IOC name
    Former-commit-id: 63a81d0af1d4e2605cc071c8855ce7aa98f2c41c

--- FINS/iocBoot/iocFINS-IOC-01/st-common.cmd
+++ FINS/iocBoot/iocFINS-IOC-01/st-common.cmd
@@ -4,15 +4,10 @@
 ##ISIS## Load common DB records 
 < $(IOCSTARTUP)/dbload.cmd
 
-# this defines macros we can use for conditional loading later
-stringtest("IFPLC01", "$(FINS_01_PLCIP=)")
-stringtest("IFPLC02", "$(FINS_02_PLCIP=)")
-
 epicsEnvSet("FINSCONFIG","$(ICPCONFIGROOT)/fins")
 
-#configure PLCs
-$(IFPLC01) < $(FINSCONFIG)/fins1.cmd
-$(IFPLC02) < $(FINSCONFIG)/fins2.cmd
+# configure PLCs - will look for FINS_01.cmd etc based on IOC name
+< $(FINSCONFIG)/$(IOCNAME).cmd
 
 ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
 < $(IOCSTARTUP)/preiocinit.cmd
