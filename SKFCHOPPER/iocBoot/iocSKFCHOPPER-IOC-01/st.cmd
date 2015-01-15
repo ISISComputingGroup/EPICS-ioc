4a552b5fc8f7ab8cb47b3dea10de67541893f3d7
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2015-01-15 12:30:02 +0000

    Add second chopper
    Former-commit-id: b1abcc1d66c31cc2e6328b5aef4d50db997beccb

--- SKFCHOPPER/iocBoot/iocSKFCHOPPER-IOC-01/st.cmd
+++ SKFCHOPPER/iocBoot/iocSKFCHOPPER-IOC-01/st.cmd
@@ -24,7 +24,7 @@ SKFCHOPPER_IOC_01_registerRecordDeviceDriver pdbbase
 #                       int noProcessEos);
 
 drvAsynIPPortConfigure("chop1","130.246.54.103:502",0,0,1)
-#drvAsynIPPortConfigure("chop2","130.246.54.104:502",0,0,1)
+drvAsynIPPortConfigure("chop2","130.246.54.104:502",0,0,1)
 
 # link type is 0 for tcp, 1 for RTU. 2 for ASCII
 #modbusInterposeConfig(const char *portName,
@@ -32,7 +32,7 @@ drvAsynIPPortConfigure("chop1","130.246.54.103:502",0,0,1)
 #                      int timeoutMsec, 
 #                      int writeDelayMsec)
 modbusInterposeConfig("chop1",0,5000,0)
-#modbusInterposeConfig("chop2",0,5000,0)
+modbusInterposeConfig("chop2",0,5000,0)
 
 # Modbus function codes supported are:
 #  Read holding registers      03 
@@ -41,8 +41,14 @@ modbusInterposeConfig("chop1",0,5000,0)
 drvModbusAsynConfigure("runspeed1", "chop1", 0, 3, 353, 2, 8, 1000, "SKF Chopper")
 drvModbusAsynConfigure("time1", "chop1", 0, 3, 240, 5, 0, 1000, "SKF Chopper")
 drvModbusAsynConfigure("time1w", "chop1", 0, 16, 242, 1, 0, 1, "SKF Chopper")
-drvModbusAsynConfigure("rdir", "chop1", 0, 3, 347, 1, 0, 1000, "SKF Chopper")
-drvModbusAsynConfigure("rdirw", "chop1", 0, 16, 347, 1, 0, 1, "SKF Chopper")
+drvModbusAsynConfigure("rdir1", "chop1", 0, 3, 347, 1, 0, 1000, "SKF Chopper")
+drvModbusAsynConfigure("rdir1w", "chop1", 0, 16, 347, 1, 0, 1, "SKF Chopper")
+
+drvModbusAsynConfigure("runspeed2", "chop2", 0, 3, 353, 2, 8, 1000, "SKF Chopper")
+drvModbusAsynConfigure("time2", "chop2", 0, 3, 240, 5, 0, 1000, "SKF Chopper")
+drvModbusAsynConfigure("time2w", "chop2", 0, 16, 242, 1, 0, 1, "SKF Chopper")
+drvModbusAsynConfigure("rdir2", "chop2", 0, 3, 347, 1, 0, 1000, "SKF Chopper")
+drvModbusAsynConfigure("rdir2w", "chop2", 0, 16, 347, 1, 0, 1, "SKF Chopper")
 
 ## Load record instances
 
@@ -50,7 +56,8 @@ drvModbusAsynConfigure("rdirw", "chop1", 0, 16, 347, 1, 0, 1, "SKF Chopper")
 < $(IOCSTARTUP)/dbload.cmd
 
 ## Load our record instances
-dbLoadRecords("$(SKFCHOPPER)/db/SKFChopper.db","P=$(MYPVPREFIX),PORT1=runspeed1,PORT2=time1,PORT3=rdir")
+dbLoadRecords("$(SKFCHOPPER)/db/SKFChopper.db","P=$(MYPVPREFIX)SKF1:,PORT1=runspeed1,PORT2=time1,PORT3=rdir1")
+dbLoadRecords("$(SKFCHOPPER)/db/SKFChopper.db","P=$(MYPVPREFIX)SKF2:,PORT1=runspeed2,PORT2=time2,PORT3=rdir2")
 
 ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
 < $(IOCSTARTUP)/preiocinit.cmd
