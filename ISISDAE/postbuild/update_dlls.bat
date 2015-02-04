3b0ed4c1bad1cfa8911d87bc35fda61db1fcbf3c
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2015-02-04 15:39:12 +0000

    Use ICP_Binaries symbolic link
    Former-commit-id: d5d78e5b1b0bcbb805394a8a35e17cd94bacbd3f

--- ISISDAE/postbuild/update_dlls.bat
+++ ISISDAE/postbuild/update_dlls.bat
@@ -1,8 +1,9 @@
 @echo off
 set MYDIR=%~dp0
 REM set ICPDIR=c:\development\instrumentcontrol\icp\labview modules\dae\service\x64
-set ICPDIR=\\isis\inst$\Kits$\CompGroup\ICP\isisdae\x64
+REM set ICPDIR=\\isis\inst$\Kits$\CompGroup\ICP\isisdae\x64
 REM set ICPDIR=\\olympic\babylon5\Public\Freddie\isisdae\x64
+set ICPDIR=%KIT_ROOT%ICP_Binaries\isisdae\x64
 echo Checking %ICPDIR% for updated DAE DLLs 
 xcopy /i /q /d /y "%ICPDIR%\debug\*.dll" "%MYDIR%..\bin\windows-x64-debug"
 xcopy /i /q /d /y "%ICPDIR%\debug\*.manifest" "%MYDIR%..\bin\windows-x64-debug"
