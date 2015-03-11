6cc1a2e7391260ee1e0ca72d4613c9114284456a
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2015-03-11 19:05:44 +0000

    Call init
    Former-commit-id: 086e0ca1d24d0826cd2e3686feafaee66ab7f08f

--- SDTEST/iocBoot/iocSDTEST-IOC-01/st.cmd
+++ SDTEST/iocBoot/iocSDTEST-IOC-01/st.cmd
@@ -9,13 +9,15 @@
 dbLoadDatabase("../../dbd/SDTEST-IOC-01.dbd",0,0)
 SDTEST_IOC_01_registerRecordDeviceDriver(pdbbase) 
 
+< $(IOCSTARTUP)/init.cmd
+
 ## set path to stream driver protocol file referenced in db files
 epicsEnvSet ("STREAM_PROTOCOL_PATH", "$(TOP)/data")
 
 drvAsynIPPortConfigure ("SD1", "130.246.51.169:23")
 
 #drvAsynSerialPortConfigure ("SD1", "COM1")
-##asynSetOption ("SD1", 0, "baud", "9600")
+#asynSetOption ("SD1", 0, "baud", "9600")
 #asynSetOption ("SD1", 0, "baud", "115200")
 #asynSetOption ("SD1", 0, "bits", "8")
 #asynSetOption ("SD1", 0, "parity", "none")
