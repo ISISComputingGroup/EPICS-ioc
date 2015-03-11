395f08c02dd48df969b02a21f513b71029c9ae10
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2015-03-11 14:38:00 +0000

    Add async input
    Former-commit-id: 1d7285a2b64a35ea1122560294ec060ac2bc1a00

--- SDTEST/iocBoot/iocSDTEST-IOC-01/st.cmd
+++ SDTEST/iocBoot/iocSDTEST-IOC-01/st.cmd
@@ -9,6 +9,7 @@
 dbLoadDatabase("../../dbd/SDTEST-IOC-01.dbd",0,0)
 SDTEST_IOC_01_registerRecordDeviceDriver(pdbbase) 
 
+## set path to stream driver protocol file referenced in db files
 epicsEnvSet ("STREAM_PROTOCOL_PATH", "$(TOP)/data")
 
 drvAsynIPPortConfigure ("SD1", "130.246.51.169:23")
@@ -28,6 +29,3 @@ drvAsynIPPortConfigure ("SD1", "130.246.51.169:23")
 dbLoadRecords("../../db/SDTEST-IOC-01.db","P=$(MYPVPREFIX),PORT=SD1")
 
 iocInit()
-
-## Start any sequence programs
-#seq sncSDTEST-IOC-01,"user=faa59"
