a3f71ee5b3837de15928b153765ee2f4a0a41ac2
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2015-04-19 20:29:58 +0000

    Add pvdump DLL location to PATH
    Former-commit-id: 49e0cbe6a2f8efa6f3a0e520848b9a24fb05b668

--- MK3CHOPPER/src/runIOC.bat
+++ MK3CHOPPER/src/runIOC.bat
@@ -3,6 +3,9 @@ set MYDIRCHOP=%~dp0
 
 call %MYDIRCHOP%..\..\..\..\config_env_base.bat
 
+REM get location of pvdump.dll added to PATH
+call %EPICS_KIT_ROOT%support\pvdump\iocBoot\iocpvdumpTest\dllPath.bat
+
 %HIDEWINDOW% h
 
 %MYDIRCHOP%..\..\bin\%EPICS_HOST_ARCH%\Mk3Chopper.exe %*
