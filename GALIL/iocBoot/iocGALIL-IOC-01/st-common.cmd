5c44c93181ec179c36505b87f14e2a29d2d0646c
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2015-04-20 14:46:49 +0000

    Change way moving motors is calculated so less likely to miss something
    Former-commit-id: ca34582f697ca86abf12e1399c45d7c2188a8571

--- GALIL/iocBoot/iocGALIL-IOC-01/st-common.cmd
+++ GALIL/iocBoot/iocGALIL-IOC-01/st-common.cmd
@@ -46,7 +46,7 @@ epicsEnvSet("GALILCONFIG","$(ICPCONFIGROOT)/galil")
 < sampleChanger.cmd
 
 ## motor util package
-dbLoadRecords("$(MOTOR)/db/motorUtil.db","P=$(MYPVPREFIX)$(IOCNAME):, PVPREFIX=$(MYPVPREFIX)")
+dbLoadRecords("$(MOTOR)/db/motorUtil.db","P=$(MYPVPREFIX)$(IOCNAME):,PVPREFIX=$(MYPVPREFIX),IFDMC01=$(IFDMC01),IFDMC02=$(IFDMC02),IFDMC03=$(IFDMC03),IFDMC04=$(IFDMC04),IFDMC05=$(IFDMC05),IFDMC06=$(IFDMC06),IFDMC07=$(IFDMC07)")
 #dbLoadRecords("${TOP}/../INSTETC/db/INSTETC.db","P=$(MYPVPREFIX),IOC=$(IOCNAME)")
 
 ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
