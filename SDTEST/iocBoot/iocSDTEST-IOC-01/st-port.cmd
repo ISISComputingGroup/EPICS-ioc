b9f4e30cec736e7ad5ff0a13efc3f98658a648f4
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2015-06-14 20:12:20 +0000

    Now simulates ports
    Former-commit-id: 3e04f5dc57db1961bb9b7a25e2379e96a7cc15cc

--- SDTEST/iocBoot/iocSDTEST-IOC-01/st-port.cmd
+++ SDTEST/iocBoot/iocSDTEST-IOC-01/st-port.cmd
@@ -4,32 +4,29 @@
 # this defines macros we can use for conditional loading later
 stringiftest("PORT", "$(PORT$(PN)=)")
 
-$(IFPORT) drvAsynSerialPortConfigure ("SD$(PN)", "$(PORT$(PN)=)")
-$(IFPORT) asynSetOption ("SD$(PN)", 0, "baud", "$(BAUD$(PN)=9600)")
-$(IFPORT) asynSetOption ("SD$(PN)", 0, "bits", "$(BITS$(PN)=8)")
-$(IFPORT) asynSetOption ("SD$(PN)", 0, "parity", "$(PARITY$(PN)=none)")
-$(IFPORT) asynSetOption ("SD$(PN)", 0, "stop", "$(STOP$(PN)=1)")
-$(IFPORT) asynSetOption ("SD$(PN)", 0, "clocal", "$(CLOCAL$(PN)=Y)") # if N, output flow control using DSR signal
-$(IFPORT) asynSetOption ("SD$(PN)", 0, "crtscts", "$(CRTSCTS$(PN)=N)") # if Y, use hardware flow control (RTS/CTS)
-$(IFPORT) asynSetOption ("SD$(PN)", 0, "ixon", "$(IXON$(PN)=N)") # if Y, use software flow control for output
-$(IFPORT) asynSetOption ("SD$(PN)", 0, "ixoff", "$(IXOFF$(PN)=N)") # if Y, use software flow control for input
-$(IFPORT) asynOctetSetOutputEos("SD$(PN)",0,"$(OEOS$(PN)=\\r\\n)")
-$(IFPORT) asynOctetSetInputEos("SD$(PN)",0,"$(IEOS$(PN)=\\r\\n)")
+# create a real serial port, unless in simulation mode then crreate an unconnected asyn port 
+$(IFPORT)$(IFSIM)    drvAsynSerialPortConfigure ("SD$(PN)", "NUL", 0, 1)
+
+$(IFPORT)$(IFNOTSIM) drvAsynSerialPortConfigure ("SD$(PN)", "$(PORT$(PN)=)")
+$(IFPORT)$(IFNOTSIM) asynSetOption ("SD$(PN)", 0, "baud", "$(BAUD$(PN)=9600)")
+$(IFPORT)$(IFNOTSIM) asynSetOption ("SD$(PN)", 0, "bits", "$(BITS$(PN)=8)")
+$(IFPORT)$(IFNOTSIM) asynSetOption ("SD$(PN)", 0, "parity", "$(PARITY$(PN)=none)")
+$(IFPORT)$(IFNOTSIM) asynSetOption ("SD$(PN)", 0, "stop", "$(STOP$(PN)=1)")
+$(IFPORT)$(IFNOTSIM) asynSetOption ("SD$(PN)", 0, "clocal", "$(CLOCAL$(PN)=Y)") # if N, output flow control using DSR signal
+$(IFPORT)$(IFNOTSIM) asynSetOption ("SD$(PN)", 0, "crtscts", "$(CRTSCTS$(PN)=N)") # if Y, use hardware flow control (RTS/CTS)
+$(IFPORT)$(IFNOTSIM) asynSetOption ("SD$(PN)", 0, "ixon", "$(IXON$(PN)=N)") # if Y, use software flow control for output
+$(IFPORT)$(IFNOTSIM) asynSetOption ("SD$(PN)", 0, "ixoff", "$(IXOFF$(PN)=N)") # if Y, use software flow control for input
+$(IFPORT)$(IFNOTSIM) asynOctetSetOutputEos("SD$(PN)",0,"$(OEOS$(PN)=\\r\\n)")
+$(IFPORT)$(IFNOTSIM) asynOctetSetInputEos("SD$(PN)",0,"$(IEOS$(PN)=\\r\\n)")
 
 stringiftest("TRACEMASK", "$(TRACEMASK$(PN)=)")
-$(IFTRACEMASK) asynSetTraceMask("SD$(PN)",0,"$(TRACEMASK$(PN)=)")
+$(IFPORT)$(IFTRACEMASK) asynSetTraceMask("SD$(PN)",0,"$(TRACEMASK$(PN)=)")
 
 stringiftest("TRACEIOMASK", "$(TRACEIOMASK$(PN)=)")
-$(IFTRACEIOMASK) asynSetTraceIOMask("SD$(PN)",0,"$(TRACEIOMASK$(PN)=)")
+$(IFPORT)$(IFTRACEIOMASK) asynSetTraceIOMask("SD$(PN)",0,"$(TRACEIOMASK$(PN)=)")
 
 ## Load record instances for connected port
-$(IFPORT) dbLoadRecords("$(TOP)/db/SDTEST-IOC-01.db","P=$(MYPVPREFIX),Q=$(IOCNAME):P$(PN):,PORT=SD$(PN),DEV=$(PORT$(PN)=),NAME=$(PORT$(PN)_NAME=),DISABLE=0,SIM=0")
-
+$(IFPORT) dbLoadRecords("$(TOP)/db/SDTEST-IOC-01.db","P=$(MYPVPREFIX),Q=$(IOCNAME):P$(PN):,PORT=SD$(PN),DEV=$(PORT$(PN)=),NAME=$(PORT$(PN)_NAME=),DISABLE=$(DISABLE),SIM=$(SIMULATE)")
 $(IFPORT) dbLoadRecords("$(ASYN)/db/asynRecord.db","P=$(MYPVPREFIX),R=$(IOCNAME):P$(PN):ASYNREC,PORT=SD$(PN),ADDR=0,OMAX=80,IMAX=80")
-
-## Load record instances for undefined port
-## not quite working - need something better than NUL
-#$(IFNOTPORT) drvAsynSerialPortConfigure ("SD$(PN)", "$(PORT$(PN)=NUL)")
-#$(IFNOTPORT) dbLoadRecords("$(TOP)/db/SDTEST-IOC-01.db","P=$(MYPVPREFIX),Q=$(IOCNAME):P$(PN):,PORT=SD$(PN),DEV=,NAME=$(PORT$(PN)_NAME=),DISABLE=1,SIM=1")
 
 #
