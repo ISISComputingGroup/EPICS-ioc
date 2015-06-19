baf0472ef46b91974838dc4e739c042941bcf709
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2015-06-19 17:37:18 +0000

    Now generic via macros
    Former-commit-id: d9b8de70a26f085769d13a6e9c49d128b4cd54cf

--- SDTEST/iocBoot/iocSDTEST-IOC-01/st-port.cmd
+++ SDTEST/iocBoot/iocSDTEST-IOC-01/st-port.cmd
@@ -7,10 +7,10 @@ stringiftest("PORT", "$(PORT$(PN)=)")
 # create a real serial port, unless in simulation mode then crreate an unconnected asyn port 
 $(IFPORT)$(IFSIM)    drvAsynSerialPortConfigure ("SD$(PN)", "NUL", 0, 1)
 
-$(IFPORT)$(IFNOTSIM)drvAsynIPPortConfigure("SD$(PN)","$(PORT$(PN)=)",0,0,0) 
+#$(IFPORT)$(IFNOTSIM)drvAsynIPPortConfigure("SD$(PN)","$(PORT$(PN)=)",0,0,0) 
 
 # defaults should be reflected in config.xml
-#$(IFPORT)$(IFNOTSIM) drvAsynSerialPortConfigure ("SD$(PN)", "$(PORT$(PN)=)")
+$(IFPORT)$(IFNOTSIM) drvAsynSerialPortConfigure ("SD$(PN)", "$(PORT$(PN)=)")
 $(IFPORT)$(IFNOTSIM) asynSetOption ("SD$(PN)", 0, "baud", "$(BAUD$(PN)=9600)")
 $(IFPORT)$(IFNOTSIM) asynSetOption ("SD$(PN)", 0, "bits", "$(BITS$(PN)=8)")
 $(IFPORT)$(IFNOTSIM) asynSetOption ("SD$(PN)", 0, "parity", "$(PARITY$(PN)=none)")
@@ -29,7 +29,8 @@ stringiftest("TRACEIOMASK", "$(TRACEIOMASK$(PN)=)")
 $(IFPORT)$(IFTRACEIOMASK) asynSetTraceIOMask("SD$(PN)",0,"$(TRACEIOMASK$(PN)=)")
 
 ## Load record instances for connected port
-$(IFPORT) dbLoadRecords("$(TOP)/db/SDTEST-IOC-01.db","P=$(MYPVPREFIX),Q=$(IOCNAME):P$(PN):,PORT=SD$(PN),DEV=$(PORT$(PN)=),NAME=$(PORT$(PN)_NAME=),DISABLE=$(DISABLE),SIM=$(SIMULATE)")
+$(IFPORT) dbLoadRecords("$(TOP)/db/SDTEST-IOC-01.db","P=$(MYPVPREFIX),Q=$(IOCNAME):P$(PN):,PORT=SD$(PN),DEV=$(PORT$(PN)=),NAME=$(PORT$(PN)_NAME=),DISABLE=$(DISABLE),SIM=$(SIMULATE),SCAN=.5 second,SETOUT1=1PA,GETOUT=1TP,GETIN=1TP%f,SETFMT=%f,SETREPLY=%*s")
+
 $(IFPORT) dbLoadRecords("$(ASYN)/db/asynRecord.db","P=$(MYPVPREFIX),R=$(IOCNAME):P$(PN):ASYNREC,PORT=SD$(PN),ADDR=0,OMAX=80,IMAX=80")
 
 #
