706f5f88f5e75bc5095601effba535eef737977a
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2015-08-24 11:09:27 +0100

    Fix path to config_env

--- MK3CHOPPER/src/runIOC.bat
+++ MK3CHOPPER/src/runIOC.bat
@@ -1,10 +1,10 @@
 @echo OFF
 set MYDIRCHOP=%~dp0
 
-call %MYDIRCHOP%..\..\..\..\config_env_base.bat
+call %MYDIRCHOP%..\..\..\..\..\config_env_base.bat
 
 REM get location of pvdump.dll added to PATH
-call %EPICS_KIT_ROOT%support\pvdump\iocBoot\iocpvdumpTest\dllPath.bat
+call %EPICS_KIT_ROOT%support\pvdump\master\iocBoot\iocpvdumpTest\dllPath.bat
 
 %HIDEWINDOW% h
 
