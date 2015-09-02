df76675ee9af47ea3a567dff8198f1ba0cd774f5
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2015-09-02 23:31:05 +0100

    Correct path to bump_stop.db

--- GALIL/iocBoot/iocGALIL-IOC-01/st.cmd
+++ GALIL/iocBoot/iocGALIL-IOC-01/st.cmd
@@ -13,6 +13,6 @@ GALIL_IOC_01_registerRecordDeviceDriver pdbbase
 
 cd ${TOP}/iocBoot/${IOC}
 
-dbLoadRecords("$(ISISSUPPORT)/Common/db/bump_stop.db", "P=$(MYPVPREFIX)MOT:,BMPSTP=DMC01:Galil0Bi3_STATUS")
+dbLoadRecords("$(ISISSUPPORT)/Common/master/db/bump_stop.db", "P=$(MYPVPREFIX)MOT:,BMPSTP=DMC01:Galil0Bi3_STATUS")
 
 < st-common.cmd
