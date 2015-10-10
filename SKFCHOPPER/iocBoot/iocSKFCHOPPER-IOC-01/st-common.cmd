75e115998ba63e7caaa62fb23114dd1c10161154
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2015-10-10 20:41:55 +0100

    Move modbus commands to support

--- SKFCHOPPER/iocBoot/iocSKFCHOPPER-IOC-01/st-common.cmd
+++ SKFCHOPPER/iocBoot/iocSKFCHOPPER-IOC-01/st-common.cmd
@@ -1,48 +1,26 @@
 ##ISIS## Run IOC initialisation 
 < $(IOCSTARTUP)/init.cmd
 
+# set name of chopper port for use later
+epicsEnvSet("CHOP", "c1")
+
 #drvAsynIPPortConfigure(const char *portName,
 #                       const char *hostInfo,
 #                       unsigned int priority,
 #                       int noAutoConnect,
 #                       int noProcessEos);
 
-drvAsynIPPortConfigure("chop","$(IPADDR):$(IPPORT=502)",0,0,1)
+drvAsynIPPortConfigure("$(CHOP)","$(IPADDR):$(IPPORT=502)",0,0,1)
 
 # link type is 0 for tcp, 1 for RTU. 2 for ASCII
 #modbusInterposeConfig(const char *portName,
 #                      modbusLinkType linkType,
 #                      int timeoutMsec, 
 #                      int writeDelayMsec)
-modbusInterposeConfig("chop",0,5000,0)
+modbusInterposeConfig("$(CHOP)",0,5000,0)
 
-# Modbus function codes supported are:
-#  Read holding registers      03 
-#  Preset/write multiple registers   16 
-# length always specified in number of 16 bit words,
-# address here is 1 less than in chopper register doc with 40xxx prefix removed 
-drvModbusAsynConfigure("ctime", "chop", 0, 3, 240, 5, 0, 1000, "SKF Chopper")
-drvModbusAsynConfigure("rdir", "chop", 0, 3, 347, 1, 0, 1000, "SKF Chopper")
-drvModbusAsynConfigure("rdirw", "chop", 0, 16, 347, 1, 0, 1, "SKF Chopper")
-drvModbusAsynConfigure("freq", "chop", 0, 3, 353, 2, 8, 1000, "SKF Chopper")
-drvModbusAsynConfigure("freqw", "chop", 0, 16, 345, 2, 8, 1, "SKF Chopper")
-drvModbusAsynConfigure("phas", "chop", 0, 3, 362, 2, 8, 1000, "SKF Chopper")
-drvModbusAsynConfigure("phasw", "chop", 0, 16, 362, 2, 8, 1, "SKF Chopper")
-drvModbusAsynConfigure("phaserr", "chop", 0, 3, 360, 2, 0, 1000, "SKF Chopper")
-drvModbusAsynConfigure("phaserrw", "chop", 0, 16, 360, 2, 0, 1, "SKF Chopper")
-drvModbusAsynConfigure("veto", "chop", 0, 3, 375, 2, 8, 1000, "SKF Chopper")
-drvModbusAsynConfigure("ready", "chop", 0, 3, 367, 2, 8, 1000, "SKF Chopper")
-drvModbusAsynConfigure("insync", "chop", 0, 3, 371, 2, 8, 1000, "SKF Chopper")
-drvModbusAsynConfigure("start", "chop", 0, 16, 388, 1, 8, 1, "SKF Chopper")
-drvModbusAsynConfigure("stop", "chop", 0, 16, 389, 1, 8, 1, "SKF Chopper")
-drvModbusAsynConfigure("state", "chop", 0, 3, 462, 1, 0, 1000, "SKF Chopper")
-drvModbusAsynConfigure("phasd", "chop", 0, 3, 483, 2, 8, 1000, "SKF Chopper")
-drvModbusAsynConfigure("phaserrd", "chop", 0, 3, 481, 2, 8, 1000, "SKF Chopper")
-drvModbusAsynConfigure("rspeed", "chop", 0, 3, 477, 1, 0, 1000, "SKF Chopper")
-drvModbusAsynConfigure("homeang", "chop", 0, 3, 379, 2, 8, 1000, "SKF Chopper")
-drvModbusAsynConfigure("homeangw", "chop", 0, 16, 379, 2, 8, 1, "SKF Chopper")
-drvModbusAsynConfigure("reset", "chop", 0, 16, 392, 1, 0, 1, "SKF Chopper")
-drvModbusAsynConfigure("status", "chop", 0, 3, 478, 1, 0, 1000, "SKF Chopper")
+# load modbus definitions for use by SKFChopper.db, this used $(CHOP)
+< $(SKFCHOPPER)/data/SKFChopper.cmd
 
 ## Load record instances
 
@@ -50,7 +28,7 @@ drvModbusAsynConfigure("status", "chop", 0, 3, 478, 1, 0, 1000, "SKF Chopper")
 < $(IOCSTARTUP)/dbload.cmd
 
 ## Load our record instances
-dbLoadRecords("$(SKFCHOPPER)/db/SKFChopper.db","P=$(MYPVPREFIX)$(IOCNAME):,TIME_L=ctime,DIR_L=rdir,FREQ_L=freq,PHAS_L=phas,PERR_L=phaserr,VETO_L=veto,READY_L=ready,INSYNC_L=insync,START_L=start,STOP_L=stop,STATE_L=state")
+dbLoadRecords("$(SKFCHOPPER)/db/SKFChopper.db","P=$(MYPVPREFIX)$(IOCNAME):,CHOP=$(CHOP)")
 
 ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
 < $(IOCSTARTUP)/preiocinit.cmd
