bf1f8c3a85f2f0010ad6168996abcb82efd63b03
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2016-04-08 15:36:12 +0100

    Add 3rd TPG300 IOC

---
+++ TPG300/iocBoot/iocTPG300-IOC-03/st.cmd
@@ -0,0 +1,47 @@
+#!../../bin/windows-x64-debug/TPG300-IOC-03
+
+## You may have to change TPG300-IOC-03 to something else
+## everywhere it appears in this file
+
+# Increase this if you get <<TRUNCATED>> or discarded messages warnings in your errlog output
+errlogInit2(65536, 256)
+
+< envPaths
+
+epicsEnvSet "STREAM_PROTOCOL_PATH" "$(TPG300)/TPG300Sup"
+#epicsEnvSet "TTY" "$(TTY=\\\\\\\\.\\\\COM20)"  
+
+cd ${TOP}
+
+## Register all support components
+dbLoadDatabase "dbd/TPG300-IOC-03.dbd"
+TPG300_IOC_02_registerRecordDeviceDriver pdbbase
+
+##ISIS## Run IOC initialisation 
+< $(IOCSTARTUP)/init.cmd
+
+drvAsynSerialPortConfigure("L0", "$(TTY)", 0, 0, 0, 0)
+asynSetOption("L0", -1, "baud", "9600")
+asynSetOption("L0", -1, "bits", "8")
+asynSetOption("L0", -1, "parity", "none")
+asynSetOption("L0", -1, "stop", "1")
+
+## Load record instances
+
+##ISIS## Load common DB records 
+< $(IOCSTARTUP)/dbload.cmd
+
+## Load our record instances
+dbLoadRecords("db/devTPG300.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0")
+
+##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
+< $(IOCSTARTUP)/preiocinit.cmd
+
+cd ${TOP}/iocBoot/${IOC}
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=mjc23Host"
+
+##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
+< $(IOCSTARTUP)/postiocinit.cmd
