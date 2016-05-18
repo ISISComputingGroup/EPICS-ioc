e64cc1387759ec3f5e5fb911e1111bd939175d75
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2016-05-18 11:18:26 +0100

    Use common startup

--- LAKESHORE218/iocBoot/iocLakeshore218-IOC-02/st.cmd
+++ LAKESHORE218/iocBoot/iocLakeshore218-IOC-02/st.cmd
@@ -7,7 +7,7 @@
 errlogInit2(65536, 256)
 
 < envPaths
-epicsEnvSet "STREAM_PROTOCOL_PATH" "$(TOP)/Lakeshore218-IOC-01App/protocol"  
+
 epicsEnvSet "TTY" "$(TTY=\\\\\\\\.\\\\COM5)"                                   
 
 cd ${TOP}
@@ -16,33 +16,6 @@ cd ${TOP}
 dbLoadDatabase "dbd/Lakeshore218-IOC-02.dbd"
 Lakeshore218_IOC_02_registerRecordDeviceDriver pdbbase
 
-##ISIS## Run IOC initialisation 
-< $(IOCSTARTUP)/init.cmd
+cd ${TOP}/iocBoot/iocLakeshore218-IOC-01
 
-drvAsynSerialPortConfigure("L0", "$(TTY)", 0, 0, 0, 0)                          
-asynSetOption("L0", -1, "baud", "9600")                                         
-asynSetOption("L0", -1, "bits", "7")                                            
-asynSetOption("L0", -1, "parity", "odd")                                       
-asynSetOption("L0", -1, "stop", "1")                                            
-#asynOctetSetInputEos("L0", -1, "\r")                                            
-#asynOctetSetOutputEos("L0", -1, "\r")                                           
-
-## Load record instances
-
-##ISIS## Load common DB records 
-< $(IOCSTARTUP)/dbload.cmd
-
-## Load our record instances
-dbLoadRecords("db/Lakeshore218.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0")
-
-##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
-< $(IOCSTARTUP)/preiocinit.cmd
-
-cd ${TOP}/iocBoot/${IOC}
-iocInit
-
-## Start any sequence programs
-#seq sncxxx,"user=luj96656Host"
-
-##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
-< $(IOCSTARTUP)/postiocinit.cmd
+< st-common.cmd
