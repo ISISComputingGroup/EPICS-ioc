b5d2dc2330a66301cc13a8380a5849cbb3883036
John-Holt-Tessella <John.Holt@tessella.com>
2016-05-23 16:28:33 +0100

    Add start of mercury controller

---
+++ MERCURY_ITC/iocBoot/iocMERCURY-IOC-01/st.cmd
@@ -0,0 +1,53 @@
+#!../../bin/windows-x64/MERCURY-IOC-01
+
+## You may have to change MERCURY-IOC-01 to something else
+## everywhere it appears in this file
+
+# Increase this if you get <<TRUNCATED>> or discarded messages warnings in your errlog output
+errlogInit2(65536, 256)
+
+< envPaths
+
+epicsEnvSet "STREAM_PROTOCOL_PATH" "$(MERCURY_ITC)/data"
+
+cd ${TOP}
+
+## Register all support components
+dbLoadDatabase "dbd/MERCURY-IOC-01.dbd"
+MERCURY_IOC_01_registerRecordDeviceDriver pdbbase
+
+##ISIS## Run IOC initialisation 
+< $(IOCSTARTUP)/init.cmd
+
+#drvAsynSerialPortConfigure("L0", "$(TTY)", 0, 0, 0, 0)
+#asynSetOption("L0", -1, "baud", "9600")               
+#asynSetOption("L0", -1, "bits", "8")                  
+#asynSetOption("L0", -1, "parity", "none")             
+#asynSetOption("L0", -1, "stop", "1")                  
+#asynOctetSetInputEos("L0", -1, "\r")                  
+#asynOctetSetOutputEos("L0", -1, "\r")
+
+drvAsynIPPortConfigure("L0","localhost:51234",0,0,0)
+                 
+
+## Load record instances
+
+##ISIS## Load common DB records 
+< $(IOCSTARTUP)/dbload.cmd
+
+## Load our record instances
+#dbLoadRecords("db/xxx.db","user=hgv27692Host")
+dbLoadRecords("db/main_unit.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0")
+
+
+##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
+< $(IOCSTARTUP)/preiocinit.cmd
+
+cd ${TOP}/iocBoot/${IOC}
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=hgv27692Host"
+
+##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
+< $(IOCSTARTUP)/postiocinit.cmd
