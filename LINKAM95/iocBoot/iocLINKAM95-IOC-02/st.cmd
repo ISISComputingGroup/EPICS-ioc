6b02bf68315a7bcb138092e06b5edbf5331e4707
AdrianPotter <adrian.potter@tessella.com>
2016-06-09 09:55:10 +0100

    Add IOC-02 st.cmd

--- LINKAM95/iocBoot/iocLINKAM95-IOC-02/st.cmd
+++ LINKAM95/iocBoot/iocLINKAM95-IOC-02/st.cmd
@@ -8,24 +8,36 @@ errlogInit2(65536, 256)
 
 < envPaths
 
+epicsEnvSet "STREAM_PROTOCOL_PATH" "$(LINKAM95)/linkam95Sup"
+epicsEnvSet "DEVICE" "L0"
+
 cd ${TOP}
 
 ## Register all support components
 dbLoadDatabase "dbd/LINKAM95-IOC-02.dbd"
 LINKAM95_IOC_02_registerRecordDeviceDriver pdbbase
 
-##ISIS## Run IOC initialisation 
+##ISIS## Run IOC initialisation
 < $(IOCSTARTUP)/init.cmd
+
+drvAsynSerialPortConfigure("$(DEVICE)", "$(PORT)", 0, 0, 0, 0)
+asynSetOption("$(DEVICE)", -1, "baud", "19200")
+asynSetOption("$(DEVICE)", -1, "bits", "8")
+asynSetOption("$(DEVICE)", -1, "parity", "none")
+asynSetOption("$(DEVICE)", -1, "stop", "1")
+asynOctetSetInputEos("$(DEVICE)", -1, "\r")
+asynOctetSetOutputEos("$(DEVICE)", -1, "\r")
 
 ## Load record instances
 
-##ISIS## Load common DB records 
+##ISIS## Load common DB records
 < $(IOCSTARTUP)/dbload.cmd
 
 ## Load our record instances
+dbLoadRecords("db/LINKAM95.db","P=$(MYPVPREFIX)$(IOCNAME):, port=$(DEVICE)")
 #dbLoadRecords("db/xxx.db","user=iew83206Host")
 
-##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
+##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called
 < $(IOCSTARTUP)/preiocinit.cmd
 
 cd ${TOP}/iocBoot/${IOC}
@@ -34,5 +46,5 @@ iocInit
 ## Start any sequence programs
 #seq sncxxx,"user=iew83206Host"
 
-##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
+##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs
 < $(IOCSTARTUP)/postiocinit.cmd
