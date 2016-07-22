5a08c26767177943afc42b2f3d0f8806a48f12ee
John-Holt-Tessella <John.Holt@tessella.com>
2016-07-22 10:57:11 +0100

    TPG26x initial commit

---
+++ TPG26x/iocBoot/iocTPG268x-IOC-01/st.cmd
@@ -0,0 +1,17 @@
+#!../../bin/windows-x64/TPG268x-IOC-01
+
+## You may have to change TPG268x-IOC-01 to something else
+## everywhere it appears in this file
+
+# Increase this if you get <<TRUNCATED>> or discarded messages warnings in your errlog output
+errlogInit2(65536, 256)
+
+< envPaths
+
+cd ${TOP}
+
+## Register all support components
+dbLoadDatabase "dbd/TPG268x-IOC-01.dbd"
+TPG268x_IOC_01_registerRecordDeviceDriver pdbbase
+
+< ${TOP}/iocBoot/${IOC}/st-common.cmd
