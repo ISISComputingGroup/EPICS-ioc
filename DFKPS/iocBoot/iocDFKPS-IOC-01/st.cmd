0e3b93a758102df877948675a307b6260f90f934
Thomas Lohnert <thomas.lohnert@stfc.ac.uk>
2016-07-26 09:13:35 +0100

    can change calib during runtime added slew record added slew & calib checks to st.cmd

--- DFKPS/iocBoot/iocDFKPS-IOC-01/st.cmd
+++ DFKPS/iocBoot/iocDFKPS-IOC-01/st.cmd
@@ -9,7 +9,9 @@ errlogInit2(65536, 256)
 < envPaths
 
 epicsEnvSet "IOCNAME" "DFKPS_01"
-epicsEnvSet "STREAM_PROTOCOL_PATH" "$(DANFYSIK8000)/master/danfysikMps8000App/protocol/:$(DANFYSIK8000)/master/danfysikMps8000App/protocol/DFK$(DEV_TYPE=8800)/"
+epicsEnvSet "DEFAULT_CFILE" "default_calib.dat"
+epicsEnvSet "DEFAULT_CPATH" "C:/Instrument/Settings/calib/danfysik/"
+epicsEnvSet "STREAM_PROTOCOL_PATH" "$(DANFYSIK8000)/master/danfysikMps8000App/protocol/:$(DANFYSIK8000)/master/danfysikMps8000App/protocol/DFK$(DEV_TYPE=8000)/"
 
 cd ${TOP}
 
@@ -20,16 +22,20 @@ DFKPS_IOC_01_registerRecordDeviceDriver pdbbase
 ##ISIS## Run IOC initialisation 
 < $(IOCSTARTUP)/init.cmd
 
+## use with emulator
 #drvAsynIPPortConfigure("L0", "localhost:xxxxx")
 
+## use with real device
 drvAsynSerialPortConfigure("L0", "$(PORT)", 0, 0, 0, 0)
 asynSetOption("L0", -1, "baud", "$(BAUD=9600)")
 asynSetOption("L0", -1, "bits", "$(BITS=8)")
 asynSetOption("L0", -1, "parity", "$(PARITY="none")")
 asynSetOption("L0", -1, "stop", "$(STOP=2)")
 
-## check for polarity type
+## checks used for loading db files
 stringiftest  "POLAR"  "$(POLARITY)"  5  "BIPOLAR"
+stringiftest  "CALIB"  "$(CALIBRATED)"  5  "1"
+stringiftest  "SLEW"  "$(USE_SLEW)"  5  "1"
 
 ## Load FileList
 ## A seperate instance must be created for each danfysik
@@ -43,6 +49,8 @@ FileListConfigure("RAMPFILELIST1", $(RAMP_DIR), $(RAMP_PAT))
 ## Load record instances
 dbLoadRecords("$(TOP)/db/DFKPS_common.db", "device=$(MYPVPREFIX)$(IOCNAME), P=$(MYPVPREFIX)$(IOCNAME):, port=L0")
 $(IFPOLAR) dbLoadRecords("$(TOP)/db/DFKPS_polarity.db", "device=$(MYPVPREFIX)$(IOCNAME), P=$(MYPVPREFIX)$(IOCNAME):, port=L0")
+$(IFCALIB) dbLoadRecords("$(TOP)/db/DFKPS_calibrated.db", "device=$(MYPVPREFIX)$(IOCNAME), P=$(MYPVPREFIX)$(IOCNAME):, CALIB_FILE=$(CFILE=$(DEFAULT_CFILE)), CALIB_PATH=$(CPATH=$(DEFAULT_CPATH)), port=L0")
+$(IFSLEW) dbLoadRecords("$(TOP)/db/DFKPS_slew.db", "device=$(MYPVPREFIX)$(IOCNAME), P=$(MYPVPREFIX)$(IOCNAME):, port=L0")
 
 ## Load device type specific st.cmd
-< iocBoot/iocDFKPS-IOC-01/st-$(DEV_TYPE=8800).cmd
+< iocBoot/iocDFKPS-IOC-01/st-$(DEV_TYPE=8000).cmd
