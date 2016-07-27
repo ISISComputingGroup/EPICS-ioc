e18ff63d9c5371fd6fdda4b0dd4662ecf5ba6a88
IsabellaRey <isabella.rey@stfc.ac.uk>
2016-07-27 13:58:11 +0100

    Ticket 1404: using common st.cmd

---
+++ LKSH336/iocBoot/iocLKSH336-IOC-01/st-common.cmd
@@ -0,0 +1,33 @@
+epicsEnvSet "STREAM_PROTOCOL_PATH" "$(LKSH336)/protocol"
+epicsEnvSet "DEVICE" "L0"
+
+cd ${TOP}
+
+##ISIS## Run IOC initialisation 
+< $(IOCSTARTUP)/init.cmd
+
+drvAsynIPPortConfigure ("$(DEVICE)", "$(ADDR):7777")
+
+## Load record instances
+
+##ISIS## Load common DB records 
+< $(IOCSTARTUP)/dbload.cmd
+
+## Load our record instances
+#dbLoadRecords("db/xxx.db","user=iew83206Host")
+dbLoadRecords("db/lakeshore336.db", "P=$(MYPVPREFIX)$(IOCNAME), PORT=$(DEVICE), ADDR=0, TEMPSCAN=1, SCAN=5, TOLERANCE=1")
+
+##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
+< $(IOCSTARTUP)/preiocinit.cmd
+
+cd ${TOP}/iocBoot/${IOC}
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=iew83206Host"
+
+##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
+< $(IOCSTARTUP)/postiocinit.cmd
+
+
+
--- LKSH336/iocBoot/iocLKSH336-IOC-02/st.cmd
+++ LKSH336/iocBoot/iocLKSH336-IOC-02/st.cmd
@@ -8,37 +8,12 @@ errlogInit2(65536, 256)
 
 < envPaths
 
-epicsEnvSet "STREAM_PROTOCOL_PATH" "$(LKSH336)/protocol"
-epicsEnvSet "DEVICE" "L0"
-
 cd ${TOP}
 
 ## Register all support components
 dbLoadDatabase "dbd/LKSH336-IOC-02.dbd"
 LKSH336_IOC_02_registerRecordDeviceDriver pdbbase
 
-##ISIS## Run IOC initialisation 
-< $(IOCSTARTUP)/init.cmd
+cd ${TOP}/iocBoot/iocLKSH336-IOC-01
 
-drvAsynIPPortConfigure ("$(DEVICE)", "$(ADDR):7777")
-
-## Load record instances
-
-##ISIS## Load common DB records 
-< $(IOCSTARTUP)/dbload.cmd
-
-## Load our record instances
-#dbLoadRecords("db/xxx.db","user=iew83206Host")
-dbLoadRecords("db/lakeshore336.db", "P=$(MYPVPREFIX)$(IOCNAME), PORT=$(DEVICE), ADDR=0, TEMPSCAN=1, SCAN=5, TOLERANCE=1")
-
-##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
-< $(IOCSTARTUP)/preiocinit.cmd
-
-cd ${TOP}/iocBoot/${IOC}
-iocInit
-
-## Start any sequence programs
-#seq sncxxx,"user=iew83206Host"
-
-##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
-< $(IOCSTARTUP)/postiocinit.cmd
+< st-common.cmd
