dbc08476a670443a256e479ba467be9d94279261
Thomas Lohnert <thomas.lohnert@stfc.ac.uk>
2016-07-28 09:23:11 +0100

    all IOCs call st-common.cmd in IOC-01 (and subsequently device-specific st.cmds)

--- DFKPS/iocBoot/iocDFKPS-IOC-01/st-8000.cmd
+++ DFKPS/iocBoot/iocDFKPS-IOC-01/st-8000.cmd
@@ -8,12 +8,3 @@ asynOctetWrite DFKINIT "UNLOCK\r"
 
 ## Load our record instances
 dbLoadRecords("$(TOP)/Db/DFKPS_8000_status.db", "device=$(MYPVPREFIX)$(IOCNAME), P=$(MYPVPREFIX)$(IOCNAME):, port=L0, powpos=$(POWER_FLAG_POSITION), ilkpos=$(INTERLOCK_FLAG_POSITION)")
-
-##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
-< $(IOCSTARTUP)/preiocinit.cmd
-
-cd ${TOP}/iocBoot/${IOC}
-iocInit
-
-##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
-< $(IOCSTARTUP)/postiocinit.cmd
--- DFKPS/iocBoot/iocDFKPS-IOC-01/st.cmd
+++ DFKPS/iocBoot/iocDFKPS-IOC-01/st.cmd
@@ -9,9 +9,6 @@ errlogInit2(65536, 256)
 < envPaths
 
 epicsEnvSet "IOCNAME" "DFKPS_01"
-epicsEnvSet "DEFAULT_CFILE" "default_calib.dat"
-epicsEnvSet "DEFAULT_CPATH" "C:/Instrument/Settings/calib/danfysik/"
-epicsEnvSet "STREAM_PROTOCOL_PATH" "$(DANFYSIK8000)/master/danfysikMps8000App/protocol/:$(DANFYSIK8000)/master/danfysikMps8000App/protocol/DFK$(DEV_TYPE=8000)/"
 
 cd ${TOP}
 
@@ -19,38 +16,5 @@ cd ${TOP}
 dbLoadDatabase "dbd/DFKPS-IOC-01.dbd"
 DFKPS_IOC_01_registerRecordDeviceDriver pdbbase
 
-##ISIS## Run IOC initialisation 
-< $(IOCSTARTUP)/init.cmd
-
-## use with emulator
-#drvAsynIPPortConfigure("L0", "localhost:xxxxx")
-
-## use with real device
-drvAsynSerialPortConfigure("L0", "$(PORT)", 0, 0, 0, 0)
-asynSetOption("L0", -1, "baud", "$(BAUD=9600)")
-asynSetOption("L0", -1, "bits", "$(BITS=8)")
-asynSetOption("L0", -1, "parity", "$(PARITY="none")")
-asynSetOption("L0", -1, "stop", "$(STOP=2)")
-
-## checks used for loading db files
-stringiftest  "POLAR"  "$(POLARITY="BIPOLAR")"  5  "BIPOLAR"
-stringiftest  "CALIB"  "$(CALIBRATED=1)"  5  "1"
-stringiftest  "SLEW"  "$(USE_SLEW=0)"  5  "1"
-
-## Load FileList
-## A seperate instance must be created for each danfysik
-epicsEnvSet "RAMP_DIR" "C:/Instrument/Settings"
-epicsEnvSet "RAMP_PAT" ".*"
-FileListConfigure("RAMPFILELIST1", $(RAMP_DIR), $(RAMP_PAT)) 
-
-##ISIS## Load common DB records 
-< $(IOCSTARTUP)/dbload.cmd
-
-## Load record instances
-dbLoadRecords("$(TOP)/db/DFKPS_common.db", "device=$(MYPVPREFIX)$(IOCNAME), P=$(MYPVPREFIX)$(IOCNAME):, FAC=$(FACTOR=1000), port=L0")
-$(IFPOLAR) dbLoadRecords("$(TOP)/db/DFKPS_polarity.db", "device=$(MYPVPREFIX)$(IOCNAME), P=$(MYPVPREFIX)$(IOCNAME):, port=L0")
-$(IFCALIB) dbLoadRecords("$(TOP)/db/DFKPS_calibrated.db", "device=$(MYPVPREFIX)$(IOCNAME), P=$(MYPVPREFIX)$(IOCNAME):, CALIB_FILE=$(CFILE=$(DEFAULT_CFILE)), CALIB_PATH=$(CPATH=$(DEFAULT_CPATH)),DRVHI=$(DRIVE_HIGH=5000000),DRVLO=$(DRIVE_LOW=-5000000), port=L0")
-$(IFSLEW) dbLoadRecords("$(TOP)/db/DFKPS_slew.db", "device=$(MYPVPREFIX)$(IOCNAME), P=$(MYPVPREFIX)$(IOCNAME):, port=L0")
-
-## Load device type specific st.cmd
-< iocBoot/iocDFKPS-IOC-01/st-$(DEV_TYPE=8000).cmd
+## Load common st.cmd
+< iocBoot/iocDFKPS-IOC-01/st-common.cmd
