5fb9684729e09c36ae74c93c1f577a004d851e6c
John-Holt-Tessella <John.Holt@tessella.com>
2016-08-12 11:28:53 +0100

    Use common calibartion dir

--- DFKPS/iocBoot/iocDFKPS-IOC-01/st-common.cmd
+++ DFKPS/iocBoot/iocDFKPS-IOC-01/st-common.cmd
@@ -1,6 +1,9 @@
 ## Environment Variables
-epicsEnvSet "DEFAULT_CFILE" "default_calib.dat"
-epicsEnvSet "DEFAULT_CPATH" "C:/Instrument/Settings/calib/danfysik/"
+
+epicsEnvSet "CALIB_BASE_DIR" "C:/Instrument/Settings/config/common"
+epicsEnvSet "CALIB_DIR" "magnets"
+epicsEnvSet "CALIB_FILE" "default_calib.dat"
+
 epicsEnvSet "STREAM_PROTOCOL_PATH" "$(DANFYSIK8000)/master/danfysikMps8000App/protocol/:$(DANFYSIK8000)/master/danfysikMps8000App/protocol/DFK$(DEV_TYPE=8000)/"
 
 ##ISIS## Run IOC initialisation 
@@ -25,9 +28,9 @@ stringiftest  "SLEW"  "$(USE_SLEW=0)"  5  "1"
 < $(IOCSTARTUP)/dbload.cmd
 
 ## Load record instances
-dbLoadRecords("$(TOP)/db/DFKPS_common.db", "device=$(MYPVPREFIX)$(IOCNAME), P=$(MYPVPREFIX)$(IOCNAME):, FAC=$(FACTOR=1000), port=L0")
+dbLoadRecords("$(TOP)/db/DFKPS_common.db", "device=$(MYPVPREFIX)$(IOCNAME), P=$(MYPVPREFIX)$(IOCNAME):, FAC=$(FACTOR=1000), port=L0,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0)")
 $(IFPOLAR) dbLoadRecords("$(TOP)/db/DFKPS_polarity.db", "device=$(MYPVPREFIX)$(IOCNAME), P=$(MYPVPREFIX)$(IOCNAME):, port=L0")
-$(IFCALIB) dbLoadRecords("$(TOP)/db/DFKPS_calibrated.db", "device=$(MYPVPREFIX)$(IOCNAME), P=$(MYPVPREFIX)$(IOCNAME):, CALIB_FILE=$(CFILE=$(DEFAULT_CFILE)), CALIB_PATH=$(CPATH=$(DEFAULT_CPATH)),DRVHI=$(DRIVE_HIGH=5000000),DRVLO=$(DRIVE_LOW=-5000000), port=L0")
+$(IFCALIB) dbLoadRecords("$(TOP)/db/DFKPS_calibrated.db", "device=$(MYPVPREFIX)$(IOCNAME), P=$(MYPVPREFIX)$(IOCNAME):, CALIB_BASE_DIR=$(CALIB_BASE_DIR),CALIB_DIR=$(CALIB_DIR),CALIB_FILE=$(CALIB_FILE),DRVHI=$(DRIVE_HIGH=5000000),DRVLO=$(DRIVE_LOW=-5000000), port=L0")
 $(IFSLEW) dbLoadRecords("$(TOP)/db/DFKPS_slew.db", "device=$(MYPVPREFIX)$(IOCNAME), P=$(MYPVPREFIX)$(IOCNAME):, port=L0")
 
 ## Load device type specific st.cmd
