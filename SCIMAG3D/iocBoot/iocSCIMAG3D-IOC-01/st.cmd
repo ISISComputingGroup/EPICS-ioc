f2698d001591d5e9e39d26b2a774c6ac0ef03782
John-Holt-Tessella <John.Holt@tessella.com>
2016-08-17 16:24:19 +0100

    Create 3D magent from scientific magnetics based on support module

---
+++ SCIMAG3D/iocBoot/iocSCIMAG3D-IOC-01/st.cmd
@@ -0,0 +1,44 @@
+#!../../bin/windows-x64/SCIMAG3D-IOC-01
+
+## You may have to change SCIMAG3D-IOC-01 to something else
+## everywhere it appears in this file
+
+# Increase this if you get <<TRUNCATED>> or discarded messages warnings in your errlog output
+errlogInit2(65536, 256)
+
+< envPaths
+
+cd ${TOP}
+
+## Register all support components
+dbLoadDatabase "dbd/SCIMAG3D-IOC-01.dbd"
+SCIMAG3D_IOC_01_registerRecordDeviceDriver pdbbase
+
+##ISIS## Run IOC initialisation 
+< $(IOCSTARTUP)/init.cmd
+
+lvDCOMConfigure("lvfp", "frontpanel", "$(MAGNET3D)/data/lv_controls.xml")
+## Load record instances
+
+
+##ISIS## Load common DB records 
+< $(IOCSTARTUP)/dbload.cmd
+
+################## Magnet 3D
+## Load our record instances
+dbLoadRecords("$(MAGNET3D)/db/controls.db","P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0)")
+dbLoadRecords("$(MAGNET3D)/db/combos.db","P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0)")
+dbLoadRecords("$(MAGNET3D)/db/unit_setter.db","P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0)")
+####################
+
+##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
+< $(IOCSTARTUP)/preiocinit.cmd
+
+cd ${TOP}/iocBoot/${IOC}
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=hgv27692Host"
+
+##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
+< $(IOCSTARTUP)/postiocinit.cmd
