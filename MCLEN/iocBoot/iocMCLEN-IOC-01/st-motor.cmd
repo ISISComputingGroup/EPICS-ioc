09876e4d49e8d63c44c382b3bf70c7ebce38d833
AdrianPotter <adrian.potter@tessella.com>
2016-08-23 09:51:26 +0100

    Use level 1 simulation mode, rather than trying to retrofit level 3

--- MCLEN/iocBoot/iocMCLEN-IOC-01/st-motor.cmd
+++ MCLEN/iocBoot/iocMCLEN-IOC-01/st-motor.cmd
@@ -7,7 +7,7 @@
 epicsEnvSet("ASERIAL", "serial$(PN)")
 epicsEnvSet("AMOTOR", "motor$(PN)")
 # Make sure controller number is 2 digits long
-calc("MTRCTRL", "$(MTRCTRL)", 2, 2)
+calc("MTRCTRL", "$(MTRCTRL=11)", 2, 2)
 epicsEnvSet("AMOTORNAME", "MTR$(MTRCTRL)0$(PN)")
 epicsEnvSet("AMOTORPV", "MOT:$(AMOTORNAME)")
 
@@ -16,7 +16,7 @@ set_pass0_restoreFile("$(IOCNAME)_$(PN)_built_settings.sav")
 set_pass1_restoreFile("$(IOCNAME)_$(PN)_built_settings.sav")
 
 $(IFSIM) motorSimCreateController("$(AMOTOR)", 1) 
-$(IFSIM) motorSimConfigAxis("$(AMOTOR)", 0, 32000, -32000,  0, 0) 
+$(IFSIM) motorSimConfigAxis("$(AMOTOR)", 0, 32000, -32000,  0, 0)
 $(IFSIM) drvAsynSerialPortConfigure("$(ASERIAL)", "NUL", 0, 1)
 
 $(IFNOTSIM) drvAsynSerialPortConfigure("$(ASERIAL)", "$(PORT$(PN)=NUL)", 0, 0, 0)
@@ -64,7 +64,7 @@ epicsEnvSet("LLMI",$(LLM$(PN)=-200))
 
 # Load asyn record 
 dbLoadRecords("$(ASYN)/db/asynRecord.db", "P=$(MYPVPREFIX),R=$(AMOTORPV):ASYN,PORT=$(ASERIAL),ADDR=0,OMAX=256,IMAX=256")
-dbLoadRecords("$(TOP)/db/motor.db", "P=$(MYPVPREFIX),M=$(AMOTORPV),PORT=$(AMOTOR),ADDR=0,VEL=$(VELI),ACC=$(ACCI),MRES=$(MRESI),ERES=$(ERESI),HLM=$(HLMI),LLM=$(LLMI),NAME=$(AMOTORNAME)") 
+dbLoadRecords("$(TOP)/db/motor.db", "P=$(MYPVPREFIX),M=$(AMOTORPV),VEL=$(VELI),ACC=$(ACCI),MRES=$(MRESI),ERES=$(ERESI),HLM=$(HLMI),LLM=$(LLMI),NAME=$(AMOTORNAME)") 
 dbLoadRecords("$(AXIS)/db/axis.db", "P=$(MYPVPREFIX),AXIS=$(IOCNAME):AXIS$(PN),mAXIS=$(AMOTORPV)") 
 
 autosaveBuild("$(IOCNAME)_$(PN)_built_settings.req", "_settings.req", 0)
