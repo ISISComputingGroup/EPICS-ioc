9b602f7abbfe76fda4c86269a8750190bb1b809b
KathrynBaker <kathryn.baker@stfc.ac.uk>
2016-09-15 17:04:05 +0100

    Changes to improve the display and interaction for current and voltage

--- DFKPS/iocBoot/iocDFKPS-IOC-01/st-8000.cmd
+++ DFKPS/iocBoot/iocDFKPS-IOC-01/st-8000.cmd
@@ -7,4 +7,4 @@ asynOctetConnect("DFKINIT","L0")
 asynOctetWrite DFKINIT "UNLOCK\r"
 
 ## Load our record instances
-dbLoadRecords("$(TOP)/Db/DFKPS_8000_status.db", "device=$(MYPVPREFIX)$(IOCNAME), P=$(MYPVPREFIX)$(IOCNAME):, port=L0, powpos=$(POWER_FLAG_POSITION), ilkpos=$(INTERLOCK_FLAG_POSITION), SP_PINI=$(SP_PINI)")
+dbLoadRecords("$(TOP)/Db/DFKPS_8000_status.db", "device=$(MYPVPREFIX)$(IOCNAME), P=$(MYPVPREFIX)$(IOCNAME):, port=L0, powpos=$(POWER_FLAG_POSITION), ilkpos=$(INTERLOCK_FLAG_POSITION), SP_PINI=$(SP_PINI), FRI=$(FACTOR_READ_I=1)")
--- DFKPS/iocBoot/iocDFKPS-IOC-01/st-8800.cmd
+++ DFKPS/iocBoot/iocDFKPS-IOC-01/st-8800.cmd
@@ -3,4 +3,4 @@ asynOctetConnect("DFKINIT","L0")
 asynOctetWrite DFKINIT "ADR 000\r"
 
 ## Load our record instances
-dbLoadRecords("$(TOP)/Db/DFKPS_8800_status.db", "device=$(MYPVPREFIX)$(IOCNAME), P=$(MYPVPREFIX)$(IOCNAME):, port=L0, SP_PINI=$(SP_PINI)")
+dbLoadRecords("$(TOP)/Db/DFKPS_8800_status.db", "device=$(MYPVPREFIX)$(IOCNAME), P=$(MYPVPREFIX)$(IOCNAME):, port=L0, SP_PINI=$(SP_PINI), FRI=$(FACTOR_READ_I=1)")
