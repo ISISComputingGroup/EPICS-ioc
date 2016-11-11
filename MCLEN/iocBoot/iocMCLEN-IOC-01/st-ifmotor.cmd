0812ffbebc74c3e47d9012fa7d12df507879e448
AdrianPotter <adrian.potter@tessella.com>
2016-11-11 09:41:21 +0000

    I think AXIS is a better name for the macro than MTR

--- MCLEN/iocBoot/iocMCLEN-IOC-01/st-ifmotor.cmd
+++ MCLEN/iocBoot/iocMCLEN-IOC-01/st-ifmotor.cmd
@@ -3,5 +3,5 @@
 ## if MN=1 the defines asyn port SD1 and uses macros such as VELO1, ACCL1 to configure 
 
 # this defines macros we can use for conditional loading later
-stringiftest("MTR$(MN)", "$(MTR$(MN)=)",5,"yes")
-$(IFMTR$(MN)) < st-motor.cmd
+stringiftest("AXIS$(MN)", "$(AXIS$(MN)=)",5,"yes")
+$(IFAXIS$(MN)) < st-motor.cmd
