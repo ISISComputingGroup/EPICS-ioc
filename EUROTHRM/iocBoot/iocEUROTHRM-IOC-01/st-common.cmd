cac4442da864e103b36c890bc6008a42ddd6051a
AdrianPotter <adrian.potter@tessella.com>
2016-12-09 11:39:07 +0000

    Use IOCNAME instead of IOCPVPREFIX: latter has been deleted

--- EUROTHRM/iocBoot/iocEUROTHRM-IOC-01/st-common.cmd
+++ EUROTHRM/iocBoot/iocEUROTHRM-IOC-01/st-common.cmd
@@ -18,7 +18,7 @@ asynSetOption("L0", -1, "stop", "$(STOP=1)")
 
 ## Load the sim and disable records
 ## These are loaded separately to allow one SIM and DISABLE to be used for ALL EUROTHRMs
-dbLoadRecords("$(TOP)/db/devSimDis.db","Q=$(MYPVPREFIX)$(IOCPVPREFIX):,DISABLE=$(DISABLE=0),RECSIM=$(RECSIM=0)")
+dbLoadRecords("$(TOP)/db/devSimDis.db","Q=$(MYPVPREFIX)$(IOCNAME):,DISABLE=$(DISABLE=0),RECSIM=$(RECSIM=0)")
 
 epicsEnvSet(EURO_NUM,1)
 < ${TOP}/iocBoot/iocEUROTHRM-IOC-01/st-euro.cmd
