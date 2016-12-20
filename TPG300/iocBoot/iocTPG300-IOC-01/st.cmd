af54a49aa978470d53ef5a12f0631e21c458a61a
AdrianPotter <adrian.potter@tessella.com>
2016-12-20 13:14:44 +0000

    Roll back previous merge

--- TPG300/iocBoot/iocTPG300-IOC-01/st.cmd
+++ TPG300/iocBoot/iocTPG300-IOC-01/st.cmd
@@ -6,9 +6,7 @@
 # Increase this if you get <<TRUNCATED>> or discarded messages warnings in your errlog output
 errlogInit2(65536, 256)
 
-< envPaths
-
-epicsEnvSet "STREAM_PROTOCOL_PATH" "$(TPG)/data"                                  
+< envPaths                             
 
 cd ${TOP}
 
@@ -16,31 +14,4 @@ cd ${TOP}
 dbLoadDatabase "dbd/TPG300-IOC-01.dbd"
 TPG300_IOC_01_registerRecordDeviceDriver pdbbase
 
-##ISIS## Run IOC initialisation 
-< $(IOCSTARTUP)/init.cmd
-
-drvAsynSerialPortConfigure("L0", "$(PORT)", 0, 0, 0, 0)
-asynSetOption("L0", -1, "baud", "9600")
-asynSetOption("L0", -1, "bits", "8")
-asynSetOption("L0", -1, "parity", "none")
-asynSetOption("L0", -1, "stop", "1")
-
-## Load record instances
-
-##ISIS## Load common DB records 
-< $(IOCSTARTUP)/dbload.cmd
-
-## Load our record instances
-dbLoadRecords("db/devTPG300.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0")
-
-##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
-< $(IOCSTARTUP)/preiocinit.cmd
-
-cd ${TOP}/iocBoot/${IOC}
-iocInit
-
-## Start any sequence programs
-#seq sncxxx,"user=mjc23Host"
-
-##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
-< $(IOCSTARTUP)/postiocinit.cmd
+< ${TOP}/iocBoot/${IOC}/st-common.cmd
