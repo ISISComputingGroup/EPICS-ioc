5e1d3663be17aee0b40bf15871a372c5416116d6
AdrianPotter <adrian.potter@tessella.com>
2016-12-22 10:24:48 +0000

    Factor out variables for easier read and tuning

--- EUROTHRM/iocBoot/iocEUROTHRM-IOC-01/st-timing.cmd
+++ EUROTHRM/iocBoot/iocEUROTHRM-IOC-01/st-timing.cmd
@@ -15,5 +15,8 @@ $(IFACTIVE) calc("NUM_SENSORS", "$(NUM_SENSORS)+1",1,1)
 stringiftest("ACTIVE", "$(ADDR_7=)")
 $(IFACTIVE) calc("NUM_SENSORS", "$(NUM_SENSORS)+1",1,1)
 
-dcalc("TREAD","$(NUM_SENSORS)*0.2",1,3)
-dcalc("TDLY","$(TREAD)/2",1,3)
+epicsEnvSet "MAX_RECORDS_PER_READ" 3
+epicsEnvSet "SECONDS_PER_READ" 0.06
+
+dcalc("TDLY","$(NUM_SENSORS)*$(SECONDS_PER_READ)",1,3)
+dcalc("TREAD","$(TDLY)*$(MAX_RECORDS_PER_READ)",1,3)
