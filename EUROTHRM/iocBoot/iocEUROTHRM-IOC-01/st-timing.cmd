5e8b36407105ac7b5eb903c41343e8515aebaac7
AdrianPotter <adrian.potter@tessella.com>
2016-12-22 10:27:59 +0000

    Previous value was too short. Led to timeout invalid on 6 sensor test

--- EUROTHRM/iocBoot/iocEUROTHRM-IOC-01/st-timing.cmd
+++ EUROTHRM/iocBoot/iocEUROTHRM-IOC-01/st-timing.cmd
@@ -16,9 +16,11 @@ stringiftest("ACTIVE", "$(ADDR_7=)")
 $(IFACTIVE) calc("NUM_SENSORS", "$(NUM_SENSORS)+1",1,1)
 
 epicsEnvSet "MAX_RECORDS_PER_READ" 3
+
 ## Edit this one if you want to tune the Eurotherm performance.
-## If it is too short, records will return TIMEOUT INVALID
-epicsEnvSet "SECONDS_PER_READ" 0.06
+## If it is too short, records will return TIMEOUT INVALID and the
+## OPI will report disconnections and appear unresponsive
+epicsEnvSet "SECONDS_PER_READ" 0.07
 
 dcalc("TDLY","$(NUM_SENSORS)*$(SECONDS_PER_READ)",1,3)
 dcalc("TREAD","$(TDLY)*$(MAX_RECORDS_PER_READ)",1,3)
