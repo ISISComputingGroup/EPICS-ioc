70569418406eb4609a550c629e12b4b3c567c3dd
Matt Clarke <matt.clarke@stfc.ac.uk>
2017-01-05 08:24:53 +0000

    Added freeport finder to julabo IOCs

---
+++ JULABO/iocBoot/iocJULABO-IOC-01/st-common.cmd
@@ -0,0 +1,37 @@
+epicsEnvSet "STREAM_PROTOCOL_PATH" "$(JULABO)/julaboApp/protocol"
+
+cd ${TOP}
+
+##ISIS## Run IOC initialisation 
+< $(IOCSTARTUP)/init.cmd
+
+## For emulator use:
+$(IFDEVSIM) freeIPPort("FREEPORT")  
+$(IFDEVSIM) epicsEnvShow("FREEPORT") 
+$(IFDEVSIM) drvAsynIPPortConfigure("L0", "localhost:$(FREEPORT)")
+
+## For real device use:
+$(IFNOTDEVSIM) drvAsynSerialPortConfigure("L0", "$(PORT)", 0, 0, 0, 0)
+$(IFNOTDEVSIM) asynSetOption("L0", -1, "baud", "4800")
+$(IFNOTDEVSIM) asynSetOption("L0", -1, "bits", "7")
+$(IFNOTDEVSIM) asynSetOption("L0", -1, "parity", "even")
+$(IFNOTDEVSIM) asynSetOption("L0", -1, "stop", "1")
+
+## Load record instances
+
+##ISIS## Load common DB records 
+< $(IOCSTARTUP)/dbload.cmd
+
+## Load our record instances
+dbLoadRecords("$(DB_FILE)","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0")
+
+##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
+< $(IOCSTARTUP)/preiocinit.cmd
+
+cd ${TOP}/iocBoot/${IOC}
+iocInit
+
+## Start any sequence programs
+
+##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
+< $(IOCSTARTUP)/postiocinit.cmd
--- JULABO/iocBoot/iocJULABO-IOC-02/st.cmd
+++ JULABO/iocBoot/iocJULABO-IOC-02/st.cmd
@@ -6,41 +6,17 @@
 # Increase this if you get <<TRUNCATED>> or discarded messages warnings in your errlog output
 errlogInit2(65536, 256)
 
+## Set the specific db file to use here because the command set can vary across models
+epicsEnvSet "DB_FILE" "db/FP50_MH.db"
+
 < envPaths
 
 cd ${TOP}
-
-epicsEnvSet "STREAM_PROTOCOL_PATH" "$(JULABO)/julaboApp/protocol"
 
 ## Register all support components
 dbLoadDatabase "dbd/JULABO-IOC-02.dbd"
 JULABO_IOC_02_registerRecordDeviceDriver pdbbase
 
-##ISIS## Run IOC initialisation 
-< $(IOCSTARTUP)/init.cmd
+cd ${TOP}/iocBoot/iocJULABO-IOC-01
 
-drvAsynSerialPortConfigure("L0", "$(PORT)", 0, 0, 0, 0)
-asynSetOption("L0", -1, "baud", "4800")
-asynSetOption("L0", -1, "bits", "7")
-asynSetOption("L0", -1, "parity", "even")
-asynSetOption("L0", -1, "stop", "1")
-
-## Load record instances
-
-##ISIS## Load common DB records 
-< $(IOCSTARTUP)/dbload.cmd
-
-## Load our record instances
-dbLoadRecords("db/FP50_MH.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0")
-
-##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
-< $(IOCSTARTUP)/preiocinit.cmd
-
-cd ${TOP}/iocBoot/${IOC}
-iocInit
-
-## Start any sequence programs
-#seq sncxxx,"user=mjc23Host"
-
-##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
-< $(IOCSTARTUP)/postiocinit.cmd
+< st-common.cmd
