5c069339d6eb7c404dea4899ffa3efd6020e02df
AdrianPotter <adrian.potter@tessella.com>
2017-01-10 13:57:55 +0000

    Add empty Keitherley 2400 IOC

---
+++ KEIT2400/iocBoot/iocKEIT2400-IOC-01/st-common.cmd
@@ -0,0 +1,33 @@
+epicsEnvSet "STREAM_PROTOCOL_PATH" "$(KEIT2400)/Keitherley2400Sup"
+epicsEnvSet "DEVICE" "L0"
+
+##ISIS## Run IOC initialisation
+< $(IOCSTARTUP)/init.cmd
+
+drvAsynSerialPortConfigure("$(DEVICE)", "$(PORT)", 0, 0, 0, 0)
+asynSetOption("$(DEVICE)", -1, "baud", "$(BAUD=19200)")
+asynSetOption("$(DEVICE)", -1, "bits", "$(BITS=8)")
+asynSetOption("$(DEVICE)", -1, "parity", "$(PARITY=none)")
+asynSetOption("$(DEVICE)", -1, "stop", "$(STOP=1)")
+asynOctetSetInputEos("$(DEVICE)", -1, "$(OEOS=\r)")
+asynOctetSetOutputEos("$(DEVICE)", -1, "$(IEOS=\r)")
+
+## Load record instances
+
+##ISIS## Load common DB records
+< $(IOCSTARTUP)/dbload.cmd
+
+## Load our record instances
+dbLoadRecords("db/KEIT2400.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=$(DEVICE), DISABLE=$(DISABLE=0),RECSIM=$(RECSIM=0)")
+
+##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called
+< $(IOCSTARTUP)/preiocinit.cmd
+
+cd ${TOP}/iocBoot/${IOC}
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=iew83206Host"
+
+##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs
+< $(IOCSTARTUP)/postiocinit.cmd
