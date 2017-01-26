62d295b099352a459b6c83f68c5681a3e30084d3
Dominic Oram <dominic.oram@stfc.ac.uk>
2017-01-26 17:37:52 +0000

    Added multiple IOCs

--- CCD100/iocBoot/iocCCD100-IOC-01/st.cmd
+++ CCD100/iocBoot/iocCCD100-IOC-01/st.cmd
@@ -8,43 +8,8 @@ errlogInit2(65536, 256)
 
 < envPaths
 
-epicsEnvSet "STREAM_PROTOCOL_PATH" "$(CCD100)/data"
-epicsEnvSet "TTY" "$(TTY=\\\\\\\\.\\\\COM6)" 
-
-cd ${TOP}
-
 ## Register all support components
-dbLoadDatabase "dbd/CCD100-IOC-01.dbd"
+dbLoadDatabase "${TOP}/dbd/CCD100-IOC-01.dbd"
 CCD100_IOC_01_registerRecordDeviceDriver pdbbase
 
-##ISIS## Run IOC initialisation 
-< $(IOCSTARTUP)/init.cmd
-
-drvAsynSerialPortConfigure("L0", "$(TTY)", 0, 0, 0, 0)                        
-asynSetOption("L0", -1, "baud", "57600")                                   
-asynSetOption("L0", -1, "bits", "8")                                           
-asynSetOption("L0", -1, "parity", "none")                                      
-asynSetOption("L0", -1, "stop", "1")                                            
-asynOctetSetInputEos("L0", -1, "\r\n")                                         
-asynOctetSetOutputEos("L0", -1, "\r\n") 
-
-## Load record instances
-
-##ISIS## Load common DB records 
-< $(IOCSTARTUP)/dbload.cmd
-
-## Load our record instances
-dbLoadRecords("db/CCD100.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0")
-dbLoadRecords("db/unit_setter.db","P=$(MYPVPREFIX)$(IOCNAME):")
-
-##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
-< $(IOCSTARTUP)/preiocinit.cmd
-
-cd ${TOP}/iocBoot/${IOC}
-iocInit
-
-## Start any sequence programs
-#seq sncxxx,"user=ffv81422Host"
-
-##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
-< $(IOCSTARTUP)/postiocinit.cmd
+< st-common.cmd
