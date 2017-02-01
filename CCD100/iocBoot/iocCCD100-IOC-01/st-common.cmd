b9a3ada86df8b92571eb96a256eb9ca997f7486d
Dominic Oram <dominic.oram@stfc.ac.uk>
2017-02-01 11:29:29 +0000

    Passed through RECSIM and DEVSIM

--- CCD100/iocBoot/iocCCD100-IOC-01/st-common.cmd
+++ CCD100/iocBoot/iocCCD100-IOC-01/st-common.cmd
@@ -22,7 +22,7 @@ $(IFDEVSIM) drvAsynIPPortConfigure("L0", "localhost:$(FREEPORT=0)")
 < $(IOCSTARTUP)/dbload.cmd
 
 ## Load our record instances
-dbLoadRecords("${TOP}/db/CCD100.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0, DEV_NAME=$(DEV_NAME=)")
+dbLoadRecords("${TOP}/db/CCD100.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0, DEV_NAME=$(DEV_NAME=), RECSIM=$(RECSIM), DEVSIM=$(DEVSIM)")
 dbLoadRecords("${TOP}/db/unit_setter.db","P=$(MYPVPREFIX)$(IOCNAME):")
 
 ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
