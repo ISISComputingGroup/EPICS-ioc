22bf4822720121ff94b334d51a3a5a0cf7425a65
AdrianPotter <adrian.potter@tessella.com>
2017-03-29 13:00:20 +0100

    Add basic IOC template for MK2 chopper

---
+++ MK2CHOPR/iocBoot/MK2CHOPR-IOC-01/st.cmd
@@ -0,0 +1,50 @@
+#!../../bin/windows-x64/MK2CHOPR-IOC-01
+
+## You may have to change MK2CHOPR-IOC-01 to something else
+## everywhere it appears in this file
+
+# Increase this if you get <<TRUNCATED>> or discarded messages warnings in your errlog output
+errlogInit2(65536, 256)
+
+< envPaths
+
+epicsEnvSet "STREAM_PROTOCOL_PATH" "$(MK2CHOPR)/mk2chopperSup"
+epicsEnvSet "DEVICE" "L0"
+
+cd ${TOP}
+
+## Register all support components
+dbLoadDatabase "dbd/MK2CHOPR-IOC-01.dbd"
+MK2CHOPR_IOC_01_registerRecordDeviceDriver pdbbase
+
+##ISIS## Run IOC initialisation
+< $(IOCSTARTUP)/init.cmd
+
+drvAsynSerialPortConfigure("$(DEVICE)", "$(PORT)", 0, 0, 0, 0)
+asynSetOption("$(DEVICE)", -1, "baud", "$(BAUD=9600)")
+asynSetOption("$(DEVICE)", -1, "bits", "$(BITS=7)")
+asynSetOption("$(DEVICE)", -1, "parity", "$(PARITY=even)")
+asynSetOption("$(DEVICE)", -1, "stop", "$(STOP=1)")
+asynOctetSetInputEos("$(DEVICE)", -1, "$(OEOS=\r)")
+asynOctetSetOutputEos("$(DEVICE)", -1, "$(IEOS=\r)")
+
+## Load record instances
+
+##ISIS## Load common DB records
+< $(IOCSTARTUP)/dbload.cmd
+
+## Load our record instances
+dbLoadRecords("db/MK2CHOPR.db","P=$(MYPVPREFIX)$(IOCNAME):, port=$(DEVICE)")
+#dbLoadRecords("db/xxx.db","user=iew83206Host")
+
+##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called
+< $(IOCSTARTUP)/preiocinit.cmd
+
+cd ${TOP}/iocBoot/${IOC}
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=iew83206Host"
+
+##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs
+< $(IOCSTARTUP)/postiocinit.cmd
