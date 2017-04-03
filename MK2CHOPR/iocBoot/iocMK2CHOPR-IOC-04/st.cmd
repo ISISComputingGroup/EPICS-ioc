43aa0fc98364f8e4640b537c8f92b1e108964e80
AdrianPotter <adrian.potter@tessella.com>
2017-04-03 16:02:44 +0100

    Factor out common startup. Fix sim/dis behaviour

--- MK2CHOPR/iocBoot/iocMK2CHOPR-IOC-04/st.cmd
+++ MK2CHOPR/iocBoot/iocMK2CHOPR-IOC-04/st.cmd
@@ -8,44 +8,8 @@ errlogInit2(65536, 256)
 
 < envPaths
 
-epicsEnvSet "STREAM_PROTOCOL_PATH" "$(MK2CHOPR)/data"
-epicsEnvSet "DEVICE" "L0"
-
-cd ${TOP}
-
 ## Register all support components
-dbLoadDatabase "dbd/MK2CHOPR-IOC-04.dbd"
+dbLoadDatabase "${TOP}/dbd/MK2CHOPR-IOC-04.dbd"
 MK2CHOPR_IOC_04_registerRecordDeviceDriver pdbbase
 
-##ISIS## Run IOC initialisation
-< $(IOCSTARTUP)/init.cmd
-
-## For emulator use:
-$(IFDEVSIM) freeIPPort("FREEPORT")  
-$(IFDEVSIM) epicsEnvShow("FREEPORT") 
-$(IFDEVSIM) drvAsynIPPortConfigure("$(DEVICE)", "localhost:$(FREEPORT=0)")
-
-$(IFNOTDEVSIM) drvAsynSerialPortConfigure("$(DEVICE)", "$(PORT)", 0, 0, 0, 0)
-$(IFNOTDEVSIM) asynSetOption("$(DEVICE)", -1, "baud", "$(BAUD=9600)")
-$(IFNOTDEVSIM) asynSetOption("$(DEVICE)", -1, "bits", "$(BITS=7)")
-$(IFNOTDEVSIM) asynSetOption("$(DEVICE)", -1, "parity", "$(PARITY=even)")
-$(IFNOTDEVSIM) asynSetOption("$(DEVICE)", -1, "stop", "$(STOP=1)")
-$(IFNOTDEVSIM) asynOctetSetInputEos("$(DEVICE)", -1, "$(OEOS=\r)")
-$(IFNOTDEVSIM) asynOctetSetOutputEos("$(DEVICE)", -1, "$(IEOS=\r)")
-
-## Load record instances
-
-##ISIS## Load common DB records
-< $(IOCSTARTUP)/dbload.cmd
-
-## Load our record instances
-dbLoadRecords("db/MK2CHOPR.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=$(DEVICE)")
-
-##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called
-< $(IOCSTARTUP)/preiocinit.cmd
-
-cd ${TOP}/iocBoot/${IOC}
-iocInit
-
-##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs
-< $(IOCSTARTUP)/postiocinit.cmd
+< ../iocMK2CHOPR-IOC-01/st-common.cmd
