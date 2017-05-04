b2c098ba17e7bd9d8367bd79ad28734616c90d13
John-Holt-Tessella <John.Holt@tessella.com>
2017-05-04 15:24:18 +0100

    Create blank IOC

---
+++ AMINT2L/iocBoot/iocAMINT2L-IOC-01/st.cmd
@@ -0,0 +1,13 @@
+#!../../bin/windows-x64/AMINT2L-IOC-01
+
+## You may have to change AMINT2L to something else
+## everywhere it appears in this file
+
+< envPaths
+
+## Register all support components
+dbLoadDatabase "${TOP}/dbd/AMINT2L-IOC-01.dbd"
+AMINT2L_IOC_01_registerRecordDeviceDriver pdbbase
+
+## calling common command file in ioc 01 boot dir
+< ${TOP}/iocAMINT2L-IOC-01/st-common.cmd
