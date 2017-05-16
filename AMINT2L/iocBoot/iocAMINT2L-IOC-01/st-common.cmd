519dd33fd271c1b3f5102762aaa5efc05075ab1a
John-Holt-Tessella <John.Holt@tessella.com>
2017-05-16 14:21:32 +0100

    Move db file to support modules and add calc support

--- AMINT2L/iocBoot/iocAMINT2L-IOC-01/st-common.cmd
+++ AMINT2L/iocBoot/iocAMINT2L-IOC-01/st-common.cmd
@@ -24,7 +24,7 @@ $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0", -1, "stop", "$(STOP=1)")
 ## Load our record instances
 ############################
 
-dbLoadRecords("$(AMINT2L)/db/devAMINT2L.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0, RECSIM=$(RECSIM=0), DISABLE=$(DISABLE=0), ADDR=$(ADDR)")
+dbLoadRecords("$(AMINT2L)/db/amint2l.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0, RECSIM=$(RECSIM=0), DISABLE=$(DISABLE=0), ADDR=$(ADDR)")
 
 ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
 < $(IOCSTARTUP)/preiocinit.cmd
