3160eec4af9d6ebae5eca2bb2048bdbf7d22dc10
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-06-23 16:59:02 +0100

    Undo erroneously committed file

--- ROTSC/iocBoot/iocROTSC-IOC-01/st.cmd
+++ ROTSC/iocBoot/iocROTSC-IOC-01/st.cmd
@@ -22,7 +22,7 @@ epicsEnvSet("DEVICE", "L0")
 
 $(IFDEVSIM) freeIPPort("FREEPORT")  
 $(IFDEVSIM) epicsEnvShow("FREEPORT") 
-$(IFDEVSIM) drvAsynIPPortConfigure("$(DEVICE)", "localhost:57677")
+$(IFDEVSIM) drvAsynIPPortConfigure("$(DEVICE)", "localhost:$(FREEPORT=0)")
 
 $(IFNOTDEVSIM) drvAsynSerialPortConfigure("$(DEVICE)", "$(PORT)", 0, 0, 0, 0)
 $(IFNOTDEVSIM) asynSetOption("$(DEVICE)", -1, "baud", "$(BAUD=38400)")
