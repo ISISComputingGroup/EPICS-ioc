c164df1429874551125c0c5e8e98c6fc7c743d22
Matt Clarke <matt.clarke@stfc.ac.uk>
2017-08-03 15:51:53 +0100

    Adjustments to st.cmd for TDK

--- TDK_LAMBDA_GENESYS/iocBoot/iocGENESYS-IOC-01/st-psu.cmd
+++ TDK_LAMBDA_GENESYS/iocBoot/iocGENESYS-IOC-01/st-psu.cmd
@@ -5,19 +5,20 @@
 # this defines macros we can use for conditional loading later
 stringiftest("PORT", "$(PORT$(PS)=)")
 
-# create a real serial port, unless in simulation mode then create an unconnected asyn port 
-$(IFPORT)$(IFRECSIM)    drvAsynSerialPortConfigure ("L$(PS)", "NUL", 0, 1)
 
 ## For unit testing:
-$(IFDEVSIM) drvAsynIPPortConfigure("L$(PS)", "localhost:$(EMULATOR_PORT=)")
+$(IFPORT) $(IFDEVSIM) drvAsynIPPortConfigure("L$(PS)", "localhost:$(EMULATOR_PORT=)")
+
+## For recsim:
+$(IFPORT) $(IFRECSIM) drvAsynSerialPortConfigure ("L$(PS)", "$(PORT=NUL)", 0, 1, 0, 0)
 
 $(IFPORT) $(IFNOTDEVSIM) $(IFNOTRECSIM) drvAsynSerialPortConfigure ("L$(PS)", "$(PORT$(PS)=)")
 $(IFPORT) $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption ("L$(PS)", 0, "baud", "$(BAUD$(PS)=9600)")
 $(IFPORT) $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption ("L$(PS)", 0, "bits", "$(BITS$(PS)=8)")
 $(IFPORT) $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption ("L$(PS)", 0, "parity", "$(PARITY$(PS)=none)")
 $(IFPORT) $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption ("L$(PS)", 0, "stop", "$(STOP$(PS)=1)")
-$(IFPORT) $(IFNOTDEVSIM) $(IFNOTRECSIM) asynOctetSetInputEos("L$(PS)",0,"$(IEOS$(PS)=\\r)")
-$(IFPORT) $(IFNOTDEVSIM) $(IFNOTRECSIM) asynOctetSetOutputEos("L$(PS)",0,"$(OEOS$(PS)=\\r)")
+$(IFPORT) $(IFNOTRECSIM) asynOctetSetInputEos("L$(PS)",0,"$(IEOS$(PS)=\\r)")
+$(IFPORT) $(IFNOTRECSIM) asynOctetSetOutputEos("L$(PS)",0,"$(OEOS$(PS)=\\r)")
 
 ## Initialise the comms with the PSU
 $(IFPORT) $(IFNOTDEVSIM) $(IFNOTRECSIM) asynOctetConnect("GENESYS_01$(PS)","L$(PS)")
