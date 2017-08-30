eb718054d0d8d0a8c84b5984fff9e51ae1b3e490
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-08-30 09:52:55 +0100

    makeBaseApp.pl

---
+++ EGXCOLIM/iocBoot/iocEGXCOLIM/st.cmd
@@ -0,0 +1,38 @@
+#!../../bin/windows-x64/EGXCOLIM
+
+## You may have to change EGXCOLIM to something else
+## everywhere it appears in this file
+
+# Increase this if you get <<TRUNCATED>> or discarded messages warnings in your errlog output
+errlogInit2(65536, 256)
+
+< envPaths
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/EGXCOLIM.dbd"
+EGXCOLIM_registerRecordDeviceDriver pdbbase
+
+##ISIS## Run IOC initialisation 
+< $(IOCSTARTUP)/init.cmd
+
+## Load record instances
+
+##ISIS## Load common DB records 
+< $(IOCSTARTUP)/dbload.cmd
+
+## Load our record instances
+#dbLoadRecords("db/xxx.db","user=ynq66733")
+
+##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
+< $(IOCSTARTUP)/preiocinit.cmd
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=ynq66733"
+
+##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
+< $(IOCSTARTUP)/postiocinit.cmd
