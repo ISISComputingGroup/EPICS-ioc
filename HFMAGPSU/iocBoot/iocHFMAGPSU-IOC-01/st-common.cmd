7f1469224884b7b670f638ea3f976082e6033177
esouthren <eilidhsouthren@gmail.com>
2017-11-07 09:59:15 +0000

    fixed line terminators for hardware communication

--- HFMAGPSU/iocBoot/iocHFMAGPSU-IOC-01/st-common.cmd
+++ HFMAGPSU/iocBoot/iocHFMAGPSU-IOC-01/st-common.cmd
@@ -9,9 +9,12 @@ $(IFDEVSIM) drvAsynIPPortConfigure("L0", "localhost:$(EMULATOR_PORT=)")
 ## For real device use:
 $(IFNOTDEVSIM) $(IFNOTRECSIM) drvAsynSerialPortConfigure("L0", "$(PORT=)", 0, 0, 0, 0)
 $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0", -1, "baud", "$(BAUD=9600)")
-$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0", -1, "bits", "$(BITS=7)")
-$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0", -1, "parity", "$(PARITY=even)")
+$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0", -1, "bits", "$(BITS=8)")
+$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0", -1, "parity", "$(PARITY=none)")
 $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0", -1, "stop", "$(STOP=1)")
+
+$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetTraceMask("L0", -1, 0x9)
+$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetTraceIOMask("L0", -1, 0x2)
 
 ##ISIS## Load common DB records 
 < $(IOCSTARTUP)/dbload.cmd
