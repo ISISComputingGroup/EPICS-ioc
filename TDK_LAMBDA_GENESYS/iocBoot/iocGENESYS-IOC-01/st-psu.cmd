105c00b6365995142d2ebe7c93508302522f56be
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2017-11-12 21:56:36 +0000

    Add alarm severity and high/low alarm levels

--- TDK_LAMBDA_GENESYS/iocBoot/iocGENESYS-IOC-01/st-psu.cmd
+++ TDK_LAMBDA_GENESYS/iocBoot/iocGENESYS-IOC-01/st-psu.cmd
@@ -21,6 +21,7 @@ $(IFPORT) $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption ("L$(PS)", 0, "crtscts", "
 $(IFPORT) $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption ("L$(PS)", 0, "clocal", "D")
 $(IFPORT) $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption ("L$(PS)", 0, "ixon", "N")
 $(IFPORT) $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption ("L$(PS)", 0, "ixoff", "N")
+
 $(IFPORT) $(IFNOTRECSIM) asynOctetSetInputEos("L$(PS)",0,"$(IEOS$(PS)=\\r)")
 $(IFPORT) $(IFNOTRECSIM) asynOctetSetOutputEos("L$(PS)",0,"$(OEOS$(PS)=\\r)")
 
