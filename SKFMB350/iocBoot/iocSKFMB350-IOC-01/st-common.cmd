6c4baf5d0e2a50b4f7c64192128b3b744342d5a1
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-11-16 11:46:10 +0000

    Make both tests and real device work. Add addressing.

--- SKFMB350/iocBoot/iocSKFMB350-IOC-01/st-common.cmd
+++ SKFMB350/iocBoot/iocSKFMB350-IOC-01/st-common.cmd
@@ -18,12 +18,16 @@ $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0", -1, "stop", "$(STOP=2)")
 $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0", -1, "break_duration", "20")
 $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0", -1, "break_delay", "20")
 
+# Need to set these for DEVSIM mode as lewis can't handle not having termination characters.
+$(IFDEVSIM) asynOctetSetOutputEos("L0",0,"\r\n") # Set it to CRLF and hope it is unlikely to ever happen "by accident" in the binary data
+$(IFDEVSIM) asynOctetSetInputEos("L0",0,"\r\n")
+
 ##ISIS## Load common DB records 
 < $(IOCSTARTUP)/dbload.cmd
 
 ## Load our record instances
 dbLoadRecords("db/simdis.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0, RECSIM=$(RECSIM=0), DISABLE=$(DISABLE=0)")
-dbLoadRecords("db/chopper.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0, RECSIM=$(RECSIM=0), DISABLE=$(DISABLE=0),PARKPOS=$(PARKPOS=1),PARKOPEN=$(PARKOPEN=0),PARKCLOSED=$(PARKCLOSED=0)")
+dbLoadRecords("db/chopper.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0, RECSIM=$(RECSIM=0), DISABLE=$(DISABLE=0),ADDRESS=$(ADDRESS=1),PARKPOS=$(PARKPOS=1),PARKOPEN=$(PARKOPEN=0),PARKCLOSED=$(PARKCLOSED=0)")
 dbLoadRecords("db/interlocks.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0, RECSIM=$(RECSIM=0), DISABLE=$(DISABLE=0)")
 dbLoadRecords("db/status.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0, RECSIM=$(RECSIM=0), DISABLE=$(DISABLE=0)")
 
