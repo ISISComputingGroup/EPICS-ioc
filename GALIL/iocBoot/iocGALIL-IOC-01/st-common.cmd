8a4bcff1a5e0489a8fd5653b41cacdb90db35fdb
Dominic Oram <dominic.oram@stfc.ac.uk>
2017-11-17 16:52:05 +0000

    Changed to new way of doing motors moving/stop all

--- GALIL/iocBoot/iocGALIL-IOC-01/st-common.cmd
+++ GALIL/iocBoot/iocGALIL-IOC-01/st-common.cmd
@@ -58,7 +58,7 @@ $(IFNOTTESTDEVSIM) < $(GALILCONFIG)/motorExtensions.cmd
 $(IFTESTDEVSIM) < $(MOTOREXT)/settings/motorExtensions.cmd
 
 ## motor util package
-dbLoadRecords("$(MOTOR)/db/motorUtil.db","P=$(MYPVPREFIX)$(IOCNAME):,PVPREFIX=$(MYPVPREFIX),IFDMC01=$(IFDMC01),IFDMC02=$(IFDMC02),IFDMC03=$(IFDMC03),IFDMC04=$(IFDMC04),IFDMC05=$(IFDMC05),IFDMC06=$(IFDMC06),IFDMC07=$(IFDMC07),IFDMC08=$(IFDMC08),IFDMC09=$(IFDMC09),IFDMC10=$(IFDMC10)")
+dbLoadRecords("$(MOTOR)/db/motorUtil.db","P=$(MYPVPREFIX)$(IOCNAME):,$(IFIOC)= ,PVPREFIX=$(MYPVPREFIX)")
 
 ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
 < $(IOCSTARTUP)/preiocinit.cmd
