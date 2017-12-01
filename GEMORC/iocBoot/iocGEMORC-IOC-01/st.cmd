2e6d17ae61cbd60cc62e5d9af807a570419f8380
Adrian Potter <Adrian.Potter@tessella.com>
2017-12-01 17:07:01 +0000

    Add initialisation logic

--- GEMORC/iocBoot/iocGEMORC-IOC-01/st.cmd
+++ GEMORC/iocBoot/iocGEMORC-IOC-01/st.cmd
@@ -20,8 +20,21 @@ GEMORC_IOC_01_registerRecordDeviceDriver pdbbase
 ##ISIS## Run IOC initialisation 
 < $(IOCSTARTUP)/init.cmd
 
+## For unit testing:
+$(IFTESTDEVSIM) drvAsynIPPortConfigure("$(DEVICE)", "localhost:$(EMULATOR_PORT=)")
+
 ## For normal devsim:
-drvAsynIPPortConfigure("$(DEVICE)", "localhost:57677")
+$(IFNOTTESTDEVSIM) $(IFDEVSIM) drvAsynIPPortConfigure("$(DEVICE)", "localhost:57677")
+
+## For recsim:
+$(IFRECSIM) drvAsynSerialPortConfigure("$(DEVICE)", "$(PORT=NUL)", 0, 1, 0, 0)
+
+## For real device:
+$(IFNOTDEVSIM) $(IFNOTRECSIM) drvAsynSerialPortConfigure("$(DEVICE)", "$(PORT=NO_PORT_MACRO)", 0, 0, 0, 0)
+$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)", -1, "baud", "$(BAUD=9600)")
+$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)", -1, "bits", "$(BITS=8)")
+$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)", -1, "parity", "$(PARITY=none)")
+$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)", -1, "stop", "$(STOP=1)")
 
 ## Load record instances
 
