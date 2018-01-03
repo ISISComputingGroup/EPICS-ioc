d31ed10272dd1f419deec04528bd985e931d7768
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2018-01-03 17:51:14 +0000

    Enable flow control

--- SCHNDR/iocBoot/iocSCHNDR-IOC-01/st-common.cmd
+++ SCHNDR/iocBoot/iocSCHNDR-IOC-01/st-common.cmd
@@ -1,38 +1,35 @@
 ##ISIS## Run IOC initialisation 
 < $(IOCSTARTUP)/init.cmd
 
-# TCP
+## TCP
 #drvAsynIPPortConfigure("$(DEVICE)","$(IPADDR=130.246.48.127):$(IPPORT=502)",0,0,1)
 #modbusInterposeConfig("$(DEVICE)",0,5000,0)
 
-# RTU
-#drvAsynSerialPortConfigure("$(DEVICE)","COM1",0,0,0)
+## RTU
 drvAsynSerialPortConfigure("$(DEVICE)","COM34",0,0,0)
 asynSetOption("$(DEVICE)",0,"baud","9600")
-#asynSetOption("$(DEVICE)",0,"baud","19200")
 asynSetOption("$(DEVICE)",0,"bits","8")
 asynSetOption("$(DEVICE)",0,"parity","even")
 asynSetOption("$(DEVICE)",0,"stop","1")
-asynSetOption("$(DEVICE)",0,"clocal","Y")
+asynSetOption("$(DEVICE)",0,"clocal","N") # N enables DSR/DTR handshaking
 asynSetOption("$(DEVICE)",0,"crtscts","N")
-asynSetOption("$(DEVICE)",0,"ixon","N")
-asynSetOption("$(DEVICE)",0,"ixoff","N")
-modbusInterposeConfig("$(DEVICE)",1,5000,2)
+asynSetOption("$(DEVICE)",0,"ixon","Y")
+asynSetOption("$(DEVICE)",0,"ixoff","Y")
+modbusInterposeConfig("$(DEVICE)",1,3000,2)
 
-# for ASCII but not used - use RTU instead
+## ASCII, but use RTU if possible
 #drvAsynSerialPortConfigure("$(DEVICE)","COM34",0,0,0)
 #asynSetOption("$(DEVICE)",0,"baud","2400")
 #asynSetOption("$(DEVICE)",0,"bits","7")
 #asynSetOption("$(DEVICE)",0,"parity","even")
 #asynSetOption("$(DEVICE)",0,"stop","1")
-#asynSetOption("$(DEVICE)",0,"clocal","Y")
+#asynSetOption("$(DEVICE)",0,"clocal","N") # N enables DSR/DTR handshaking
 #asynSetOption("$(DEVICE)",0,"crtscts","N")
 #asynSetOption("$(DEVICE)",0,"ixon","Y")
 #asynSetOption("$(DEVICE)",0,"ixoff","Y")
 #asynOctetSetOutputEos("$(DEVICE)",0,"\r\n")
 #asynOctetSetInputEos("$(DEVICE)",0,"\r\n")
-#modbusInterposeConfig("$(DEVICE)",2,5000,2)
-
+#modbusInterposeConfig("$(DEVICE)",2,3000,2)
 
 # link type is 0 for tcp, 1 for RTU. 2 for ASCII
 #modbusInterposeConfig(const char *portName,
@@ -48,8 +45,8 @@ modbusInterposeConfig("$(DEVICE)",1,5000,2)
 
 ## a readback of > 0 on a write (code 16) configure line below means it does a 
 ## single initial read of the value on IOC startup. 
-## 3rd arg is slave address, check on PLC for this. Deosn;rt seem inportant for TCP, but importanf for serial
-drvModbusAsynConfigure("$(DEVICE)test", "$(DEVICE)", 1, 3, 2, 1, 0, 1000, "PLC") # or 4
+## 3rd arg is PLC slave address, check on PLC for this. Doesn't seem important for TCP, but is for RTU/ASCII
+drvModbusAsynConfigure("$(DEVICE)test", "$(DEVICE)", 1, 3, 2, 1, 0, 500, "PLC") # or 4
 
 ## Load record instances
 
