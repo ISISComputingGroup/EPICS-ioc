0ac02a7cf6348303055c495c6d8ff50b78f761ee
Tom Willemsen <tomwillemsen1995@gmail.com>
2018-01-05 13:35:25 +0000

    Add coordinator skeleton

---
+++ COORD/iocBoot/iocCOORD-IOC-01/st.cmd
@@ -0,0 +1,30 @@
+#!../../bin/windows-x64/COORD-IOC-01
+
+# Increase this if you get <<TRUNCATED>> or discarded messages warnings in your errlog output
+errlogInit2(65536, 256)
+
+< envPaths
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/COORD-IOC-01.dbd"
+COORD_IOC_01_registerRecordDeviceDriver pdbbase
+
+##ISIS## Run IOC initialisation 
+< $(IOCSTARTUP)/init.cmd
+
+##ISIS## Load common DB records 
+< $(IOCSTARTUP)/dbload.cmd
+
+##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
+< $(IOCSTARTUP)/preiocinit.cmd
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
+## Start any sequence programs
+seq coord
+
+##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
+< $(IOCSTARTUP)/postiocinit.cmd
