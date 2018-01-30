63395ea93684395d9c607bc854a4dddeb19c6d8d
esouthren <eilidhsouthren@gmail.com>
2018-01-30 14:48:58 +0000

    ioc running smoothly - before starting to work on emulator

--- KHLY2700/iocBoot/iocKHLY2700-IOC-02/st.cmd
+++ KHLY2700/iocBoot/iocKHLY2700-IOC-02/st.cmd
@@ -1,38 +1,13 @@
 #!../../bin/windows-x64/KHLY2700-IOC-02
 
-## You may have to change KHLY2700-IOC-02 to something else
+## You may have to change KHLY2700-IOC-1 to something else
 ## everywhere it appears in this file
-
-# Increase this if you get <<TRUNCATED>> or discarded messages warnings in your errlog output
-errlogInit2(65536, 256)
 
 < envPaths
 
-cd "${TOP}"
-
 ## Register all support components
-dbLoadDatabase "dbd/KHLY2700-IOC-02.dbd"
+dbLoadDatabase "${TOP}/dbd/KHLY2700-IOC-02.dbd"
 KHLY2700_IOC_02_registerRecordDeviceDriver pdbbase
 
-##ISIS## Run IOC initialisation 
-< $(IOCSTARTUP)/init.cmd
-
-## Load record instances
-
-##ISIS## Load common DB records 
-< $(IOCSTARTUP)/dbload.cmd
-
-## Load our record instances
-#dbLoadRecords("db/xxx.db","user=zyz29835")
-
-##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
-< $(IOCSTARTUP)/preiocinit.cmd
-
-cd "${TOP}/iocBoot/${IOC}"
-iocInit
-
-## Start any sequence programs
-#seq sncxxx,"user=zyz29835"
-
-##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
-< $(IOCSTARTUP)/postiocinit.cmd
+## calling common command file in ioc 01 boot dir
+< ../iocKHLY2700-IOC-01/st-common.cmd
