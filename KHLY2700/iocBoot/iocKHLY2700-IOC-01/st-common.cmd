713f018ad1fb21b883978f4741dc2ac1fb61c694
esouthren <eilidhsouthren@gmail.com>
2018-02-09 15:03:47 +0000

    buffer fill/read functioning

--- KHLY2700/iocBoot/iocKHLY2700-IOC-01/st-common.cmd
+++ KHLY2700/iocBoot/iocKHLY2700-IOC-01/st-common.cmd
@@ -19,6 +19,11 @@ $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0", -1, "stop", "$(STOP=1)")
 ## Load our record instances
 dbLoadRecords("${TOP}/db/keithley2700.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0, RECSIM=$(RECSIM=0), DISABLE=$(DISABLE=0)")
 
+## For debugging:
+asynSetTraceMask("L0",-1,0x9) 
+asynSetTraceIOMask("L0",-1,0x2)
+
+
 ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
 < $(IOCSTARTUP)/preiocinit.cmd
 
