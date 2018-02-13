c2f368635213bbdd6aeb31fec269822035100d0e
Adrian Potter <Adrian.Potter@tessella.com>
2018-02-13 15:35:31 +0000

    Refactor startup into shared and distinct

--- ILM200/iocBoot/iocILM200-IOC-02/st.cmd
+++ ILM200/iocBoot/iocILM200-IOC-02/st.cmd
@@ -1,57 +1,13 @@
 #!../../bin/windows-x64/ILM200-IOC-02
 
-## You may have to change ILM200-IOC-02 to something else
+## You may have to change ILM200 to something else
 ## everywhere it appears in this file
-
-# Increase this if you get <<TRUNCATED>> or discarded messages warnings in your errlog output
-errlogInit2(65536, 256)
 
 < envPaths
 
-epicsEnvSet "STREAM_PROTOCOL_PATH" "$(ILM200)/data"
-epicsEnvSet "DEVICE" "L0"
-
-cd "${TOP}"
-
 ## Register all support components
-dbLoadDatabase "dbd/ILM200-IOC-02.dbd"
+dbLoadDatabase "${TOP}/dbd/ILM200-IOC-02.dbd"
 ILM200_IOC_02_registerRecordDeviceDriver pdbbase
 
-##ISIS## Run IOC initialisation 
-< $(IOCSTARTUP)/init.cmd
-
-## For testing with emulator:
-$(IFDEVSIM) drvAsynIPPortConfigure("$(DEVICE)", "localhost:$(EMULATOR_PORT=57677)")
-
-## For recsim:
-$(IFRECSIM) drvAsynSerialPortConfigure("$(DEVICE)", "$(PORT=NUL)", 0, 1, 0, 0)
-
-## For real device:
-$(IFNOTDEVSIM) $(IFNOTRECSIM) drvAsynSerialPortConfigure("$(DEVICE)", "$(PORT=NO_PORT_MACRO)", 0, 0, 0, 0)
-$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)", -1, "baud", "$(BAUD=9600)")
-$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)", -1, "bits", "$(BITS=8)")
-$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)", -1, "parity", "$(PARITY=none)")
-$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)", -1, "stop", "$(STOP=1)")
-asynOctetSetInputEos("$(DEVICE)", -1, "\r")
-asynOctetSetOutputEos("$(DEVICE)", -1, "\r")
-
-## Load record instances
-
-##ISIS## Load common DB records 
-< $(IOCSTARTUP)/dbload.cmd
-
-## Load our record instances
-dbLoadRecords("db/ILM200_common.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=$(DEVICE),ISO=$(ISOBUS=1)")
-dbLoadRecordsLoop("db/ILM200_channel.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,N=\$(N), PORT=$(DEVICE), ISO=$(ISOBUS=1)", "N", 1, 3, 1)
-
-##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
-< $(IOCSTARTUP)/preiocinit.cmd
-
-cd "${TOP}/iocBoot/${IOC}"
-iocInit
-
-## Start any sequence programs
-#seq sncxxx,"user=hra63823"
-
-##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
-< $(IOCSTARTUP)/postiocinit.cmd
+## calling common command file in ioc 01 boot dir
+< ${TOP}/iocBoot/iocILM200-IOC-01/st-common.cmd
