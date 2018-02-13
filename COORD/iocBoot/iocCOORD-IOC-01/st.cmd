b61438dd6c6498a36143692dcaa32249d3fc81c0
Tom Willemsen <tomwillemsen1995@gmail.com>
2018-02-13 15:41:53 +0000

    Rewrite the db startup and build process

--- COORD/iocBoot/iocCOORD-IOC-01/st.cmd
+++ COORD/iocBoot/iocCOORD-IOC-01/st.cmd
@@ -17,8 +17,18 @@ COORD_IOC_01_registerRecordDeviceDriver pdbbase
 ##ISIS## Load common DB records 
 < $(IOCSTARTUP)/dbload.cmd
 
-< ${TOP}/iocBoot/${IOC}/st_riken_port_changeover.cmd
-< ${TOP}/iocBoot/${IOC}/st_riken_rb2_mode_changeover.cmd
+# Riken port change
+epicsEnvSet(PC_PSU_DISABLE,$(MYPVPREFIX)$(IOCNAME):PC:PSUS:DISABLE)
+epicsEnvSet(PC_PSU_POWER,$(MYPVPREFIX)$(IOCNAME):PC:PSUS:POWER)
+$(IFRIKEN=#) dbLoadRecords("$(TOP)/db/riken_changeover.db","P=$(MYPVPREFIX)$(IOCNAME):,PSU_DISABLE=$(PC_PSU_DISABLE),PSU_POWER=$(PC_PSU_POWER)")
+$(IFRIKEN=#) dbLoadRecords("$(TOP)/db/riken_port_changeover_psus.db","PV_PREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,PSU_DISABLE=$(PC_PSU_DISABLE)")
+
+# Riken RB2 mode change
+epicsEnvSet(RB2C_PSU_DISABLE,$(MYPVPREFIX)$(IOCNAME):RB2C:PSUS:DISABLE)
+epicsEnvSet(RB2C_PSU_POWER,$(MYPVPREFIX)$(IOCNAME):RB2C:PSUS:POWER)
+$(IFRIKEN=#) dbLoadRecords("$(TOP)/db/riken_changeover.db","P=$(MYPVPREFIX)$(IOCNAME):,PSU_DISABLE=$(RB2C_PSU_DISABLE),PSU_POWER=$(RB2C_PSU_POWER)")
+$(IFRIKEN=#) dbLoadRecords("$(TOP)/db/riken_rb2_mode_changeover_psus.db","PV_PREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,PSU_DISABLE=$(RB2C_PSU_DISABLE)")
+
 
 ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
 < $(IOCSTARTUP)/preiocinit.cmd
@@ -26,8 +36,11 @@ COORD_IOC_01_registerRecordDeviceDriver pdbbase
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
 
-seq riken_changeover, "OK_TO_RUN_PSUS=$(PC_OK_TO_RUN_PSUS),ALLOW_CHANGEOVER=$(PC_ALLOW_CHANGEOVER),PSU_DISABLE=$(PC_PSU_DISABLE),PSU_POWER=$(PC_PSU_POWER)"
-seq riken_changeover, "OK_TO_RUN_PSUS=$(RB2C_OK_TO_RUN_PSUS),ALLOW_CHANGEOVER=$(RB2C_ALLOW_CHANGEOVER),PSU_DISABLE=$(RB2C_PSU_DISABLE),PSU_POWER=$(RB2C_PSU_POWER)"
+# Riken port change
+$(IFRIKEN=#) seq riken_changeover, "OK_TO_RUN_PSUS=$(MYPVPREFIX)$(RIKEN_PC_IN),ALLOW_CHANGEOVER=$(MYPVPREFIX)$(RIKEN_PC_OUT),PSU_DISABLE=$(PC_PSU_DISABLE),PSU_POWER=$(PC_PSU_POWER)"
+
+# Riken RB2 mode change
+$(IFRIKEN=#) seq riken_changeover, "OK_TO_RUN_PSUS=$(MYPVPREFIX)$(RIKEN_RB2C_IN),ALLOW_CHANGEOVER=$(MYPVPREFIX)$(RIKEN_RB2C_OUT),PSU_DISABLE=$(RB2C_PSU_DISABLE),PSU_POWER=$(RB2C_PSU_POWER)"
 
 
 ##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
