20585b0bccf3e5a98b36d3bd9631922f22689e5e
esouthren <eilidhsouthren@gmail.com>
2018-03-20 15:40:36 +0000

    changed baud/parity settings after testing on hardware

--- KHLY2700/iocBoot/iocKHLY2700-IOC-01/st-common.cmd
+++ KHLY2700/iocBoot/iocKHLY2700-IOC-01/st-common.cmd
@@ -9,8 +9,8 @@ $(IFDEVSIM) drvAsynIPPortConfigure("L0", "localhost:$(EMULATOR_PORT=)")
 ## For real device use:
 $(IFNOTDEVSIM) $(IFNOTRECSIM) drvAsynSerialPortConfigure("L0", "$(PORT=NO_PORT_MACRO)", 0, 0, 0, 0)
 $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0", -1, "baud", "$(BAUD=9600)")
-$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0", -1, "bits", "$(BITS=7)")
-$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0", -1, "parity", "$(PARITY=even)")
+$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0", -1, "bits", "$(BITS=8)")
+$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0", -1, "parity", "$(PARITY=none)")
 $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0", -1, "stop", "$(STOP=1)")
 
 ##ISIS## Load common DB records 
@@ -21,8 +21,8 @@ dbLoadRecords("${TOP}/db/keithley2700.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0, 
 dbLoadRecords("${TOP}/db/keithley2700_channels.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0, RECSIM=$(RECSIM=0), DISABLE=$(DISABLE=0)")
 
 ## For debugging:
-#asynSetTraceMask("L0",-1,0x9) 
-#asynSetTraceIOMask("L0",-1,0x2)
+asynSetTraceMask("L0",-1,0x9) 
+asynSetTraceIOMask("L0",-1,0x2)
 
 
 ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
