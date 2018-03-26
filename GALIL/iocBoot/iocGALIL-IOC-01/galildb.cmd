3be54dd9b9cddca310da3133b3643c2da86512c8
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2018-03-26 16:17:24 +0100

    Pass additional required macros

--- GALIL/iocBoot/iocGALIL-IOC-01/galildb.cmd
+++ GALIL/iocBoot/iocGALIL-IOC-01/galildb.cmd
@@ -2,11 +2,12 @@
 
 ## MTRCTRL is the galil crate number - 01, 02, ...
 
-dbLoadRecords("$(TOP)/db/galil_motor.db", "PVPREFIX=$(MYPVPREFIX),PORT=$(GALIL_MTR_PORT),P=$(MYPVPREFIX)MOT,CCP=$(MTRCTRL),ASG01=$(ASG01=DEFAULT),ASG02=$(ASG02=DEFAULT),ASG03=$(ASG03=DEFAULT),ASG04=$(ASG04=DEFAULT),ASG05=$(ASG05=DEFAULT),ASG06=$(ASG06=DEFAULT),ASG07=$(ASG07=DEFAULT),ASG08=$(ASG08=DEFAULT)")
+## GALIL_MTR_PORT will be Galil or GalilSim
+dbLoadRecords("$(TOP)/db/galil_motor.db", "PORT=$(GALIL_MTR_PORT),P=$(MYPVPREFIX)MOT,CCP=$(MTRCTRL)")
 
 #Load extra functionality, untop of motorRecord features for axis/motors (eg. runtime gear ratio changes between master & slaves)
 dbLoadRecords("$(TOP)/db/galil_ctrl_extras.db", "P=$(MYPVPREFIX)MOT,CCP=$(MTRCTRL)")
-dbLoadRecords("$(TOP)/db/galil_motor_extras.db", "P=$(MYPVPREFIX)MOT,CCP=$(MTRCTRL)")
+dbLoadRecords("$(TOP)/db/galil_motor_extras.db", "P=$(MYPVPREFIX)MOT,CCP=$(MTRCTRL),PVPREFIX=$(MYPVPREFIX),ASG01=$(ASG01=DEFAULT),ASG02=$(ASG02=DEFAULT),ASG03=$(ASG03=DEFAULT),ASG04=$(ASG04=DEFAULT),ASG05=$(ASG05=DEFAULT),ASG06=$(ASG06=DEFAULT),ASG07=$(ASG07=DEFAULT),ASG08=$(ASG08=DEFAULT)")
 
 #Load digital IO databases
 dbLoadRecords("$(TOP)/db/galil_digital_ports.db", "P=$(MYPVPREFIX)MOT,CCP=$(MTRCTRL)")
