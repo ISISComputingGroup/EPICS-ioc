2f98d76f468500ea67d0e02af7626c07457a5bd3
Tom Willemsen <tomwillemsen1995@gmail.com>
2018-05-14 11:51:43 +0100

    Tidy up DB. Modify st.cmd to not throw errors in recsim.

--- ITC503/iocBoot/iocITC503-IOC-01/st.cmd
+++ ITC503/iocBoot/iocITC503-IOC-01/st.cmd
@@ -15,10 +15,10 @@ epicsEnvSet "STREAM_PROTOCOL_PATH" "$(ITC503)/data"
 < $(IOCSTARTUP)/init.cmd
 
 ## For recsim:
-$(IFRECSIM) drvAsynSerialPortConfigure("$(DEVICE)", "$(PORT=NUL)", 0, 1, 0, 0)
+$(IFRECSIM) drvAsynSerialPortConfigure("L0", "$(PORT=NUL)", 0, 1, 0, 0)
 
 # For dev sim devices
-$(IFDEVSIM) drvAsynIPPortConfigure("L0", "localhost:$(EMULATOR_PORT=)")
+$(IFDEVSIM) drvAsynIPPortConfigure("L0", "localhost:$(EMULATOR_PORT=57677)")
 
 ## For real device use:
 $(IFNOTDEVSIM) $(IFNOTRECSIM) drvAsynSerialPortConfigure("L0", "$(PORT=NO_PORT_MACRO)", 0, 0, 0, 0)
