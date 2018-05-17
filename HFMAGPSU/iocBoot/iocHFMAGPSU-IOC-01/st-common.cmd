97dc296a15617c1544bb6a7431b402c7cd3962be
esouthren <eilidhsouthren@gmail.com>
2018-05-17 11:20:45 +0100

    Code linting, .db passes dbChecker

--- HFMAGPSU/iocBoot/iocHFMAGPSU-IOC-01/st-common.cmd
+++ HFMAGPSU/iocBoot/iocHFMAGPSU-IOC-01/st-common.cmd
@@ -27,9 +27,6 @@ $(IFNOTRECSIM) asynOctetSetOutputEos("L0",0,"$(OEOS=\\r\\n)")
 ## Load our record instances
 dbLoadRecords("db/HFMAGPSU.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0, RECSIM=$(RECSIM=0), DISABLE=$(DISABLE=0)")
 
-
-
-
 ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
 < $(IOCSTARTUP)/preiocinit.cmd
 
@@ -42,10 +39,11 @@ iocInit
 
 epicsEnvSet "P" "$(MYPVPREFIX)$(IOCNAME)"
 
-# Directory of ramp rate table
-epicsEnvSet "RAMPTABLEDIR" "C:\\Instrument\\Settings\\config\\NDLT658\\configurations\\hifi_ramp_table.txt"
+epicsEnvSet "CALIB_BASE_DIR" "C:/Instrument/Settings/config/common/"
+epicsEnvSet "CALIB_DIR" "ramps/"
+epicsEnvSet "CALIB_FILE" "cryomag_ramp_table.txt"
+epicsEnvSet "RAMPTABLEDIR" $(CALIB_BASE_DIR)$(CALIB_DIR)$(CALIB_FILE)
+
 ## Start any sequence programs
 
 seq fsm, "P=$(P), RAMPTABLEDIR=$(RAMPTABLEDIR)"
-
-
