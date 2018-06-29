82b6fb63a2c8448cc43f2ff56e00730fbf044731
Alistair McGann <alistair.mcgann@tessella.com>
2018-06-29 16:28:17 +0100

    Created st-common file and added user changable macro

---
+++ KYNCTM3K/iocBoot/iocKYNCTM3K-IOC-01/st-common.cmd
@@ -0,0 +1,55 @@
+#!../../bin/windows-x64/KYNCTM3K-IOC-01
+
+## You may have to change KYNCTM3K-IOC-01 to something else
+## everywhere it appears in this file
+
+# Increase this if you get <<TRUNCATED>> or discarded messages warnings in your errlog output
+errlogInit2(65536, 256)
+
+#< envPaths
+
+epicsEnvSet "STREAM_PROTOCOL_PATH" "$(KYNCTM3K)/data"
+epicsEnvSet "DEVICE" "L0"
+
+#cd "${TOP}"
+
+## Register all support components
+#dbLoadDatabase "dbd/KYNCTM3K-IOC-01.dbd"
+#KYNCTM3K_IOC_01_registerRecordDeviceDriver pdbbase
+
+##ISIS## Run IOC initialisation 
+< $(IOCSTARTUP)/init.cmd
+
+## Device simulation mode IP configuration
+$(IFDEVSIM) drvAsynIPPortConfigure("$(DEVICE)", "localhost:$(EMULATOR_PORT=57677)")
+
+## For recsim:
+$(IFRECSIM) drvAsynSerialPortConfigure("$(DEVICE)", "$(PORT=NUL)", 0, 1, 0, 0)
+
+## For real device:
+$(IFNOTDEVSIM) $(IFNOTRECSIM) drvAsynSerialPortConfigure("$(DEVICE)", "$(PORT=NO_PORT_MACRO)", 0, 0, 0, 0)
+$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)", -1, "baud", "$(BAUD=9600)")
+$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)", -1, "bits", "$(BITS=8)")
+$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)", -1, "parity", "$(PARITY=none)")
+$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)", -1, "stop", "$(STOP=1)")
+
+## Load record instances
+
+##ISIS## Load common DB records 
+< $(IOCSTARTUP)/dbload.cmd
+
+## Load our record instances
+dbLoadRecords("$(KYNCTM3K)/db/KYNCTM3K.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=$(DEVICE),MEAS_UNITS=$(MEAS_UNITS=mm)")
+dbLoadRecords("$(KYNCTM3K)/db/KYNCTM3K_out_channels.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=$(DEVICE),MEAS_UNITS=$(MEAS_UNITS=mm)")
+
+##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
+< $(IOCSTARTUP)/preiocinit.cmd
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=mcga"
+
+##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
+< $(IOCSTARTUP)/postiocinit.cmd
--- KYNCTM3K/iocBoot/iocKYNCTM3K-IOC-02/st.cmd
+++ KYNCTM3K/iocBoot/iocKYNCTM3K-IOC-02/st.cmd
@@ -3,13 +3,7 @@
 ## You may have to change KYNCTM3K-IOC-02 to something else
 ## everywhere it appears in this file
 
-# Increase this if you get <<TRUNCATED>> or discarded messages warnings in your errlog output
-errlogInit2(65536, 256)
-
 < envPaths
-
-epicsEnvSet "STREAM_PROTOCOL_PATH" "$(KYNCTM3K)/data"
-epicsEnvSet "DEVICE" "L0"
 
 cd "${TOP}"
 
@@ -17,38 +11,5 @@ cd "${TOP}"
 dbLoadDatabase "dbd/KYNCTM3K-IOC-02.dbd"
 KYNCTM3K_IOC_02_registerRecordDeviceDriver pdbbase
 
-##ISIS## Run IOC initialisation 
-< $(IOCSTARTUP)/init.cmd
-
-## Device simulation mode IP configuration
-$(IFDEVSIM) drvAsynIPPortConfigure("$(DEVICE)", "localhost:$(EMULATOR_PORT=57677)")
-
-## For recsim:
-$(IFRECSIM) drvAsynSerialPortConfigure("$(DEVICE)", "$(PORT=NUL)", 0, 1, 0, 0)
-
-## For real device:
-$(IFNOTDEVSIM) $(IFNOTRECSIM) drvAsynSerialPortConfigure("$(DEVICE)", "$(PORT=NO_PORT_MACRO)", 0, 0, 0, 0)
-$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)", -1, "baud", "$(BAUD=9600)")
-$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)", -1, "bits", "$(BITS=8)")
-$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)", -1, "parity", "$(PARITY=none)")
-$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)", -1, "stop", "$(STOP=1)")
-
-## Load record instances
-
-##ISIS## Load common DB records 
-< $(IOCSTARTUP)/dbload.cmd
-
-## Load our record instances
-dbLoadRecords("db/KYNCTM3K.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=$(DEVICE)")
-
-##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
-< $(IOCSTARTUP)/preiocinit.cmd
-
-cd "${TOP}/iocBoot/${IOC}"
-iocInit
-
-## Start any sequence programs
-#seq sncxxx,"user=mcga"
-
-##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
-< $(IOCSTARTUP)/postiocinit.cmd
+##ISIS## Run IOC common boot file
+< ${TOP}/iocBoot/iocKYNCTM3K-IOC-01/st-common.cmd
