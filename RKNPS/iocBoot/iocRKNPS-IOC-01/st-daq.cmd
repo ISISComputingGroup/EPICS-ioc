28073af301365671a015b8d23f6a36b9872f5cd5
Tom Willemsen <tomwillemsen1995@gmail.com>
2018-08-03 10:36:36 +0100

    Fiddle with st.cmd

--- RKNPS/iocBoot/iocRKNPS-IOC-01/st-daq.cmd
+++ RKNPS/iocBoot/iocRKNPS-IOC-01/st-daq.cmd
@@ -1,24 +1,21 @@
 ## NI cDAQ-9181 with NI 9375 card
 
-#epicsEnvSet("CDAQ","cDAQ1Mod1")
 epicsEnvSet("CDAQ","cDAQ-R3G39Mod1")
 
 ## CDAQ port0 is input
-DAQmxConfig("R0", "$(CDAQ)/port0/line0", 0, "BI","OneShot N=1 F=0")
-DAQmxConfig("R0", "$(CDAQ)/port0/line1", 1, "BI","OneShot N=1 F=0")
-DAQmxConfig("R0", "$(CDAQ)/port0/line2", 2, "BI","OneShot N=1 F=0")
-DAQmxConfig("R0", "$(CDAQ)/port0/line3", 3, "BI","OneShot N=1 F=0")
-DAQmxConfig("R0", "$(CDAQ)/port0/line4", 4, "BI","OneShot N=1 F=0")
-DAQmxConfig("R0", "$(CDAQ)/port0/line5", 5, "BI","OneShot N=1 F=0")
-#asynSetTraceMask("R0", 0, 0x11)
+$(IFNOTSIM) DAQmxConfig("R0", "$(CDAQ)/port0/line0", 0, "BI","OneShot N=1 F=0")
+$(IFNOTSIM) DAQmxConfig("R0", "$(CDAQ)/port0/line1", 1, "BI","OneShot N=1 F=0")
+$(IFNOTSIM) DAQmxConfig("R0", "$(CDAQ)/port0/line2", 2, "BI","OneShot N=1 F=0")
+$(IFNOTSIM) DAQmxConfig("R0", "$(CDAQ)/port0/line3", 3, "BI","OneShot N=1 F=0")
+$(IFNOTSIM) DAQmxConfig("R0", "$(CDAQ)/port0/line4", 4, "BI","OneShot N=1 F=0")
+$(IFNOTSIM) DAQmxConfig("R0", "$(CDAQ)/port0/line5", 5, "BI","OneShot N=1 F=0")
 
 ## CDAQ port1 is output
-DAQmxConfig("W0", "$(CDAQ)/port1/line0", 0, "BO","N=1 F=0")
-DAQmxConfig("W0", "$(CDAQ)/port1/line1", 1, "BO","N=1 F=0")
-#asynSetTraceMask("W0", 0,0x11)
+$(IFNOTSIM) DAQmxConfig("W0", "$(CDAQ)/port1/line0", 0, "BO","N=1 F=0")
+$(IFNOTSIM) DAQmxConfig("W0", "$(CDAQ)/port1/line1", 1, "BO","N=1 F=0")
 
 ## Load record instances
-dbLoadRecords("$(DAQMXBASE)/db/DAQmxBaseIOC.vdb","DAQMX=$(MYPVPREFIX)$(IOCNAME):DAQ,RECSIM=$(RECSIM=0)")
+dbLoadRecords("$(DAQMXBASE)/db/DAQmxBaseIOC.vdb","DAQMX=$(MYPVPREFIX)$(IOCNAME):DAQ,RECSIM=$(SIMULATE=0)")
 
 dbLoadRecords("$(DAQMXBASE)/db/DAQmxBaseDigIn.vdb","DAQMX=$(MYPVPREFIX)$(IOCNAME):DAQ,PORT=R0,CHAN=0")
 dbLoadRecords("$(DAQMXBASE)/db/DAQmxBaseDigIn.vdb","DAQMX=$(MYPVPREFIX)$(IOCNAME):DAQ,PORT=R0,CHAN=1")
