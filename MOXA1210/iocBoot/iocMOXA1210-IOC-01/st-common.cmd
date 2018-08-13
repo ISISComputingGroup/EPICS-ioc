f813571ae18921b57957de459713f8c021bea1ec
Alistair McGann <alistair.mcgann@tessella.com>
2018-08-13 09:48:34 +0100

    Increased length of data ports to stop input 15 being chopped off

--- MOXA1210/iocBoot/iocMOXA1210-IOC-01/st-common.cmd
+++ MOXA1210/iocBoot/iocMOXA1210-IOC-01/st-common.cmd
@@ -34,20 +34,20 @@ $(IFNOTDEVSIM) $(IFNOTRECSIM) drvAsynIPPortConfigure ("$(E1210_ASYNPORT)","$(IP_
 modbusInterposeConfig("$(E1210_ASYNPORT)", 0, 2000, 0)
 
 # MOXA E1210 DIs (if NOT counter mode) : function 2 (Read Discrete Inputs), address 0x0, length 16, data_type = UINT16 = 0, # pollMsec = for read func, waits XXX msecs
-drvModbusAsynConfigure("$(E1210_ASYNPORT)_DI",             "$(E1210_ASYNPORT)", 0, 2, 0x0,   0xF, 0, 100, "ioLogik")
+drvModbusAsynConfigure("$(E1210_ASYNPORT)_DI",             "$(E1210_ASYNPORT)", 0, 2, 0x0,   0x10, 0, 100, "ioLogik")
 
 # MOXA E1210 DIs (if counter mode: start/stop counter) : function 5 (Write Single Coil), address 0x0100, length 16, data_type = UINT16 = 0, # pollMsec = for read func, waits XXX msecs
-drvModbusAsynConfigure("$(E1210_ASYNPORT)_DICNT_S",        "$(E1210_ASYNPORT)", 0, 5, 0x0100,   0xF, 0, 1, "ioLogik")
+drvModbusAsynConfigure("$(E1210_ASYNPORT)_DICNT_S",        "$(E1210_ASYNPORT)", 0, 5, 0x0100,   0x10, 0, 1, "ioLogik")
 
 # MOXA E1210 DIs (if counter mode: start/stop counter) : function 1 (Read Coils), address 0x0100, length 16, data_type = UINT16 = 0, # pollMsec = for read func, waits XXX msecs
-drvModbusAsynConfigure("$(E1210_ASYNPORT)_DICNT_S_RBV",    "$(E1210_ASYNPORT)", 0, 1, 0x0100,   0xF, 0, 500, "ioLogik")
+drvModbusAsynConfigure("$(E1210_ASYNPORT)_DICNT_S_RBV",    "$(E1210_ASYNPORT)", 0, 1, 0x0100,   0x10, 0, 500, "ioLogik")
 
 # MOXA E1210 DIs (if counter mode: clear counter) : function 5 (Write Single Coil), address 0x0110, length 16, data_type = UINT16 = 0, # pollMsec = for read func, waits XXX msecs
 # (RBV is always 0)
-drvModbusAsynConfigure("$(E1210_ASYNPORT)_DICNT_CLR",      "$(E1210_ASYNPORT)", 0, 5, 0x0110,   0xF, 0, 1, "ioLogik")
+drvModbusAsynConfigure("$(E1210_ASYNPORT)_DICNT_CLR",      "$(E1210_ASYNPORT)", 0, 5, 0x0110,   0x10, 0, 1, "ioLogik")
 
 # MOXA E1210 DIs (if counter mode: counter value) : function 4 (Read Input Registers), address 0x0010, 0x10 double words => length 0x20, data_type = INT32_BE = 6, # pollMsec = for read func, waits XXX msecs
-drvModbusAsynConfigure("$(E1210_ASYNPORT)_DICNT",          "$(E1210_ASYNPORT)", 0, 4, 0x0010,   0x20, 6, 500, "ioLogik")
+drvModbusAsynConfigure("$(E1210_ASYNPORT)_DICNT",          "$(E1210_ASYNPORT)", 0, 4, 0x0010,   0x22, 6, 500, "ioLogik")
 
 ##ISIS## Load common DB records 
 < $(IOCSTARTUP)/dbload.cmd
