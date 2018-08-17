ae2f9d5631ced876c11a2120da6501244acdf8e0
Rory-Potter-Tessella <rory.potter@tessella.com>
2018-08-17 13:34:07 +0100

    Updated DAQ config

--- KICKER/iocBoot/iocKICKER-IOC-01/st-daq.cmd
+++ KICKER/iocBoot/iocKICKER-IOC-01/st-daq.cmd
@@ -1,18 +1,17 @@
 ## NI cDAQ-9181 with NI 9375 card
-epicsEnvSet("CDAQ","cDAQ9181-RFEMod1")
+epicsEnvSet("CDAQ","cDAQ9185-1D195CF")
 
 ## CDAQ port0 is input
-$(IFNOTDEVSIM) DAQmxConfig("R0", "$(CDAQ)/port0/line0", 0, "AI","OneShot N=1 F=0")
-$(IFNOTDEVSIM) DAQmxConfig("R0", "$(CDAQ)/port0/line1", 1, "AI","OneShot N=1 F=0")
-$(IFNOTDEVSIM) DAQmxConfig("R0", "$(CDAQ)/port0/line2", 2, "AI","OneShot N=1 F=0")
-$(IFNOTDEVSIM) DAQmxConfig("R0", "$(CDAQ)/port0/line3", 3, "AI","OneShot N=1 F=0")
+$(IFNOTDEVSIM) DAQmxConfig("R0", "$(CDAQ)/port1/line0", 0, "AI","OneShot N=1 F=0")
+$(IFNOTDEVSIM) DAQmxConfig("R0", "$(CDAQ)/port1/line1", 1, "AI","OneShot N=1 F=0")
+$(IFNOTDEVSIM) DAQmxConfig("R0", "$(CDAQ)/port1/line2", 2, "AI","OneShot N=1 F=0")
+$(IFNOTDEVSIM) DAQmxConfig("R0", "$(CDAQ)/port1/line3", 3, "AI","OneShot N=1 F=0")
 
 
 ## Load record instances
 $(IFNOTDEVSIM) dbLoadRecords("$(DAQMXBASE)/db/DAQmxBaseIOC.vdb","DAQMX=$(MYPVPREFIX)$(IOCNAME):DAQ,RECSIM=$(RECSIM=0)")
 
-$(IFNOTDEVSIM) dbLoadRecords("$(DAQMXBASE)/db/DAQmxBaseAnIn.db","DAQMX=$(MYPVPREFIX)$(IOCNAME):DAQ:AI,PORT=R0,CHAN=0")
-$(IFNOTDEVSIM) dbLoadRecords("$(DAQMXBASE)/db/DAQmxBaseAnIn.db","DAQMX=$(MYPVPREFIX)$(IOCNAME):DAQ:AI,PORT=R0,CHAN=1")
-$(IFNOTDEVSIM) dbLoadRecords("$(DAQMXBASE)/db/DAQmxBaseAnIn.db","DAQMX=$(MYPVPREFIX)$(IOCNAME):DAQ:AI,PORT=R0,CHAN=2")
-$(IFNOTDEVSIM) dbLoadRecords("$(DAQMXBASE)/db/DAQmxBaseAnIn.db","DAQMX=$(MYPVPREFIX)$(IOCNAME):DAQ:AI,PORT=R0,CHAN=3")
-
+$(IFNOTDEVSIM) dbLoadRecords("$(DAQMXBASE)/db/DAQmxBaseAnIn.db","DAQMX=$(MYPVPREFIX)$(IOCNAME):DAQ,PORT=R0,CHAN=0,IFRECSIM=$(IFRECSIM),IFNOTRECSIM=$(IFNOTRECSIM)")
+$(IFNOTDEVSIM) dbLoadRecords("$(DAQMXBASE)/db/DAQmxBaseAnIn.db","DAQMX=$(MYPVPREFIX)$(IOCNAME):DAQ,PORT=R0,CHAN=1,IFRECSIM=$(IFRECSIM),IFNOTRECSIM=$(IFNOTRECSIM)")
+$(IFNOTDEVSIM) dbLoadRecords("$(DAQMXBASE)/db/DAQmxBaseAnIn.db","DAQMX=$(MYPVPREFIX)$(IOCNAME):DAQ,PORT=R0,CHAN=2,IFRECSIM=$(IFRECSIM),IFNOTRECSIM=$(IFNOTRECSIM)")
+$(IFNOTDEVSIM) dbLoadRecords("$(DAQMXBASE)/db/DAQmxBaseAnIn.db","DAQMX=$(MYPVPREFIX)$(IOCNAME):DAQ,PORT=R0,CHAN=3,IFRECSIM=$(IFRECSIM),IFNOTRECSIM=$(IFNOTRECSIM)")
