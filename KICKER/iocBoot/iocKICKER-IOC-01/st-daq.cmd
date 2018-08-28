1929c112d8f0a6f3009fd65b79264155c5cbfb5d
Rory-Potter-Tessella <rory.potter@tessella.com>
2018-08-28 13:31:25 +0100

    Tidy up st-daq for testing

--- KICKER/iocBoot/iocKICKER-IOC-01/st-daq.cmd
+++ KICKER/iocBoot/iocKICKER-IOC-01/st-daq.cmd
@@ -2,5 +2,5 @@
 epicsEnvSet("CDAQAI","cDAQ9185-1D195CFMod3")
 
 ## input
-$(IFNOTDEVSIM) DAQmxConfig("R0", "$(CDAQAI)/ai0", 0, "AI","N=1000 F=1000") ## Kicker Volt
-$(IFNOTDEVSIM) DAQmxConfig("R0", "$(CDAQAI)/ai1", 1, "AI","N=1000 F=1000") ## Kicker Curr
+$(IFNOTRECSIM) DAQmxConfig("R0", "$(CDAQAI)/ai0", 0, "AI","N=1000 F=1000") ## Kicker Volt
+$(IFNOTRECSIM) DAQmxConfig("R0", "$(CDAQAI)/ai1", 1, "AI","N=1000 F=1000") ## Kicker Curr
