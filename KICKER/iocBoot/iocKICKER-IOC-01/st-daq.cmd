b9655c9cca4f387aa3c733e497ff53f70c7cc4d5
Rory-Potter-Tessella <rory.potter@tessella.com>
2018-09-07 12:28:56 +0100

    Tidy up

--- KICKER/iocBoot/iocKICKER-IOC-01/st-daq.cmd
+++ KICKER/iocBoot/iocKICKER-IOC-01/st-daq.cmd
@@ -1,6 +1,6 @@
 ## NI cDAQ-9185
-epicsEnvSet("CDAQAI", "$(DAQ_AI_PORT_NAME = cDAQ9185-MuonFEMod3)")
+$(IFNOTRECSIM) epicsEnvSet("CDAQAI", "$(DAQ_AI_PORT_NAME = cDAQ9185-MuonFEMod3)")
 
 ## input
-$(IFNOTRECSIM) DAQmxConfig("R0", "$(CDAQAI)/ai0", 0, "AI","N=$(NELM = 1000) F=$(FREQ = 1000)") ## Kicker Volt
-$(IFNOTRECSIM) DAQmxConfig("R0", "$(CDAQAI)/ai1", 1, "AI","N=$(NELM = 1000) F=$(FREQ = 1000)") ## Kicker Curr
+$(IFNOTRECSIM) DAQmxConfig("R0", "$(CDAQAI)/ai1", 1, "AI","N=$(NELM=1000), F=$(FREQ=1000)") ## Kicker Curr
+$(IFNOTRECSIM) DAQmxConfig("R0", "$(CDAQAI)/ai0", 0, "AI","N=$(NELM=1000), F=$(FREQ=1000)") ## Kicker Volt
