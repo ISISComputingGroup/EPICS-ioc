3b899caef8255cac7ad795463bc55399a249d18a
Alistair McGann <alistair.mcgann@tessella.com>
2018-09-17 16:51:47 +0100

    Added ioc macros for separator stability

--- SEPRTR/iocBoot/iocSEPRTR-IOC-01/st-daq.cmd
+++ SEPRTR/iocBoot/iocSEPRTR-IOC-01/st-daq.cmd
@@ -6,10 +6,10 @@ epicsEnvSet("CDAQAO","cDAQ9185-MUONFEMod4")
 cDAQ9185-MUONFE-Mod4
 
 ## input
-$(IFNOTDEVSIM) DAQmxConfig("R0", "$(CDAQAI)/ai0", 0, "AI","N=1000 F=1000") ## Kicker Volt
-$(IFNOTDEVSIM) DAQmxConfig("R0", "$(CDAQAI)/ai1", 1, "AI","N=1000 F=1000") ## Kicker Curr
-$(IFNOTDEVSIM) DAQmxConfig("R0", "$(CDAQAI)/ai2", 2, "AI","N=1000 F=1000") ## Separator Volt
-$(IFNOTDEVSIM) DAQmxConfig("R0", "$(CDAQAI)/ai3", 3, "AI","N=1000 F=1000") ## Separator Curr
+$(IFNOTDEVSIM) DAQmxConfig("R0", "$(CDAQAI)/ai0", 0, "AI","N=$(NELM=1000) F=$(FREQ=1000)") ## Kicker Volt
+$(IFNOTDEVSIM) DAQmxConfig("R0", "$(CDAQAI)/ai1", 1, "AI","N=$(NELM=1000) F=$(FREQ=1000)") ## Kicker Curr
+$(IFNOTDEVSIM) DAQmxConfig("R0", "$(CDAQAI)/ai2", 2, "AI","N=$(NELM=1000) F=$(FREQ=1000)") ## Separator Volt
+$(IFNOTDEVSIM) DAQmxConfig("R0", "$(CDAQAI)/ai3", 3, "AI","N=$(NELM=1000) F=$(FREQ=1000)") ## Separator Curr
 
 ## output
 $(IFNOTDEVSIM) DAQmxConfig("W0", "$(CDAQAO)/ao0", 0, "AO","N=1 F=0") ## x
