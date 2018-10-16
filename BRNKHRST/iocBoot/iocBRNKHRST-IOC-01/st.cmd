2c2f85bec59641283a18682ef2eeab36beac07ac
Tom Willemsen <tomwillemsen1995@gmail.com>
2018-10-16 11:28:48 +0100

    Clean up after IOC generator

--- BRNKHRST/iocBoot/iocBRNKHRST-IOC-01/st.cmd
+++ BRNKHRST/iocBoot/iocBRNKHRST-IOC-01/st.cmd
@@ -3,16 +3,11 @@
 ## You may have to change BRNKHRST-IOC-01 to something else
 ## everywhere it appears in this file
 
-#< envPaths
+< envPaths
 
 ## Register all support components
-dbLoadDatabase("../../dbd/BRNKHRST-IOC-01.dbd",0,0)
+dbLoadDatabase("${TOP}/dbd/BRNKHRST-IOC-01.dbd",0,0)
 BRNKHRST_IOC_01_registerRecordDeviceDriver(pdbbase) 
 
-## Load record instances
-dbLoadRecords("../../db/BRNKHRST-IOC-01.db","user=ynq66733")
-
-iocInit()
-
-## Start any sequence programs
-#seq sncBRNKHRST-IOC-01,"user=ynq66733"
+## calling common command file in ioc 01 boot dir
+< ${TOP}/iocBoot/iocBRNKHRST-IOC-01/st-common.cmd
