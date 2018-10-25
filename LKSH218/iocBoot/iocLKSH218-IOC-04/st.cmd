8147568146f6efecec66bfbc04163a6347574acb
Rory-Potter-Tessella <rory.potter@tessella.com>
2018-10-25 14:02:00 +0100

    Added a fourth lakeshore

---
+++ LKSH218/iocBoot/iocLKSH218-IOC-04/st.cmd
@@ -0,0 +1,19 @@
+#!../../bin/windows-x64/LKSH218-IOC-04
+
+## You may have to change Lakeshore_218-04 to something else
+## everywhere it appears in this file
+
+# Increase this if you get <<TRUNCATED>> or discarded messages warnings in your errlog output
+errlogInit2(65536, 256)
+
+< envPaths
+
+cd ${TOP}
+
+## Register all support components
+dbLoadDatabase "dbd/LKSH218-IOC-04.dbd"
+LKSH218_IOC_04_registerRecordDeviceDriver pdbbase
+
+cd ${TOP}/iocBoot/iocLKSH218-IOC-01
+
+< st-common.cmd
