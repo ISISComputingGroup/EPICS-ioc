9cb132be5a7e4623272aa4548ed6745277ab5507
Alistair McGann <alistair.mcgann@tessella.com>
2019-01-17 17:19:28 +0000

    Got moxa 1240 to boot

--- MOXA12XX/iocBoot/iocMOXA12XX-IOC-01/st-e1240.cmd
+++ MOXA12XX/iocBoot/iocMOXA12XX-IOC-01/st-e1240.cmd
@@ -1,20 +1,20 @@
 # MOXA E1240 AIs: function 4, address 0x0, 0x8 words , data_type = UINT16 = 0,
-#drvModbusAsynConfigure("$(E12XX_ASYNPORT)_AI", "$(E12XX_ASYNPORT)", 0, 4, 0x000, 0x8, 0, 100, "ioLogik")
+drvModbusAsynConfigure("$(E12XX_ASYNPORT)_AI", "$(E12XX_ASYNPORT)", 0, 4, 0x000, 0x8, 0, 100, "ioLogik")
 
 # MOXA E1240 Current mode status: function 4, address 0x3c, 0x8 words , data_type = UINT16 = 0,
-#drvModbusAsynConfigure("$(E12XX_ASYNPORT)_AI_St", "$(E12XX_ASYNPORT)", 0, 4, 0x3c, 0x8, 0, 500, "ioLogik")
+drvModbusAsynConfigure("$(E12XX_ASYNPORT)_AI_St", "$(E12XX_ASYNPORT)", 0, 4, 0x3c, 0x8, 0, 500, "ioLogik")
 
 # MOXA E1240 Current mode settings read: function 3, address 0x18, 0x8 words , data_type = UINT16 = 0,
-#drvModbusAsynConfigure("$(E12XX_ASYNPORT)_AIMR", "$(E12XX_ASYNPORT)", 0, 3, 0x18, 0x8, 0, 500, "ioLogik")
+drvModbusAsynConfigure("$(E12XX_ASYNPORT)_AIMR", "$(E12XX_ASYNPORT)", 0, 3, 0x18, 0x8, 0, 500, "ioLogik")
 
 # MOXA E1240 Current mode settings write: function 6, address 0x18, 0x8 words , data_type = UINT16 = 0,
-#drvModbusAsynConfigure("$(E12XX_ASYNPORT)_AIMW", "$(E12XX_ASYNPORT)", 0, 6, 0x18, 0x8, 0, 1, "ioLogik")
+drvModbusAsynConfigure("$(E12XX_ASYNPORT)_AIMW", "$(E12XX_ASYNPORT)", 0, 6, 0x18, 0x8, 0, 1, "ioLogik")
 
 # MOXA E1240 Burn out value read (float - two words): function 3, address 0x28, 0x8 double words => 0x10, data_type = FLOAT32_LE = 7,
-#drvModbusAsynConfigure("$(E12XX_ASYNPORT)_BOR", "$(E12XX_ASYNPORT)", 0, 3, 0x0028, 0x10, 7, 500, "ioLogik")
+drvModbusAsynConfigure("$(E12XX_ASYNPORT)_BOR", "$(E12XX_ASYNPORT)", 0, 3, 0x0028, 0x10, 7, 500, "ioLogik")
 
 # MOXA E1240 Burn out value write (float - two words): function 16 write multiple regs, address 0x28, 0x8 double words => 0x10 , data_type = FLOAT32_LE = 7,
-#drvModbusAsynConfigure("$(E12XX_ASYNPORT)_BOW", "$(E12XX_ASYNPORT)", 0, 16, 0x0028, 0x10, 7, 1, "ioLogik")
+drvModbusAsynConfigure("$(E12XX_ASYNPORT)_BOW", "$(E12XX_ASYNPORT)", 0, 16, 0x0028, 0x10, 7, 1, "ioLogik")
 
 
 ##ISIS## Load common DB records
@@ -23,8 +23,5 @@
 
 dbLoadRecords("$(MOXA12XX)/db/ioLogik_E1240.db","NAME=$(MYPVPREFIX)$(IOCNAME), ASYNPORT=$(E12XX_ASYNPORT)")
 
-dbLoadRecords("${TOP}/db/IBEX_PVs.db","NAME=$(MYPVPREFIX)$(IOCNAME), P=$(MYPVPREFIX)$(IOCNAME), ASYNPORT=$(E12XX_ASYNPORT)")
+dbLoadRecords("${TOP}/db/moxa_e1240_PVs.db","NAME=$(MYPVPREFIX)$(IOCNAME), P=$(MYPVPREFIX)$(IOCNAME), ASYNPORT=$(E12XX_ASYNPORT)")
 
-# Load the user-given aliases of the channels
-
-iocshCmdList("< ${TOP}/iocBoot/iocMOXA12XX-IOC-01/st-aliases.cmd", "CHAN=\$(I)", "I", "00;01;02;03;04;05;06;07;08;09;10;11;12;13;14;15", ";")
