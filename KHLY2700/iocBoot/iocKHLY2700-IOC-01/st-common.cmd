6a97ff7025ff55d2d7a3e315d26f6d1e6bae3f56
Liam <02liamp@gmail.com>
2019-01-30 11:02:46 +0000

    Most review changes

--- KHLY2700/iocBoot/iocKHLY2700-IOC-01/st-common.cmd
+++ KHLY2700/iocBoot/iocKHLY2700-IOC-01/st-common.cmd
@@ -40,5 +40,3 @@ iocInit
 
 ##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs
 < $(IOCSTARTUP)/postiocinit.cmd
-asynSetTraceIOMask("L0", -1, 0x2)
-asynSetTraceMask("L0", -1, 0x9)
