81a13e07b2506f57dfaa370c721725b4afefee65
Tom Willemsen <tomwillemsen1995@gmail.com>
2019-02-22 20:06:40 +0000

    Make IOC 2 work

--- MEZFLIPR/iocBoot/iocMEZFLIPR-IOC-01/st.cmd
+++ MEZFLIPR/iocBoot/iocMEZFLIPR-IOC-01/st.cmd
@@ -1,58 +1,13 @@
 #!../../bin/windows-x64/MEZFLIPR-IOC-01
 
-## You may have to change MEZFLIPR-IOC-01 to something else
+## You may have to change MEZFLIPR to something else
 ## everywhere it appears in this file
-
-# Increase this if you get <<TRUNCATED>> or discarded messages warnings in your errlog output
-errlogInit2(65536, 256)
 
 < envPaths
 
-epicsEnvSet "STREAM_PROTOCOL_PATH" "$(MEZFLIPR)/data"
-epicsEnvSet "DEVICE" "L0"
-
-cd "${TOP}"
-
 ## Register all support components
-dbLoadDatabase "dbd/MEZFLIPR-IOC-01.dbd"
+dbLoadDatabase "${TOP}/dbd/MEZFLIPR-IOC-01.dbd"
 MEZFLIPR_IOC_01_registerRecordDeviceDriver pdbbase
 
-##ISIS## Run IOC initialisation 
-< $(IOCSTARTUP)/init.cmd
-
-## Device simulation mode IP configuration:
-$(IFDEVSIM) drvAsynIPPortConfigure("$(DEVICE)", "localhost:$(EMULATOR_PORT=57677)")
-
-## For recsim:
-$(IFRECSIM) drvAsynSerialPortConfigure("$(DEVICE)", "$(PORT=NUL)", 0, 1, 0, 0)
-
-# For real device:
-$(IFDEVSIM) drvAsynIPPortConfigure("$(DEVICE)", "$(IP_ADDR):80")
-
-
-## Load record instances
-
-##ISIS## Load common DB records 
-< $(IOCSTARTUP)/dbload.cmd
-
-## Load our record instances
-dbLoadRecords("$(MEZFLIPR)/db/mezflipr_common.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=$(DEVICE)")
-
-dbLoadRecords("$(MEZFLIPR)/db/mezflipr_single.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=$(DEVICE),NAME=ANALYSER,CODE=a")
-dbLoadRecords("$(MEZFLIPR)/db/mezflipr_single.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=$(DEVICE),NAME=POLARISER,CODE=p")
-
-
-##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
-< $(IOCSTARTUP)/preiocinit.cmd
-
-cd "${TOP}/iocBoot/${IOC}"
-iocInit
-
-## Start any sequence programs
-#seq sncxxx,"user=ynq66733"
-
-##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
-< $(IOCSTARTUP)/postiocinit.cmd
-
-asynSetTraceMask("L0", -1, 0x9)
-asynSetTraceIOMask("L0", -1, 0x2)
+## calling common command file in ioc 01 boot dir
+< ${TOP}/iocBoot/iocMEZFLIPR-IOC-01/st-common.cmd
--- MEZFLIPR/iocBoot/iocMEZFLIPR-IOC-02/st.cmd
+++ MEZFLIPR/iocBoot/iocMEZFLIPR-IOC-02/st.cmd
@@ -1,60 +1,13 @@
 #!../../bin/windows-x64/MEZFLIPR-IOC-02
 
-## You may have to change MEZFLIPR-IOC-02 to something else
+## You may have to change MEZFLIPR to something else
 ## everywhere it appears in this file
-
-# Increase this if you get <<TRUNCATED>> or discarded messages warnings in your errlog output
-errlogInit2(65536, 256)
 
 < envPaths
 
-epicsEnvSet "STREAM_PROTOCOL_PATH" "$(_SUPPORT_MACRO_)/data"
-epicsEnvSet "DEVICE" "L0"
-
-cd "${TOP}"
-
 ## Register all support components
-dbLoadDatabase "dbd/MEZFLIPR-IOC-02.dbd"
+dbLoadDatabase "${TOP}/dbd/MEZFLIPR-IOC-02.dbd"
 MEZFLIPR_IOC_02_registerRecordDeviceDriver pdbbase
 
-##ISIS## Run IOC initialisation 
-< $(IOCSTARTUP)/init.cmd
-
-## Device simulation mode IP configuration
-$(IFDEVSIM) drvAsynIPPortConfigure("$(DEVICE)", "localhost:$(EMULATOR_PORT=57677)")
-
-## For recsim:
-$(IFRECSIM) drvAsynSerialPortConfigure("$(DEVICE)", "$(PORT=NUL)", 0, 1, 0, 0)
-
-## For real device:
-$(IFNOTDEVSIM) $(IFNOTRECSIM) drvAsynSerialPortConfigure("$(DEVICE)", "$(PORT=NO_PORT_MACRO)", 0, 0, 0, 0)
-$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)", -1, "baud", "$(BAUD=9600)")
-$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)", -1, "bits", "$(BITS=8)")
-$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)", -1, "parity", "$(PARITY=none)")
-$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)", -1, "stop", "$(STOP=1)")
-## Hardware flow control off
-$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)", 0, "clocal", "Y")
-$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)",0,"crtscts","N")
-## Software flow control off
-$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)",0,"ixon","N")
-$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)",0,"ixoff","N")
-
-## Load record instances
-
-##ISIS## Load common DB records 
-< $(IOCSTARTUP)/dbload.cmd
-
-## Load our record instances
-dbLoadRecords("$(_SUPPORT_MACRO_)/db/_NAME_LOWER_.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=$(DEVICE)")
-
-##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
-< $(IOCSTARTUP)/preiocinit.cmd
-
-cd "${TOP}/iocBoot/${IOC}"
-iocInit
-
-## Start any sequence programs
-#seq sncxxx,"user=ynq66733"
-
-##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
-< $(IOCSTARTUP)/postiocinit.cmd
+## calling common command file in ioc 01 boot dir
+< ${TOP}/iocBoot/iocMEZFLIPR-IOC-01/st-common.cmd
