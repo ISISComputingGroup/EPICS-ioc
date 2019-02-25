5cf5cb349ec6c78f3ffef70f453161579b92d9e6
aaronlong <aaron.long@stfc.ac.uk>
2019-02-25 11:49:29 +0000

    Add template IOC

---
+++ KNRK6/iocBoot/iocKNRK6-IOC-01/st.cmd
@@ -0,0 +1,18 @@
+#!../../bin/windows-x64/KNRK6-IOC-01
+
+## You may have to change KNRK6-IOC-01 to something else
+## everywhere it appears in this file
+
+# Increase this if you get <<TRUNCATED>> or discarded messages warnings in your errlog output
+errlogInit2(65536, 256)
+
+< envPaths
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/KNRK6-IOC-01.dbd"
+KNRK6_IOC_01_registerRecordDeviceDriver pdbbase
+
+## calling common command file in ioc 01 boot dir
+< ${TOP}/iocBoot/iocKNRK6-IOC-01/st-common.cmd
