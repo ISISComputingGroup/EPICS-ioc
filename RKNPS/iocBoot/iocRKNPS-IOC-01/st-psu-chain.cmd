a57a7303b0edbd413b41796a57bf04e1b8f1dda0
Alistair McGann <alistair.mcgann@tessella.com>
2019-04-04 14:43:00 +0100

    Roll back macro change

--- RKNPS/iocBoot/iocRKNPS-IOC-01/st-psu-chain.cmd
+++ RKNPS/iocBoot/iocRKNPS-IOC-01/st-psu-chain.cmd
@@ -1,7 +1,7 @@
 epicsEnvSet "DEVICE" "L$(CHAIN)"
 
 ## use with emulator
-$(IFDEVSIM) drvAsynIPPortConfigure("$(DEVICE)", "localhost:$(EMULATOR_PORT$(CHAIN))")
+$(IFDEVSIM) drvAsynIPPortConfigure("$(DEVICE)", "localhost:$(EMULATOR_PORT=)")
 
 ## use with real device
 $(IFNOTDEVSIM) $(IFNOTRECSIM) drvAsynSerialPortConfigure("$(DEVICE)", "$(CHAIN$(CHAIN)_PORT)", 0, 0, 0, 0)
