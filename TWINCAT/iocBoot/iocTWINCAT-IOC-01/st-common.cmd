06aace4a3f108fd8928c7dcfec9d562e619f3519
Dominic Oram <dominic.oram@stfc.ac.uk>
2019-05-16 11:57:21 +0100

    Added TWINCAT ioc based on tcIoc

---
+++ TWINCAT/iocBoot/iocTWINCAT-IOC-01/st-common.cmd
@@ -0,0 +1,19 @@
+##ISIS## Run IOC initialisation 
+< $(IOCSTARTUP)/init.cmd
+
+## Load record instances
+tcSetAlias("PLC:TEST:")
+tcSetScanRate(10, 5)
+tcLoadRecords ("$(TPY_FILE)", "-eo -devtc")
+
+##ISIS## Load common DB records 
+< $(IOCSTARTUP)/dbload.cmd
+
+##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
+< $(IOCSTARTUP)/preiocinit.cmd
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
+##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
+< $(IOCSTARTUP)/postiocinit.cmd
