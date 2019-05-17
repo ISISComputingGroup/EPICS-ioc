49e2fe17ed8e841bb387e1333c5a7b36f11b1103
Alistair McGann <alistair.mcgann@tessella.com>
2019-05-17 15:49:04 +0100

    Allow ADDRESS macro to be set

--- DFKPS/iocBoot/iocDFKPS-IOC-01/st-8500.cmd
+++ DFKPS/iocBoot/iocDFKPS-IOC-01/st-8500.cmd
@@ -1,5 +1,5 @@
 # Cast PSU address as 3-digit number
-calc("ADDRESS", "$(ADDRESS)", 2, 3)
+calc("ADDRESS", "$(ADDRESS=0)", 2, 3)
 
 # Send device initialisation commands
 asynOctetConnect("DFKINIT","L0")
@@ -11,4 +11,4 @@ asynOctetWrite DFKINIT "REM\n\r"
 epicsEnvSet "POLCMD" "PO "
 
 ## Load our record instances
-dbLoadRecords("$(TOP)/Db/DFKPS_8500_status.db", "device=$(MYPVPREFIX)$(IOCNAME), P=$(MYPVPREFIX)$(IOCNAME):, port=L0, SP_PINI=$(SP_PINI), ADDRESS=$(ADDRESS=0), POLCMD=$(POLCMD)")
+dbLoadRecords("$(TOP)/Db/DFKPS_8500_status.db", "device=$(MYPVPREFIX)$(IOCNAME), P=$(MYPVPREFIX)$(IOCNAME):, port=L0, SP_PINI=$(SP_PINI), ADDRESS=$(ADDRESS), POLCMD=$(POLCMD)")
