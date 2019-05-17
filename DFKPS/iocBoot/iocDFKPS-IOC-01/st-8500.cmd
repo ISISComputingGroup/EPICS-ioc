b8eb8490abc3ae53739c8abd0ba0e9d2e03736d0
Alistair McGann <alistair.mcgann@tessella.com>
2019-05-17 09:15:40 +0100

    Remove asyn trace logging

--- DFKPS/iocBoot/iocDFKPS-IOC-01/st-8500.cmd
+++ DFKPS/iocBoot/iocDFKPS-IOC-01/st-8500.cmd
@@ -9,6 +9,3 @@ asynOctetWrite DFKINIT "REM$(IEOS=\\n\\r)"
 
 ## Load our record instances
 dbLoadRecords("$(TOP)/Db/DFKPS_8500_status.db", "device=$(MYPVPREFIX)$(IOCNAME), P=$(MYPVPREFIX)$(IOCNAME):, port=L0, SP_PINI=$(SP_PINI), ADDRESS=$(ADDRESS=0)")
-
-asynSetTraceMask("L0", -1, 0x9)
-asynSetTraceIOMask("L0", -1, 0x2)
