ee4ab4292d137bc5b690e33551a08c47210d86d1
Alistair McGann <alistair.mcgann@tessella.com>
2019-05-17 11:39:49 +0100

    Add variable polarity command

--- DFKPS/iocBoot/iocDFKPS-IOC-01/st-8500.cmd
+++ DFKPS/iocBoot/iocDFKPS-IOC-01/st-8500.cmd
@@ -3,9 +3,12 @@ calc("ADDRESS", "$(ADDRESS)", 2, 3)
 
 # Send device initialisation commands
 asynOctetConnect("DFKINIT","L0")
-asynOctetWrite DFKINIT "ADR $(ADDRESS)$(IEOS=\\n\\r)"
-asynOctetWrite DFKINIT "UNLOCK$(IEOS=\\n\\r)"
-asynOctetWrite DFKINIT "REM$(IEOS=\\n\\r)"
+asynOctetWrite DFKINIT "ADR $(ADDRESS)\n\r"
+asynOctetWrite DFKINIT "UNLOCK\n\r"
+asynOctetWrite DFKINIT "REM\n\r"
+
+# Set polarity command
+epicsEnvSet "POLCMD" "PO "
 
 ## Load our record instances
-dbLoadRecords("$(TOP)/Db/DFKPS_8500_status.db", "device=$(MYPVPREFIX)$(IOCNAME), P=$(MYPVPREFIX)$(IOCNAME):, port=L0, SP_PINI=$(SP_PINI), ADDRESS=$(ADDRESS=0)")
+dbLoadRecords("$(TOP)/Db/DFKPS_8500_status.db", "device=$(MYPVPREFIX)$(IOCNAME), P=$(MYPVPREFIX)$(IOCNAME):, port=L0, SP_PINI=$(SP_PINI), ADDRESS=$(ADDRESS=0), POLCMD=$(POLCMD)")
