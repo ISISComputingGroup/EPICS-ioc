d671680f314648408630a0eca0f818572abe8c7b
Dominic Oram <dominic.oram@stfc.ac.uk>
2019-06-28 19:20:52 +0100

    Moved more logic into lua

--- ATTOCUBE/iocBoot/iocATTOCUBE-IOC-01/st-common.cmd
+++ ATTOCUBE/iocBoot/iocATTOCUBE-IOC-01/st-common.cmd
@@ -5,28 +5,12 @@ epicsEnvSet "NUM_AXES" "3"
 ##ISIS## Run IOC initialisation 
 < $(IOCSTARTUP)/init.cmd
 
-## Device simulation mode IP configuration
-$(IFDEVSIM) drvAsynIPPortConfigure("$(DEVICE)", "localhost:$(EMULATOR_PORT=57677)")
-
-## For recsim:
-$(IFRECSIM) drvAsynSerialPortConfigure("$(DEVICE)", "$(PORT=NUL)", 0, 1, 0, 0)
-
-## For real device:
-$(IFNOTDEVSIM) $(IFNOTRECSIM) drvAsynSerialPortConfigure("$(DEVICE)", "$(IP_ADDR=NO_PORT_MACRO):2101", 0, 0, 0, 0)
-$(IFNOTDEVSIM) $(IFNOTRECSIM) drvAsynIPPortConfigure("$(DEVICE)","130.246.51.234:2101",0,0,0)
-
-## For real device or devsim
-$(IFNOTRECSIM) anc350AsynMotorCreate("$(DEVICE)", "0", "0", "$(NUM_AXES)")
-$(IFNOTRECSIM) drvAsynMotorConfigure("$(MOTOR_PORT)", "anc350AsynMotor", "0", "$(NUM_AXES)")
-
-## Load record instances
+## Load our record instances
+epicsEnvSet("LUA_SCRIPT_PATH","${TOP}/iocBoot/${IOC}")
+luash("st-common.lua")
 
 ##ISIS## Load common DB records 
 < $(IOCSTARTUP)/dbload.cmd
-
-## Load our record instances
-epicsEnvSet("LUA_SCRIPT_PATH","${TOP}/iocBoot/${IOC}")
-luash("load_dbs.lua")
 
 ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
 < $(IOCSTARTUP)/preiocinit.cmd
