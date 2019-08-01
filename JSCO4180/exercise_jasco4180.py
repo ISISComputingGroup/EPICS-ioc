72b311567d09650fa897b062e89e24d153f220bc
aaronlong <aaron.long@stfc.ac.uk>
2019-08-01 12:25:01 +0100

    Wrapped exercise into a method with no. of iter argument

--- JSCO4180/exercise_jasco4180.py
+++ JSCO4180/exercise_jasco4180.py
@@ -1,22 +1,27 @@
 # Exercise the JASCO PU-4180
 from genie_python import genie as g
 
-attempts = 2000
-check_freq = attempts/10
+def exercise_pump(attempts=2000):
 
-g.set_pv("JSCO4180_01:COMP:A:SP", 25, is_local=True)
-g.set_pv("JSCO4180_01:COMP:B:SP", 25, is_local=True)
-g.set_pv("JSCO4180_01:COMP:C:SP", 25, is_local=True)
+    attempts = attempts
+    check_freq = attempts/10
 
-print("Starting crash test of JASCO PU-4180:")
-for i in range(attempts):
-    g.set_pv("JSCO4180_01:_EXERCISE", 1, is_local=True)
-    if i%check_freq == 0:
-        print("Current iteration: {0}".format(i))
-        alarm = g.get_pv("JSCO4180_01:COMP:A.STAT", is_local=True)
-        print("Alarm status: {alarm}".format(alarm=alarm))
-        if alarm == "TIMEOUT":
-            print("Record timeout - Device most likely crashed at iteration: {0}".format(i))
-            break
-else:
-    print("Script finished: Pump was not crashed")
+    g.set_pv("JSCO4180_01:COMP:A:SP", 25, is_local=True)
+    g.set_pv("JSCO4180_01:COMP:B:SP", 25, is_local=True)
+    g.set_pv("JSCO4180_01:COMP:C:SP", 25, is_local=True)
+
+    g.set_pv("JSCO4180_01:FLOWRATE:SP", 0.01, is_local=True)
+
+    print("Starting crash test of JASCO PU-4180:")
+    for i in range(attempts):
+        g.set_pv("JSCO4180_01:PUMP_FOR_TIME:SP", "Start", is_local=True)
+        if i%check_freq == 0:
+            print("Current iteration: {0}".format(i))
+            alarm = g.get_pv("JSCO4180_01:COMP:A.STAT", is_local=True)
+            print("Alarm status: {alarm}".format(alarm=alarm))
+            if alarm == "TIMEOUT":
+                print("Record timeout - Device most likely crashed at iteration: {0}".format(i))
+                break
+    else:
+        g.set_pv("JSCO4180_01:STOP:SP", "Stop", is_local=True)
+        print("Script finished: Pump was not crashed")
