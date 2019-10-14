d3c913859cda4627d72d994401515802907a9b69
JamesKingWork <JamesKingWork@ndlt1208.isis.cclrc.ac.uk>
2019-10-14 09:30:24 +0100

    Removed unused cmd code

--- OERCONE/iocBoot/iocOERCONE-IOC-01/st-common.cmd
+++ OERCONE/iocBoot/iocOERCONE-IOC-01/st-common.cmd
@@ -1,50 +1,6 @@
-# epicsEnvSet "STREAM_PROTOCOL_PATH" "$(OERCONE)/data"
-# epicsEnvSet "DEVICE" "L0"
-
-##ISIS## Run IOC initialisation 
-# < $(IOCSTARTUP)/init.cmd
-
-## Device simulation mode IP configuration
-# $(IFDEVSIM) drvAsynIPPortConfigure("$(DEVICE)", "localhost:$(EMULATOR_PORT=57677)")
-
-# ## For recsim:
-# $(IFRECSIM) drvAsynSerialPortConfigure("$(DEVICE)", "$(PORT=NUL)", 0, 1, 0, 0)
-
-# ## For real device:
-# $(IFNOTDEVSIM) $(IFNOTRECSIM) drvAsynSerialPortConfigure("$(DEVICE)", "$(PORT=NO_PORT_MACRO)", 0, 0, 0, 0)
-# $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)", -1, "baud", "$(BAUD=9600)")
-# $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)", -1, "bits", "$(BITS=8)")
-# $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)", -1, "parity", "$(PARITY=none)")
-# $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)", -1, "stop", "$(STOP=1)")
-# ## Hardware flow control off
-# $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)", 0, "clocal", "Y")
-# $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)",0,"crtscts","N")
-# ## Software flow control off
-# $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)",0,"ixon","N")
-# $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)",0,"ixoff","N")
-
+# Set up lua paths for access in the lua shell
 epicsEnvSet("LUA_PATH", "${UTILITIES}/lua")
 epicsEnvSet("LUA_SCRIPT_PATH","${TOP}/iocBoot/${IOC}")
+
+# Call into the lua shell to boot the ioc
 luash("st-common.lua")
-
-## Load record instances
-
-##ISIS## Load common DB records 
-# < $(IOCSTARTUP)/dbload.cmd
-
-# ## Load our record instances
-# dbLoadRecords("$(OERCONE)/db/oercone.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=$(DEVICE)")
-# dbLoadRecords("$(OERCONE)/db/unit_setter.db","P=$(MYPVPREFIX)$(IOCNAME):")
-
-
-# ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
-# < $(IOCSTARTUP)/preiocinit.cmd
-
-# cd "${TOP}/iocBoot/${IOC}"
-# iocInit
-
-# ## Start any sequence programs
-# #seq sncxxx,"user=ltu34219"
-
-# ##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
-# < $(IOCSTARTUP)/postiocinit.cmd
