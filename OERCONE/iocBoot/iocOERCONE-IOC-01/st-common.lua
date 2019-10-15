cbbb20dc25ce9b41540218924f0dde0d7460ba27
JamesKingWork <JamesKingWork@ndlt1208.isis.cclrc.ac.uk>
2019-10-15 11:12:15 +0100

    Conformed to luacheck guidelines using local, changed use of port macro

--- OERCONE/iocBoot/iocOERCONE-IOC-01/st-common.lua
+++ OERCONE/iocBoot/iocOERCONE-IOC-01/st-common.lua
@@ -10,23 +10,23 @@ local setSoftwareFlowControl = utils.setSoftwareFlowControl
 
 
 -- ## Get required macros and related vars
-oercone = getMacroValue{macro="OERCONE"}
-iocstartup = getMacroValue{macro="IOCSTARTUP"}
-emulator_port = getMacroValue{macro="EMULATOR_PORT", default="57677"}
+local oercone = getMacroValue{macro="OERCONE"}
+local iocstartup = getMacroValue{macro="IOCSTARTUP"}
+local emulator_port = getMacroValue{macro="EMULATOR_PORT", default="57677"}
 
 -- ## Set EPICS environment vars
-stream_protocol_path = string.format("%s/data", oercone)
-device = "L0"
+local device = "L0"
 
--- ##ISIS## Run IOC initialisation 
+-- ##ISIS## Run IOC initialisation
 iocsh.iocshLoad(string.format("%s/init.cmd", iocstartup))
 
 -- Get macros required and related vars that required init.cmd to run
-isDevsim = getMacroValue{macro="DEVSIM", default="0"} == "1"
-isRecsim = getMacroValue{macro="RECSIM", default="0"} == "1"
-pvprefix = string.format("%s", getMacroValue{macro="MYPVPREFIX"})
-disable = getMacroValue{macro="DISABLE", default="0"}
-iocname = getMacroValue{macro="IOCNAME"}
+local recsim = getMacroValue{macro="RECSIM", default="0"}
+local isRecsim = recsim == "1"
+local isDevsim = getMacroValue{macro="DEVSIM", default="0"} == "1"
+local pvprefix = string.format("%s", getMacroValue{macro="MYPVPREFIX"})
+local disable = getMacroValue{macro="DISABLE", default="0"}
+local iocname = getMacroValue{macro="IOCNAME"}
 
 -- ## Device simulation mode IP configuration
 if (isDevsim) then
@@ -34,22 +34,22 @@ if (isDevsim) then
 end
 
 -- ## For recsim:
-if (isRecsim) then 
-    port = getMacroValue{macro="PORT", default="NUL"}
+if (isRecsim) then
+    local port = getMacroValue{macro="PORT", default="NUL"}
     iocsh.drvAsynSerialPortConfigure(device, port, 0, 1 ,0 ,0)
 end
 
 
 -- ## For real device:
-if (not isRecsim and not isDevsim) then 
-    if (port == port_default) then port = "NO_PORT_MACRO" end
-    baud = getMacroValue{macro="BAUD", default="9600"}
-    bits = getMacroValue{macro="BITS", default="8"}
-    parity = getMacroValue{macro="PARITY", default="none"}
-    stop = getMacroValue{macro="STOP", default="1"}
+if (not isRecsim and not isDevsim) then
+    local port = getMacroValue{macro="PORT", default="NO_PORT_MACRO"}
+    local baud = getMacroValue{macro="BAUD", default="9600"}
+    local bits = getMacroValue{macro="BITS", default="8"}
+    local parity = getMacroValue{macro="PARITY", default="none"}
+    local stop = getMacroValue{macro="STOP", default="1"}
     setAsynOptions(device, port, baud, bits, parity, stop)
     -- Hardware flow control off
-    flowControlOn = false
+    local flowControlOn = false
     setHardwareFlowControl(device, flowControlOn)
     -- Software flow control off
     setSoftwareFlowControl(device, flowControlOn)
@@ -57,24 +57,24 @@ end
 
 -- ## Load record instances
 
--- ##ISIS## Load common DB records 
+-- ##ISIS## Load common DB records
 iocsh.iocshLoad(string.format("%s/dbload.cmd", iocstartup))
 
 -- ## Load our record instances
 iocsh.dbLoadRecords(
-    string.format("%s/db/oercone.db",oercone), 
+    string.format("%s/db/oercone.db",oercone),
     string.format("PVPREFIX=%s,P=%s%s:,RECSIM=%s,DISABLE=%s,PORT=%s",
      pvprefix, pvprefix, iocname, recsim, disable, device)
 )
 iocsh.dbLoadRecords(
-    string.format("%s/db/unit_setter.db", oercone), 
+    string.format("%s/db/unit_setter.db", oercone),
     string.format("P=%s%s:", pvprefix, iocname)
 )
 
--- ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
+-- ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called
 iocsh.iocshLoad(string.format("%s/preiocinit.cmd", iocstartup))
 
 iocsh.iocInit()
 
--- ##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
+-- ##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs
 iocsh.iocshLoad(string.format("%s/postiocinit.cmd", iocstartup))
