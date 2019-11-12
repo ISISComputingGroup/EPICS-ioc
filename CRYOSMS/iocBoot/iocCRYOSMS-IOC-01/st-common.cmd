271e2a8ff072fc28f4c02d5e8ccc4c8f96253d2d
ChrisColeExpControl <christopher.cole@stfc.ac.uk>
2019-11-12 12:09:46 +0000

    minor reworks

--- CRYOSMS/iocBoot/iocCRYOSMS-IOC-01/st-common.cmd
+++ CRYOSMS/iocBoot/iocCRYOSMS-IOC-01/st-common.cmd
@@ -1,7 +1,24 @@
 ##ISIS## Run IOC initialisation 
 < $(IOCSTARTUP)/init.cmd
+## For recsim:
+$(IFRECSIM) drvAsynSerialPortConfigure("L0", "$(PORT=NUL)", 0, 1, 0, 0)
 
-drvAsynIPPortConfigure("$(PORT=STREAM_PORT)", "localhost:$(EMULATOR_PORT=57677)")
+# For dev sim devices
+$(IFDEVSIM) drvAsynIPPortConfigure("L0", "localhost:$(EMULATOR_PORT=57677)")
+
+## For real device use:
+$(IFNOTDEVSIM) $(IFNOTRECSIM) drvAsynSerialPortConfigure("L0", "$(PORT=NO_PORT_MACRO)", 0, 0, 0, 0)
+$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0", -1, "baud", "$(BAUD=9600)")
+$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0", -1, "bits", "$(BITS=7)")
+$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0", -1, "parity", "$(PARITY=even)")
+$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0", -1, "stop", "$(STOP=1)")
+## Hardware flow control off
+$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0", 0, "clocal", "Y")
+$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0",0,"crtscts","N")
+## Software flow control off
+$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0",0,"ixon","N")
+$(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0",0,"ixoff","N")
+
 CRYOSMSConfigure("ASYN_PORT", "$(MYPVPREFIX)$(IOCNAME):")
 
 epicsEnvSet "STREAM_PROTOCOL_PATH" "$(CRYOSMS)/data"
@@ -12,7 +29,7 @@ epicsEnvSet "STREAM_PROTOCOL_PATH" "$(CRYOSMS)/data"
 ############################
 ## Load our record instances
 ############################
-dbLoadRecords("$(CRYOSMS)/db/cryosms.db","P=$(MYPVPREFIX)$(IOCNAME):,PORT=$(PORT=STREAM_PORT),ASYN_PORT=ASYN_PORT,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PROTO=$(PROTO=cryosms.proto)")
+dbLoadRecords("$(CRYOSMS)/db/cryosms.db","P=$(MYPVPREFIX)$(IOCNAME):,PORT=L0,ASYN_PORT=ASYN_PORT,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0)")
 
 ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
 < $(IOCSTARTUP)/preiocinit.cmd
