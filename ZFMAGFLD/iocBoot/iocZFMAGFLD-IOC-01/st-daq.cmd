0a5ae8280c02958f0097adb9b423aed5632fbbf7
Alistair McGann <alistair.mcgann@tessella.com>
2019-11-22 18:08:58 +0000

    Changed config to be like magnetometer

--- ZFMAGFLD/iocBoot/iocZFMAGFLD-IOC-01/st-daq.cmd
+++ ZFMAGFLD/iocBoot/iocZFMAGFLD-IOC-01/st-daq.cmd
@@ -1,18 +1,6 @@
-## Hostname of Instrument: cDAQ9185-MUONFE
-## Model: NI cDAQ-9185
-## Last updated: 2018-09-26
+epicsEnvSet("CDAQ","$(DAQ_AI_PORT_NAME=cDAQ9181-1B0C1FCMod1)")
 
-epicsEnvSet("CDAQAI","$(DAQ_AI_PORT_NAME=cDAQ9185-MUONFEMod3)")
-epicsEnvSet("CDAQAO","$(DAQ_AO_PORT_NAME=cDAQ9185-MUONFEMod4)")
-
-## input
-$(IFNOTRECSIM) DAQmxConfig("R0", "$(CDAQAI)/ai0", 0, "AI","N=1000 F=1000") ## Kicker Volt
-$(IFNOTRECSIM) DAQmxConfig("R0", "$(CDAQAI)/ai1", 1, "AI","N=1000 F=1000") ## Kicker Curr
-$(IFNOTRECSIM) DAQmxConfig("R0", "$(CDAQAI)/ai2", 2, "AI","N=$(NELM=100) F=$(FREQ=1000)") ## Separator Volt
-$(IFNOTRECSIM) DAQmxConfig("R0", "$(CDAQAI)/ai3", 3, "AI","N=$(NELM=100) F=$(FREQ=1000)" ## Separator Curr
-
-## output
-$(IFNOTRECSIM) DAQmxConfig("W0", "$(CDAQAO)/ao0", 0, "AO","N=1 F=0") ## x
-$(IFNOTRECSIM) DAQmxConfig("W0", "$(CDAQAO)/ao1", 1, "AO","N=1 F=0") ## x
-$(IFNOTRECSIM) DAQmxConfig("W0", "$(CDAQAO)/ao2", 2, "AO","N=1 F=0") ## Separator Volt
-$(IFNOTRECSIM) DAQmxConfig("W0", "$(CDAQAO)/ao3", 3, "AO","N=1 F=0") ## Separator Curr
+## 3xAI inputs from a cDAQ 9181, one input per magnetometer channel
+$(IFNOTRECSIM) DAQmxConfig("R0", "$(CDAQ)/ai0", 0, "AI","OneShot TerminalDiff N=1 F=0") # X (L)
+$(IFNOTRECSIM) DAQmxConfig("R0", "$(CDAQ)/ai1", 1, "AI","OneShot TerminalDiff N=1 F=0") # Y (T)
+$(IFNOTRECSIM) DAQmxConfig("R0", "$(CDAQ)/ai2", 2, "AI","OneShot TerminalDiff N=1 F=0") # Z (V)
