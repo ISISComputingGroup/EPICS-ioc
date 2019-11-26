21727032ff94739f65d41b4e30bc007622346c15
Alistair McGann <alistair.mcgann@tessella.com>
2019-11-26 11:51:27 +0000

    Add db file for loading sensor matrix elements

--- ZFMAGFLD/iocBoot/iocZFMAGFLD-IOC-01/st.cmd
+++ ZFMAGFLD/iocBoot/iocZFMAGFLD-IOC-01/st.cmd
@@ -37,6 +37,7 @@ $(IFRECSIM) drvAsynSerialPortConfigure("$(DEVICE)", "$(PORT=NUL)", 0, 1, 0, 0)
 ## Load our record instances
 dbLoadRecords("$(ZFMAGFLD)/db/zfmagfld.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=$(DEVICE)")
 dbLoadRecords("$(ZFMAGFLD)/db/zfmagfld_axes.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=$(DEVICE),IFNOTRECSIM=$(IFNOTRECSIM),IFRECSIM=$(IFRECSIM),OFFSET_X=$(OFFSET_X),OFFSET_Y=$(OFFSET_Y),OFFSET_Z=$(OFFSET_Z)")
+dbLoadRecords("$(ZFMAGFLD)/db/zfmagfld_sensor_matrix.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):")
 
 
 ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
