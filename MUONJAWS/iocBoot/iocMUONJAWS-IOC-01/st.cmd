ac9ff4f011fd9ed4fe9871aa8378670d96630efb
David Keymer <david.keymer@stfc.ac.uk>
2019-12-11 14:21:59 +0000

    Initial commit of IOC directory structure

---
+++ MUONJAWS/iocBoot/iocMUONJAWS-IOC-01/st.cmd
@@ -0,0 +1,37 @@
+#!../../bin/windows-x64/MUONJAWS-IOC-01
+## You may have to change MUONJAWS-IOC-01 to something else
+## everywhere it appears in this file
+
+# Increase this if you get <<TRUNCATED>> or discarded messages warnings in your errlog output
+errlogInit2(65536, 256)
+
+< envPaths
+
+cd ${TOP}
+
+epicsEnvSet(IOC_NUM,1)
+## Register all support components
+dbLoadDatabase "dbd/MUONJAWS-IOC-01.dbd"
+MUONJAWS_IOC_01_registerRecordDeviceDriver pdbbase
+
+##ISIS## Run IOC initialisation 
+< $(IOCSTARTUP)/init.cmd
+
+## Load record instances
+
+##ISIS## Load common DB records 
+< $(IOCSTARTUP)/dbload.cmd
+
+dbLoadRecords("db/MuonJaws.db", "P=$(MYPVPREFIX)$(IOCNAME):")
+
+##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
+< $(IOCSTARTUP)/preiocinit.cmd
+
+cd ${TOP}/iocBoot/${IOC}
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=hgv27692Host"
+
+##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
+< $(IOCSTARTUP)/postiocinit.cmd
