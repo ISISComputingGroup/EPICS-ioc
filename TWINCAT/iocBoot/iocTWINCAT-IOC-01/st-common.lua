da04e588a3740033fb3396d6699b72006e538f81
Dominic Oram <dominic.oram@stfc.ac.uk>
2019-12-17 18:38:41 +0000

    Added option to use different versions of PLC code

--- TWINCAT/iocBoot/iocTWINCAT-IOC-01/st-common.lua
+++ TWINCAT/iocBoot/iocTWINCAT-IOC-01/st-common.lua
@@ -1,18 +1,28 @@
-motor_port = "L0"
-num_axes = 8
-pv_prefix = os.getenv("MYPVPREFIX")
-tpy_file = os.getenv("TPY_FILE")
+package.path = package.path .. ';' .. os.getenv("UTILITIES") .. '/lua/luaUtils.lua;'
+local utils = require("luaUtils")
+local getMacroValue = utils.getMacroValue
 
-iocsh.tcSetScanRate(150, 5)
-iocsh.tcLoadRecords (tpy_file, string.format("-eo -devtc -p %s", pv_prefix))
+function twincat_stcommon_main()
+	local motor_port = "L0"
+	local num_axes = 8
+	local pv_prefix = getMacroValue{macro="MYPVPREFIX"}
+	local tpy_file = getMacroValue{macro="TPY_FILE"}
+	local plc_version = getMacroValue{macro="PLC_VERSION", default="0"}
 
-iocsh.devMotorCreateController(motor_port, "Controller", num_axes, pv_prefix)
+	iocsh.tcSetScanRate(150, 2)
+	iocsh.tcLoadRecords (tpy_file, string.format("-eo -devtc -p %s", pv_prefix))
 
-for axis_num=1,num_axes,1
-do
-    motor_pv = string.format("MTR%02i%02i", os.getenv("MTRCTRL"), axis_num)
-    single_axis_db = "db/single_axis.db"
-    db_args = string.format("MYPVPREFIX=%s,MOTOR_PV=%s,MOTOR_PORT=%s,ADDR=%s", pv_prefix, motor_pv, motor_port, axis_num-1)
-	iocsh.devMotorCreateAxis(motor_port, axis_num-1)
-	iocsh.dbLoadRecords(single_axis_db, db_args)
+	iocsh.devMotorCreateController(motor_port, "Controller", num_axes, pv_prefix)
+
+	for axis_num=1,num_axes,1
+	do
+		motor_pv = string.format("MTR%02i%02i", os.getenv("MTRCTRL"), axis_num)
+		single_axis_db = "db/single_axis.db"
+		db_args = string.format("MYPVPREFIX=%s,MOTOR_PV=%s,MOTOR_PORT=%s,ADDR=%s", pv_prefix, motor_pv, motor_port, axis_num-1)
+		iocsh.devMotorCreateAxis(motor_port, axis_num-1, plc_version)
+		iocsh.dbLoadRecords(single_axis_db, db_args)
+	end
+
 end
+
+twincat_stcommon_main()
