5b7ee8817f232f754c4caad9a991ebf46831c59a
John-Holt-Tessella <John.Holt@tessella.com>
2020-01-10 16:54:42 +0000

    Use st-common update config with default values

--- MERCURY_ITC/iocBoot/iocMERCURY-IOC-02/st.cmd
+++ MERCURY_ITC/iocBoot/iocMERCURY-IOC-02/st.cmd
@@ -8,7 +8,7 @@ errlogInit2(65536, 256)
 
 < envPaths
 
-cd ${TOP}
+cd "${TOP}"
 
 epicsEnvSet(IOC_NUM,2)
 
@@ -16,34 +16,4 @@ epicsEnvSet(IOC_NUM,2)
 dbLoadDatabase "dbd/MERCURY-IOC-02.dbd"
 MERCURY_IOC_02_registerRecordDeviceDriver pdbbase
 
-##ISIS## Run IOC initialisation 
-< $(IOCSTARTUP)/init.cmd
-
-## Load record instances
-
-##ISIS## Load common DB records 
-< $(IOCSTARTUP)/dbload.cmd
-
-epicsEnvSet(TEMP_NUM,1)
-< $(MERCURY_ITC)/iocBoot/iocMercuryiTC/st-temp.cmd
-
-epicsEnvSet(TEMP_NUM,2)
-< $(MERCURY_ITC)/iocBoot/iocMercuryiTC/st-temp.cmd
-
-epicsEnvSet(TEMP_NUM,3)
-< $(MERCURY_ITC)/iocBoot/iocMercuryiTC/st-temp.cmd
-
-epicsEnvSet(TEMP_NUM,4)
-< $(MERCURY_ITC)/iocBoot/iocMercuryiTC/st-temp.cmd
-
-##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
-< $(IOCSTARTUP)/preiocinit.cmd
-
-cd ${TOP}/iocBoot/${IOC}
-iocInit
-
-## Start any sequence programs
-#seq sncxxx,"user=hgv27692Host"
-
-##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
-< $(IOCSTARTUP)/postiocinit.cmd
+< $(MERCURY_ITC)/iocBoot/iocMercuryiTC/st-common.cmd
