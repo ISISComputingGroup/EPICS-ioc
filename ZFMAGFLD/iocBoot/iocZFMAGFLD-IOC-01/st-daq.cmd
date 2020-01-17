de74de5298a4c20d01b20933b4f19ebdf6bba50e
Tom Willemsen <tomwillemsen1995@gmail.com>
2020-01-17 15:54:12 +0000

    Make axes configurable

--- ZFMAGFLD/iocBoot/iocZFMAGFLD-IOC-01/st-daq.cmd
+++ ZFMAGFLD/iocBoot/iocZFMAGFLD-IOC-01/st-daq.cmd
@@ -1,6 +1,6 @@
 epicsEnvSet("CDAQ","$(HOST=cDAQ9181-1B0C1FCMod1)")
 
 ## 3xAI inputs from a cDAQ 9181, one input per magnetometer channel
-$(IFNOTRECSIM) DAQmxConfig("R0", "$(CDAQ)/ai0", 0, "AI","MONSTER TerminalDiff N=1 F=1000") # X (L)
-$(IFNOTRECSIM) DAQmxConfig("R0", "$(CDAQ)/ai1", 1, "AI","MONSTER TerminalDiff N=1 F=1000") # Y (T)
-$(IFNOTRECSIM) DAQmxConfig("R0", "$(CDAQ)/ai2", 2, "AI","MONSTER TerminalDiff N=1 F=1000") # Z (V)
+$(IFNOTRECSIM) DAQmxConfig("R0", "$(CDAQ)/$(X_DAQ_CHANNEL=ai0)", 0, "AI","MONSTER TerminalDiff N=1 F=1000") # X (horizontal transverse - parallel to the floor)
+$(IFNOTRECSIM) DAQmxConfig("R0", "$(CDAQ)/$(Y_DAQ_CHANNEL=ai1)", 1, "AI","MONSTER TerminalDiff N=1 F=1000") # Y (vertical transverse - perpendicular to the floor)
+$(IFNOTRECSIM) DAQmxConfig("R0", "$(CDAQ)/$(Z_DAQ_CHANNEL=ai2)", 2, "AI","MONSTER TerminalDiff N=1 F=1000") # Z (along the beam, in the direction of the beam)
