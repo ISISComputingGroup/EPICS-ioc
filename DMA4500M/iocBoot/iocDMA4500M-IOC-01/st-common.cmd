8edb00c0d07665e7c4cfbe8cbe3f72f902ea272f
Cosimo Campo <cosimo.campo@stfc.ac.uk>
2020-03-04 14:29:59 +0000

    Add DB for alarm propagation

--- DMA4500M/iocBoot/iocDMA4500M-IOC-01/st-common.cmd
+++ DMA4500M/iocBoot/iocDMA4500M-IOC-01/st-common.cmd
@@ -30,6 +30,7 @@ $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)",0,"ixoff","N")
 
 ## Load our record instances
 dbLoadRecords("$(DMA4500M)/db/dma4500m.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME),RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=$(DEVICE),MEASUREMENT_MODE=DENSITY_ONLY")
+dbLoadRecords("$(DMA4500M)/db/dma4500m_error_setter.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME),RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=$(DEVICE)")
 
 ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
 < $(IOCSTARTUP)/preiocinit.cmd
