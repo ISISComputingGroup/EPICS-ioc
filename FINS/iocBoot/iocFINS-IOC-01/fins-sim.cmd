bcb070462643b5f52decaf00e11f0b8c7e593570
Alexandru Dascalu <965337@swansea.ac.uk>
2020-04-28 08:07:59 +0100

    added default plc node macro to example cmd

--- FINS/iocBoot/iocFINS-IOC-01/fins-sim.cmd
+++ FINS/iocBoot/iocFINS-IOC-01/fins-sim.cmd
@@ -1,6 +1,6 @@
 ## Init and connect
 ## 1 as argument 4 enables simulation mode
-finsUDPInit("PLC", "$(PLCIP)", "TCP", 1)
+finsUDPInit("PLC", "$(PLCIP)", "TCP", 1, "$(PLCNODE=)")
 
 ## Load our record instances
 dbLoadRecords("${TOP}/db/larmor-air.db","P=$(MYPVPREFIX),Q=$(IOCNAME):BENCH:")
