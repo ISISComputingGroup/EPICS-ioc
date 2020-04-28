596d8cbddb96156e619e6b9f74de73183090710d
Alexandru Dascalu <965337@swansea.ac.uk>
2020-04-28 08:11:44 +0100

    added comment explaining what fins sim is

--- FINS/iocBoot/iocFINS-IOC-01/fins-sim.cmd
+++ FINS/iocBoot/iocFINS-IOC-01/fins-sim.cmd
@@ -1,3 +1,5 @@
+#This is an example cmd file that should be in the FINS configuration folder under Settings\config\Instrument_name
+
 ## Init and connect
 ## 1 as argument 4 enables simulation mode
 finsUDPInit("PLC", "$(PLCIP)", "TCP", 1, "$(PLCNODE=)")
