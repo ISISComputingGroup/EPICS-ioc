c8a9f44969e28accbe47fae4c2c72afe5deeb7ae
Alexandru Dascalu <965337@swansea.ac.uk>
2020-05-28 13:07:02 +0100

    Revert "added settings for helium recovery"
    This reverts commit 67db7295b657286b443f00426f6c5088cdabca8f.

--- FINS/exampleSettings/HELIUM_RECOVERY/FINS_01.cmd
+++ FINS/exampleSettings/HELIUM_RECOVERY/FINS_01.cmd
@@ -1,5 +1,5 @@
 #Init and connect
-finsUDPInit("PLC", "$(PLCIP)", "TCP", 0, "$(PLCNODE=)")
+finsUDPInit("PLC", "$(PLCIP)", "UDP", 0, "$(PLCNODE=)")
 
 ## Load our record instances
 dbLoadRecords("${TOP}/db/he-recovery.db","P=$(MYPVPREFIX),Q=$(IOCNAME):HE-RCVRY:")
