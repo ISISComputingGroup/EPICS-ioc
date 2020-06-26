e7a0c4d84741d8999c6a962f330bc269f449da6a
Alexandru Dascalu <965337@swansea.ac.uk>
2020-06-26 11:44:07 +0100

    corrected macro names

--- FINS/exampleSettings/HELIUM_RECOVERY/FINS_01.cmd
+++ FINS/exampleSettings/HELIUM_RECOVERY/FINS_01.cmd
@@ -1,7 +1,7 @@
 #Init and connect
-$(IFDEVSIM) finsUDPInit("PLC", "$(PLCIP):$(EMULATOR_PORT=)", "TCPNOHEAD", 0, "$(PLCNODE=)")
-$(IFRECSIM) finsUDPInit("PLC", "$(PLCIP):$(EMULATOR_PORT=)", "TCPNOHEAD", 1, "$(PLCNODE=)")
-$(IFNOTDEVSIM) $(IFNOTRECSIM) finsUDPInit("PLC", "$(PLCIP)", "TCP", 0, "$(PLCNODE=)")
+$(IFDEVSIM) finsUDPInit("PLC", "$(PLC_IP):$(EMULATOR_PORT=)", "TCPNOHEAD", 0, "$(PLC_NODE=)")
+$(IFRECSIM) finsUDPInit("PLC", "$(PLC_IP):$(EMULATOR_PORT=)", "TCPNOHEAD", 1, "$(PLC_NODE=)")
+$(IFNOTDEVSIM) $(IFNOTRECSIM) finsUDPInit("PLC", "$(PLC_IP)", "TCP", 0, "$(PLC_NODE=)")
 
 ## Load our record instances
 dbLoadRecords("${TOP}/db/he-recovery.db","P=$(MYPVPREFIX),Q=$(IOCNAME):HE_RCVRY:,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),SCAN=1 second")
