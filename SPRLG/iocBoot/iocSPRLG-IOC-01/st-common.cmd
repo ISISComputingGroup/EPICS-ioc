ab4ddf4e294c80d454dff0a8eb6ec5a2764deff5
Tom Willemsen <tomwillemsen1995@gmail.com>
2020-06-30 17:11:24 +0100

    Add dbpf for record existing or not

--- SPRLG/iocBoot/iocSPRLG-IOC-01/st-common.cmd
+++ SPRLG/iocBoot/iocSPRLG-IOC-01/st-common.cmd
@@ -30,9 +30,6 @@ $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0",0,"ixoff","N")
 
 ## Load our record instances
 
-# Top-level DBs e.g. DISABLE, SIM, which channels are present
-dbLoadRecords("${SPRLG}/db/superlogics.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0, RECSIM=$(RECSIM=0), DISABLE=$(DISABLE=0)")
-
 # Address 01 has 1 channel, and is not necessarily present on all instruments
 stringiftest("CHANNEL1", "$(HAS_CHAN1=Y)", 5, "Y")
 $(IFCHANNEL1) dbLoadRecords("${SPRLG}/db/superlogics_address.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0, RECSIM=$(RECSIM=0), DISABLE=$(DISABLE=0),ADDR=01,COUNT=1,UNITS=V")
@@ -42,11 +39,17 @@ stringiftest("CHANNEL2", "$(HAS_CHAN2=Y)", 5, "Y")
 $(IFCHANNEL2) dbLoadRecords("${SPRLG}/db/superlogics_address.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0, RECSIM=$(RECSIM=0), DISABLE=$(DISABLE=0),ADDR=02,COUNT=8,UNITS=C")
 $(IFCHANNEL2) dbLoadRecords("${SPRLG}/db/superlogics_channel.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0, RECSIM=$(RECSIM=0), DISABLE=$(DISABLE=0),ADDR=02,UNITS=C")
 
+# Top-level DBs e.g. DISABLE, SIM, which channels are present
+dbLoadRecords("${SPRLG}/db/superlogics.db","P=$(MYPVPREFIX)$(IOCNAME):, PORT=L0, RECSIM=$(RECSIM=0), DISABLE=$(DISABLE=0),HAS_CHAN1=$(HAS_CHAN1=Y),HAS_CHAN2=$(HAS_CHAN2=Y)")
+
 ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
 < $(IOCSTARTUP)/preiocinit.cmd
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
+
+dbpf $(MYPVPREFIX)$(IOCNAME):HAS_CHAN1 $(HAS_CHAN1=Y)
+dbpf $(MYPVPREFIX)$(IOCNAME):HAS_CHAN2 $(HAS_CHAN2=Y)
 
 ## Start any sequence programs
 #seq sncxxx,"user=hgv27692Host"
