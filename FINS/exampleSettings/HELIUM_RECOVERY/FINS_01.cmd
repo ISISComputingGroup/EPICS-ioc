d32cf1bbcb3588f34e551f733b354389cc043749
Alexandru Dascalu <965337@swansea.ac.uk>
2020-07-07 19:37:29 +0100

    added units and changed pv prefix

--- FINS/exampleSettings/HELIUM_RECOVERY/FINS_01.cmd
+++ FINS/exampleSettings/HELIUM_RECOVERY/FINS_01.cmd
@@ -4,4 +4,4 @@ $(IFRECSIM) finsUDPInit("PLC", "$(PLC_IP):$(EMULATOR_PORT=)", "TCPNOHEAD", 1, "$
 $(IFNOTDEVSIM) $(IFNOTRECSIM) finsUDPInit("PLC", "$(PLC_IP)", "TCP", 0, "$(PLC_NODE=)")
 
 ## Load our record instances
-dbLoadRecords("${TOP}/db/he-recovery.db","P=$(MYPVPREFIX),Q=$(IOCNAME):HE_RCVRY:,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),SCAN=1 second")
+dbLoadRecords("${TOP}/db/he-recovery.db","P=$(MYPVPREFIX),Q=HLM:,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),SCAN=1 second")
