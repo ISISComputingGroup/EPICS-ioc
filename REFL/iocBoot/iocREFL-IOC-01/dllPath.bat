b4f30c4303b9c2cd990480f57ae90f0af37afc61
John-Holt-Tessella <John.Holt@tessella.com>
2020-07-20 20:09:28 +0100

    Make arctivecture independent

--- REFL/iocBoot/iocREFL-IOC-01/dllPath.bat
+++ REFL/iocBoot/iocREFL-IOC-01/dllPath.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-SET "PATH=C:/Instrument/Apps/EPICS/support/icpconfig/master/bin/windows-x64;C:/Instrument/Apps/EPICS/support/pugixml/master/bin/windows-x64;C:/Instrument/Apps/EPICS/support/utilities/master/bin/windows-x64;C:/Instrument/Apps/EPICS/support/pcre/master/bin/windows-x64;C:/Instrument/Apps/EPICS/base/master/bin/windows-x64;%PATH%"
+SET "PATH=C:/Instrument/Apps/EPICS/support/icpconfig/master/bin/%EPICS_HOST_ARCH%;C:/Instrument/Apps/EPICS/support/pugixml/master/bin/%EPICS_HOST_ARCH%;C:/Instrument/Apps/EPICS/support/utilities/master/bin/%EPICS_HOST_ARCH%;C:/Instrument/Apps/EPICS/support/pcre/master/bin/%EPICS_HOST_ARCH%;C:/Instrument/Apps/EPICS/base/master/bin/%EPICS_HOST_ARCH%;%PATH%"
