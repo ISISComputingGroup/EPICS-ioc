f2c918449e69c39ad88b13e908362342b6c1a395
JamesKingWork <james.c.king@stfc.ac.uk>
2020-10-07 14:00:29 +0100

    Remove misleading echo and accidentally added section

--- LSICORR/iocBoot/iocLSICORR-IOC-02/runIOC.bat
+++ LSICORR/iocBoot/iocLSICORR-IOC-02/runIOC.bat
@@ -2,20 +2,6 @@
 setlocal
 set MYDIRBLOCK=%~dp0
 call C:\Instrument\Apps\EPICS\config_env_base.bat
-
-%HIDEWINDOW% h
-
-set EPICS_CAS_INTF_ADDR_LIST=127.0.0.1
-set EPICS_CAS_BEACON_ADDR_LIST=127.255.255.255
-
-set PYTHONUNBUFFERED=TRUE
-
-%PYTHON3W% %EPICS_KIT_ROOT%/support/lsicorr/master/LSi_Correlator.py --pv_prefix %MYPVPREFIX% --ioc_number 2
-
-@echo off
-setlocal
-set MYDIRBLOCK=%~dp0
-call C:\Instrument\Apps\EPICS\config_env_base.bat
 call dllPath.bat
 %HIDEWINDOW% h
 
@@ -34,6 +20,5 @@ if "%REFL_MACROS%"=="" (
 ) else (
     echo Macros already defined
 )
-echo Macro JSON is %REFL_MACROS%
 
 %PYTHON3W% %EPICS_KIT_ROOT%/support/lsicorr/master/correlator_pcaspy.py --pv_prefix %MYPVPREFIX% --ioc_number 2
