a38379135dd5df2706487542a0c04e9b2a74dce9
Tom Willemsen <tomwillemsen1995@gmail.com>
2020-12-04 16:16:01 +0000

    Add ability to specify alarm thresholds

--- ILM200/iocBoot/iocILM200-IOC-01/st-common.cmd
+++ ILM200/iocBoot/iocILM200-IOC-01/st-common.cmd
@@ -32,7 +32,7 @@ $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0",0,"ixoff","N")
 
 ## Load our record instances
 dbLoadRecords("${TOP}/db/ILM200_common.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=$(DEVICE),ISO=$(ISOBUS=1)")
-dbLoadRecordsLoop("${TOP}/db/ILM200_channel.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,N=\$(N), PORT=$(DEVICE), ISO=$(ISOBUS=1)", "N", 1, 3, 1)
+dbLoadRecordsLoop("${TOP}/db/ILM200_channel.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,N=\$(N), PORT=$(DEVICE), ISO=$(ISOBUS=1),CH1_ALARM_THRESHOLD=$(CH1_ALARM_THRESHOLD=0.0),CH2_ALARM_THRESHOLD=$(CH2_ALARM_THRESHOLD=0.0),CH3_ALARM_THRESHOLD=$(CH3_ALARM_THRESHOLD=0.0)", "N", 1, 3, 1)
 
 ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
 < $(IOCSTARTUP)/preiocinit.cmd
