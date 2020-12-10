072069e1b3a0de06583e1b07aa8f51ae91fe6f52
Dominic Oram <dominic.oram@stfc.ac.uk>
2020-12-10 18:55:54 +0000

    Final tidying up

--- HVCAENA/iocBoot/iocHVCAENA-IOC-01/st.cmd
+++ HVCAENA/iocBoot/iocHVCAENA-IOC-01/st.cmd
@@ -14,25 +14,23 @@ HVCAENA_IOC_01_registerRecordDeviceDriver pdbbase
 ##ISIS## Run IOC initialisation 
 < $(IOCSTARTUP)/init.cmd
 
-stringiftest("IP0Present", "$(HVCAENIP0="")", 13, "")
+CAENHVAsynReadOnly($(READ_ONLY=0))
+
+stringiftest("IP0Present", "$(IP_ADDRESS_0="")", 13, "")
 $(IFIP0Present) CAENHVAsynSetEpicsPrefix("$(MYPVPREFIX)$(IOCNAME):HV0:")
-$(IFIP0Present) CAENHVAsynConfig("HV0",2,"$(HVCAENIP0)","admin","admin")
+$(IFIP0Present) CAENHVAsynConfig("HV0",$(SYS_TYPE_0),"$(IP_ADDRESS_0)","admin","admin")
 
-stringiftest("IP1Present", "$(HVCAENIP1="")", 13, "")
+stringiftest("IP1Present", "$(IP_ADDRESS_1="")", 13, "")
 $(IFIP1Present) CAENHVAsynSetEpicsPrefix("$(MYPVPREFIX)$(IOCNAME):HV1:")
-$(IFIP1Present) CAENHVAsynConfig("HV1",2,"$(HVCAENIP1)","admin","admin")
+$(IFIP1Present) CAENHVAsynConfig("HV1",$(SYS_TYPE_1),"$(IP_ADDRESS_1)","admin","admin")
 
-stringiftest("IP2Present", "$(HVCAENIP2="")", 13, "")
+stringiftest("IP2Present", "$(IP_ADDRESS_2="")", 13, "")
 $(IFIP2Present) CAENHVAsynSetEpicsPrefix("$(MYPVPREFIX)$(IOCNAME):HV2:")
-$(IFIP2Present) CAENHVAsynConfig("HV2",2,"$(HVCAENIP2)","admin","admin")
+$(IFIP2Present) CAENHVAsynConfig("HV2",$(SYS_TYPE_2),"$(IP_ADDRESS_2)","admin","admin")
 
-stringiftest("IP3Present", "$(HVCAENIP3="")", 13, "")
+stringiftest("IP3Present", "$(IP_ADDRESS_3="")", 13, "")
 $(IFIP3Present) CAENHVAsynSetEpicsPrefix("$(MYPVPREFIX)$(IOCNAME):HV3:")
-$(IFIP3Present) CAENHVAsynConfig("HV3",2,"$(HVCAENIP3)","admin","admin")
-
-stringiftest("IP4Present", "$(HVCAENIP4="")", 13, "")
-$(IFIP4Present) CAENHVAsynSetEpicsPrefix("$(MYPVPREFIX)$(IOCNAME):HV4:")
-$(IFIP4Present) CAENHVAsynConfig("HV4",2,"$(HVCAENIP4)","admin","admin")
+$(IFIP3Present) CAENHVAsynConfig("HV3",$(SYS_TYPE_3),"$(IP_ADDRESS_3)","admin","admin")
 
 ##ISIS## Load common DB records 
 < $(IOCSTARTUP)/dbload.cmd
