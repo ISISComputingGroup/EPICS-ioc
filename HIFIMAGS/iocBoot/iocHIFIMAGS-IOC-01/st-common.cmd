4e2529bc77d2bd5ec9939915831b3c7cadea2f43
Kathryn Baker <kathryn.baker@stfc.ac.uk>
2021-01-06 18:13:07 +0000

    Adding extras for main PSU

--- HIFIMAGS/iocBoot/iocHIFIMAGS-IOC-01/st-common.cmd
+++ HIFIMAGS/iocBoot/iocHIFIMAGS-IOC-01/st-common.cmd
@@ -35,6 +35,7 @@ $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)",0,"ixoff","N")
 dbLoadRecords("$(TOP)/db/hifimags.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0)")
 dbLoadRecords("$(TOP)/db/hifimags_magnet.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),X_IOC=$(X_IOC=),Y_IOC=$(Y_IOC=),Z_IOC=$(Z_IOC=),M_IOC=$(M_IOC=)")
 dbLoadRecords("$(TOP)/db/switching.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),MAG=$(MAG=Z),PSU_IOC=$(Z_IOC=))")
+dbLoadRecords("$(TOP)/db/mainpsu.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),MAG=$(MAG=M),PSU_IOC=$(M_IOC=))")
 
 ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
 < $(IOCSTARTUP)/preiocinit.cmd
