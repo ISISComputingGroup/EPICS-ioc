4cb2d801b66eca7bcc45e3e4e834deaafb86785b
Kathryn Baker <kathryn.baker@stfc.ac.uk>
2021-01-11 11:52:42 +0000

    Adding Hall Sensors

--- HIFIMAGS/iocBoot/iocHIFIMAGS-IOC-01/st-common.cmd
+++ HIFIMAGS/iocBoot/iocHIFIMAGS-IOC-01/st-common.cmd
@@ -32,7 +32,7 @@ $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)",0,"ixoff","N")
 < $(IOCSTARTUP)/dbload.cmd
 
 ## Load our record instances
-dbLoadRecords("$(TOP)/db/hifimags.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0)")
+dbLoadRecords("$(TOP)/db/hifimags.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),GM_IOC=$(GM_IOC=)")
 dbLoadRecords("$(TOP)/db/hifimags_magnet.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),X_IOC=$(X_IOC=),Y_IOC=$(Y_IOC=),Z_IOC=$(Z_IOC=),M_IOC=$(M_IOC=)")
 dbLoadRecords("$(TOP)/db/switching.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),MAG=$(MAG=Z),PSU_IOC=$(Z_IOC=))")
 dbLoadRecords("$(TOP)/db/mainpsu.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),MAG=$(MAG=M),PSU_IOC=$(M_IOC=))")
