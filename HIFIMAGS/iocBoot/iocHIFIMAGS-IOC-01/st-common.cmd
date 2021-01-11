39bf23bf5f2942065eca294da28e8e99117c9af3
Kathryn Baker <kathryn.baker@stfc.ac.uk>
2021-01-11 15:32:33 +0000

    AAdding Temperatures

--- HIFIMAGS/iocBoot/iocHIFIMAGS-IOC-01/st-common.cmd
+++ HIFIMAGS/iocBoot/iocHIFIMAGS-IOC-01/st-common.cmd
@@ -36,6 +36,7 @@ dbLoadRecords("$(TOP)/db/hifimags.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(I
 dbLoadRecords("$(TOP)/db/hifimags_magnet.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),X_IOC=$(X_IOC=),Y_IOC=$(Y_IOC=),Z_IOC=$(Z_IOC=),M_IOC=$(M_IOC=)")
 dbLoadRecords("$(TOP)/db/switching.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),MAG=$(MAG=Z),PSU_IOC=$(Z_IOC=))")
 dbLoadRecords("$(TOP)/db/mainpsu.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),MAG=$(MAG=M),PSU_IOC=$(M_IOC=))")
+dbLoadRecords("$(TOP)/db/hifimags_temp.db","PVPREFIX=$(MYPVPREFIX),P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),STAGE1=$(STAGE1=), SHIELD=$(SHIELD=), SWITCH=$(SWITCH=), STAGE2A=$(STAGE2A=), STAGE2B=$(STAGE2B=), INRABAS=$(INRABAS=))")
 
 ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
 < $(IOCSTARTUP)/preiocinit.cmd
