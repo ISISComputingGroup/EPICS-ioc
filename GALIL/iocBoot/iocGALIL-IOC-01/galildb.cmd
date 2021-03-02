013ec83b25fb4de82c201f1d48629b2140dbee43
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2021-03-02 11:23:17 +0000

    Add missing csmotor Db

--- GALIL/iocBoot/iocGALIL-IOC-01/galildb.cmd
+++ GALIL/iocBoot/iocGALIL-IOC-01/galildb.cmd
@@ -12,17 +12,20 @@ dbLoadRecords("$(TOP)/db/galil_dmc_ctrl.db", "P=$(MYPVPREFIX)MOT:,CCP=$(MTRCTRL)
 #Load extra features for real axis/motors (eg. Motor type, encoder type)
 dbLoadRecords("$(TOP)/db/galil_motor_extras.db", "P=$(MYPVPREFIX)MOT:,CCP=$(MTRCTRL),PVPREFIX=$(MYPVPREFIX),ASG01=$(ASG01=DEFAULT),ASG02=$(ASG02=DEFAULT),ASG03=$(ASG03=DEFAULT),ASG04=$(ASG04=DEFAULT),ASG05=$(ASG05=DEFAULT),ASG06=$(ASG06=DEFAULT),ASG07=$(ASG07=DEFAULT),ASG08=$(ASG08=DEFAULT)")
 
-#Load digital IO databases
-dbLoadRecords("$(TOP)/db/galil_digital_ports.db", "P=$(MYPVPREFIX)MOT:,CCP=$(MTRCTRL)")
-
-#Load analog IO databases
-dbLoadRecords("$(TOP)/db/galil_analog_ports.db", "P=$(MYPVPREFIX)MOT:,CCP=$(MTRCTRL)")
+#Load extra features for CS axis/motors (eg. Setpoint monitor)
+dbLoadRecords("$(TOP)/db/galil_csmotor_extras.db", "P=$(MYPVPREFIX)MOT:,CCP=$(MTRCTRL)")
 
 #Load kinematics for CS axis/motors (eg. Forward and reverse kinematics, kinematic variables)
 dbLoadRecords("$(TOP)/db/galil_csmotor_kinematics.db", "P=$(MYPVPREFIX)MOT:,CCP=$(MTRCTRL)")
 
 #Load coordinate system features (eg. Coordinate system S and T status, motor list, segments processed, moving status)
 dbLoadRecords("$(TOP)/db/galil_coordinate_systems.db", "P=$(MYPVPREFIX)MOT:,CCP=$(MTRCTRL)")
+
+#Load digital IO databases
+dbLoadRecords("$(TOP)/db/galil_digital_ports.db", "P=$(MYPVPREFIX)MOT:,CCP=$(MTRCTRL)")
+
+#Load analog IO databases
+dbLoadRecords("$(TOP)/db/galil_analog_ports.db", "P=$(MYPVPREFIX)MOT:,CCP=$(MTRCTRL)")
 
 #Load user defined functions
 dbLoadRecords("$(TOP)/db/galil_userdef_records8.db", "P=$(MYPVPREFIX)MOT:,CCP=$(MTRCTRL)")
