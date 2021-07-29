9012ff084dc5f4e5313a6026f9297f2626086c2a
daryakoskeroglu <daryak09@gmail.com>
2021-07-29 16:26:12 +0100

    added address to st-common

--- WEEDER/iocBoot/iocWEEDER-IOC-01/st-common.cmd
+++ WEEDER/iocBoot/iocWEEDER-IOC-01/st-common.cmd
@@ -29,7 +29,7 @@ $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)",0,"ixoff","N")
 < $(IOCSTARTUP)/dbload.cmd
 
 ## Load our record instances
-dbLoadRecords("$(WEEDER)/db/devWeeder.db","P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=$(DEVICE)")
+dbLoadRecords("$(WEEDER)/db/devWeeder.db","P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=$(DEVICE),ADDRESS=$(ADDRESS=A)")
 
 ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
 < $(IOCSTARTUP)/preiocinit.cmd
