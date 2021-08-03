7635cdc552b6e22263d6a933257b305967540d3a
JamesKingWork <james.c.king@stfc.ac.uk>
2021-08-03 09:21:18 +0100

    Add template IOC

--- Makefile
+++ Makefile
@@ -46,6 +46,7 @@ IOCDIRS += PT2025
 IOCDIRS += FMR
 IOCDIRS += HIFIMAGS
 IOCDIRS += HLX503
+IOCDIRS += TESTD
 
 ## check on missing directories
 IOCMAKES = $(wildcard */Makefile)
---
+++ TESTD/iocBoot/iocTESTD-IOC-03/st.cmd
@@ -0,0 +1,18 @@
+#!../../bin/windows-x64/TESTD-IOC-03
+
+## You may have to change TESTD-IOC-03 to something else
+## everywhere it appears in this file
+
+# Increase this if you get <<TRUNCATED>> or discarded messages warnings in your errlog output
+errlogInit2(65536, 256)
+
+< envPaths
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/TESTD-IOC-03.dbd"
+TESTD_IOC_03_registerRecordDeviceDriver pdbbase
+
+## calling common command file in ioc 01 boot dir
+< ${TOP}/iocBoot/iocTESTD-IOC-01/st-common.cmd
