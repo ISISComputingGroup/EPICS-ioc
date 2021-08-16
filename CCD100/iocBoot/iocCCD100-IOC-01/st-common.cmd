dfaec8e228356d60bb126bc2edf4b6c5cae1ecb9
Jack Harper <jack.harper@stfc.ac.uk>
2021-08-16 13:49:12 +0100

    giving errors still - empty packets

--- CCD100/iocBoot/iocCCD100-IOC-01/st-common.cmd
+++ CCD100/iocBoot/iocCCD100-IOC-01/st-common.cmd
@@ -9,7 +9,7 @@ $(IFRECSIM) drvAsynSerialPortConfigure("L0", "$(PORT=NUL)", 0, 1, 0, 0)
 # For dev sim devices
 $(IFDEVSIM) drvAsynIPPortConfigure("L0", "localhost:$(EMULATOR_PORT=)")
 
-$(IFNOTDEVSIM) $(IFNOTRECSIM) drvAsynSerialPortConfigure("L0", "$(PORT=NUL)", 0, 0, 0, 0)
+$(IFNOTDEVSIM) $(IFNOTRECSIM) drvAsynIPPortConfigure("L0", "192.168.1.180:101")
 $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0", -1, "baud", "$(BAUD=57600)")
 $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0", -1, "bits", "$(BITS=8)")
 $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("L0", -1, "parity", "$(PARITY=none)")
