914fa9b5d464aa0684195dc6ad19295967a122d1
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2021-11-24 14:30:24 +0000

    Remove duplicate code

--- GALILMUL/iocBoot/iocGALILMUL-IOC-01/st-motor-controller.cmd
+++ GALILMUL/iocBoot/iocGALILMUL-IOC-01/st-motor-controller.cmd
@@ -25,19 +25,5 @@ $(IFHASMTRCTRL$(CN))
 $(IFHASMTRCTRL$(CN)) ## load the generic ISIS axis db for each axis
 $(IFHASMTRCTRL$(CN)) iocshCmdLoop("< st-axis.cmd", "MN=\$(I)", "I", 1, 8)
 $(IFHASMTRCTRL$(CN)) 
-$(IFHASMTRCTRL$(CN)) ## motor util package
-$(IFHASMTRCTRL$(CN)) dbLoadRecords("$(MOTOR)/db/motorUtil.db","P=$(MYPVPREFIX)$(IOCNAME):,$(IFIOC)= ,PVPREFIX=$(MYPVPREFIX)")
-$(IFHASMTRCTRL$(CN)) 
-$(IFHASMTRCTRL$(CN)) stringiftest("MOTORCONFIG", "$(MOTORCONFIG=)", 0, 0)
-$(IFHASMTRCTRL$(CN)) $(IFMOTORCONFIG) dbLoadRecords("$(AUTOSAVE)/asApp/Db/configMenu.db","P=$(MYPVPREFIX)$(IOCNAME):CONFIG:,CONFIG=$(MOTORCONFIG=)")
-$(IFHASMTRCTRL$(CN)) 
-$(IFHASMTRCTRL$(CN)) ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
-$(IFHASMTRCTRL$(CN)) < $(IOCSTARTUP)/preiocinit.cmd
-$(IFHASMTRCTRL$(CN)) 
-$(IFHASMTRCTRL$(CN)) ## trace mask: 0x1=trace_error,0x2=traceio_device,0x4=traceio_filter, 0x8=traceio_driver, 0x10=trace_flow, 0x20=trace_warning  
-$(IFHASMTRCTRL$(CN)) #asynSetTraceMask("Galil",-1,0x39)
-$(IFHASMTRCTRL$(CN)) #asynSetTraceFile("Galil",-1,"galil.trace")
-$(IFHASMTRCTRL$(CN)) #asynSetTraceIOMask("Galil",-1,0x2)
-$(IFHASMTRCTRL$(CN)) 
 $(IFHASMTRCTRL$(CN)) stringiftest("HASMTRCTRL", "$(MTRCTRL=)", 0, 0)
 $(IFHASMTRCTRL$(CN)) $(IFNOTHASMTRCTRL) errlogSev(2, "MTRCTRL has not been set")
