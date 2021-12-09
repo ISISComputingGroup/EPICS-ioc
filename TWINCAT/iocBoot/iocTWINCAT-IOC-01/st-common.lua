8a9ac53e9312f71a44ae26f9c39c9273b92fca38
Jack Harper <jack.harper@stfc.ac.uk>
2021-12-09 14:08:08 +0000

    working now

--- TWINCAT/iocBoot/iocTWINCAT-IOC-01/st-common.lua
+++ TWINCAT/iocBoot/iocTWINCAT-IOC-01/st-common.lua
@@ -13,7 +13,7 @@ function twincat_stcommon_main()
 	local tpy_file = ibex_utils.getMacroValue{macro="TPY_FILE"}
 	local ioc_name = ibex_utils.getMacroValue{macro="IOCNAME"}
 	local plc_version = ibex_utils.getMacroValue{macro="PLC_VERSION", default="1"}
-
+	-- iocsh.callbackSetQueueSize(50000)
 	iocsh.tcSetScanRate(150, 2)
 
 	local full_tpy_path = ibex_utils.getMacroValue{macro="TWINCATCONFIG"} .. "/" .. tpy_file
@@ -24,27 +24,27 @@ function twincat_stcommon_main()
 	
 	iocsh.tcLoadRecords(full_tpy_path, string.format("-eo -devtc -p %s", pv_prefix))
 
-	iocsh.countdbgrep("AXES_NUM", "*ASTAXES_*:STCONTROL-BENABLE*")
+	-- iocsh.countdbgrep("AXES_NUM", "*ASTAXES_*:STCONTROL-BENABLE*")
 
-	local num_axes = ibex_utils.getMacroValue{macro="AXES_NUM", default="8"}
+	-- local num_axes = ibex_utils.getMacroValue{macro="AXES_NUM", default="8"}
 
-	iocsh.devMotorCreateController(motor_port, "Controller", num_axes, pv_prefix)
+	-- iocsh.devMotorCreateController(motor_port, "Controller", num_axes, pv_prefix)
 	
-	autosave_file = io.open (ioc_name .. "_settings.req", "w")
+	-- autosave_file = io.open (ioc_name .. "_settings.req", "w")
 	
-	db_args = string.format("P=%s::,$(IFIOC)= ,PVPREFIX=%s,MTRCTRL=%02i,AXES_NUM=%s", pv_prefix, pv_prefix, os.getenv("MTRCTRL"), num_axes)
-	iocsh.dbLoadRecords("$(MOTOR)/db/motorUtil.db", db_args)
+	-- db_args = string.format("P=%s::,$(IFIOC)= ,PVPREFIX=%s,MTRCTRL=%02i,AXES_NUM=%s", pv_prefix, pv_prefix, os.getenv("MTRCTRL"), num_axes)
+	-- iocsh.dbLoadRecords("$(MOTOR)/db/motorUtil.db", db_args)
 	
-	for axis_num=1,num_axes,1
-	do
-		motor_pv = string.format("MTR%02i%02i", os.getenv("MTRCTRL"), axis_num)
-		single_axis_db = "db/single_axis.db"
-		db_args = string.format("MYPVPREFIX=%s,MOTOR_PV=%s,MOTOR_PORT=%s,ADDR=%s", pv_prefix, motor_pv, motor_port, axis_num-1)
-		iocsh.devMotorCreateAxis(motor_port, axis_num-1, plc_version)
-		iocsh.dbLoadRecords(single_axis_db, db_args)
-		autosave_file:write(string.format("file \"motor_settings.req\" P=%s, M=MOT:%s\n", pv_prefix, motor_pv))
-	end
-	autosave_file:close()
+	-- for axis_num=1,num_axes,1
+	-- do
+	-- 	motor_pv = string.format("MTR%02i%02i", os.getenv("MTRCTRL"), axis_num)
+	-- 	single_axis_db = "db/single_axis.db"
+	-- 	db_args = string.format("MYPVPREFIX=%s,MOTOR_PV=%s,MOTOR_PORT=%s,ADDR=%s", pv_prefix, motor_pv, motor_port, axis_num-1)
+	-- 	iocsh.devMotorCreateAxis(motor_port, axis_num-1, plc_version)
+	-- 	iocsh.dbLoadRecords(single_axis_db, db_args)
+	-- 	autosave_file:write(string.format("file \"motor_settings.req\" P=%s, M=MOT:%s\n", pv_prefix, motor_pv))
+	-- end
+	-- autosave_file:close()
 
 end
 
