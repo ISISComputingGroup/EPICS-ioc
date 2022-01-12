88cc0c43477aaa34036c763b2aa1ef4ac25304aa
Jack Harper <jack.harper@stfc.ac.uk>
2022-01-12 11:52:57 +0000

    adding comments

--- TWINCAT/iocBoot/iocTWINCAT-IOC-01/st-common.lua
+++ TWINCAT/iocBoot/iocTWINCAT-IOC-01/st-common.lua
@@ -1,19 +1,20 @@
 package.path = package.path .. ';' .. os.getenv("UTILITIES") .. '/lua/luaUtils.lua;'
 ibex_utils = require "luaUtils"
 
+-- Check a given file exists on disk. 
 function exists(file)
 	local f=io.open(file,"r")
     if f~=nil then io.close(f) return true else return false end
  end
  
-
+-- Main function called by st-common.cmd
 function twincat_stcommon_main()
 	local motor_port = "L0"
 	local pv_prefix = ibex_utils.getMacroValue{macro="MYPVPREFIX"}
 	local tpy_file = ibex_utils.getMacroValue{macro="TPY_FILE"}
 	local ioc_name = ibex_utils.getMacroValue{macro="IOCNAME"}
 	local plc_version = ibex_utils.getMacroValue{macro="PLC_VERSION", default="1"}
-
+	-- Set TcIoc's scan rate to 10ms for ADS and 50ms for EPICS - this is to avoid ADS fields not updating in channel access quickly enough. 
 	iocsh.tcSetScanRate(10, 5)
 
 	local full_tpy_path = ibex_utils.getMacroValue{macro="TWINCATCONFIG"} .. "/" .. tpy_file
@@ -24,8 +25,8 @@ function twincat_stcommon_main()
 	
 	iocsh.tcLoadRecords(full_tpy_path, string.format("-eo -devtc -p %s", pv_prefix))
 
+	-- count BENABLEs to determine how many Axes a Beckhoff is using
 	iocsh.countdbgrep("AXES_NUM", "*ASTAXES_*:STCONTROL-BENABLE*")
-
 	local num_axes = ibex_utils.getMacroValue{macro="AXES_NUM", default="8"}
 	local mtrctrl = os.getenv("MTRCTRL")
 
@@ -44,7 +45,7 @@ function twincat_stcommon_main()
 		iocsh.devMotorCreateAxis(motor_port, axis_num-1, plc_version)
 		iocsh.dbLoadRecords(single_axis_db, db_args)
 		autosave_file:write(string.format("file \"motor_settings.req\" P=%s, M=MOT:%s\n", pv_prefix, motor_pv))
-
+		-- wrap around to next MTRCTRL - this is so we can show >8 axes in the IBEX table of motors. 
 		if axis_num > 8 then
 			alias_args_orig = string.format("$(MYPVPREFIX)MOT:%s(.*)", motor_pv)
 			alias_args_aliased = string.format("$(MYPVPREFIX)MOT:MTR%02i%02i\\1", ((axis_num-1)//8) + mtrctrl, (axis_num-1)%8 + 1)
