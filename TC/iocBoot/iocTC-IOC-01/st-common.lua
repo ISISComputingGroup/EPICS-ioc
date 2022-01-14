bb8c0edf833789bcb780976c8ebd001c75be036c
Jack Harper <jack.harper@stfc.ac.uk>
2022-01-14 15:56:23 +0000

    passing enabled_monitor args correctly - including  macro

--- TC/iocBoot/iocTC-IOC-01/st-common.lua
+++ TC/iocBoot/iocTC-IOC-01/st-common.lua
@@ -48,7 +48,7 @@ function twincat_stcommon_main()
 		iocsh.dbLoadRecords(single_axis_db, db_args)
 
 		enabled_monitor_db = "db/enabled_monitor.db"
-		enabled_monitor_db_args = string.format("P=%s,AXIS_NUM=%s,MOTOR_PV=%s", ioc_prefix, axis_num, motor_pv)
+		enabled_monitor_db_args = string.format("P=%s,I=%s,AXIS_NUM=%s,MOTOR_PV=%s", pv_prefix, ioc_name, axis_num, motor_pv)
 		iocsh.dbLoadRecords(enabled_monitor_db, enabled_monitor_db_args)
 		autosave_file:write(string.format("file \"motor_settings.req\" P=%s, M=MOT:%s\n", pv_prefix, motor_pv))
 		-- wrap around to next MTRCTRL - this is so we can show >8 axes in the IBEX table of motors. 
