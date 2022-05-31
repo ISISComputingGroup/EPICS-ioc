96c462388e8711aa05e52293efbb6a2f60b12299
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2022-05-31 14:02:15 +0100

    Add back FMR that was deleted in TECHNIX merge

---
+++ FMR/iocBoot/iocFMR-IOC-01/st.cmd
@@ -0,0 +1,37 @@
+#!../../bin/windows-x64/FMR-IOC-01
+## You may have to change FMR-IOC-01 to something else
+## everywhere it appears in this file
+
+# Increase this if you get <<TRUNCATED>> or discarded messages warnings in your errlog output
+errlogInit2(65536, 256)
+
+< envPaths
+
+cd ${TOP}
+
+epicsEnvSet(IOC_NUM,1)
+## Register all support components
+dbLoadDatabase "dbd/FMR-IOC-01.dbd"
+FMR_IOC_01_registerRecordDeviceDriver pdbbase
+
+##ISIS## Run IOC initialisation 
+< $(IOCSTARTUP)/init.cmd
+
+# Configure lvDCOM interface
+lvDCOMConfigure("lvfp", "frontpanel", "${FMR}/data/lv_FMR.xml", "$(LVDCOM_HOST=)", $(LVDCOM_OPTIONS=2))
+
+## Load record instances
+
+##ISIS## Load common DB records 
+< $(IOCSTARTUP)/dbload.cmd
+
+dbLoadRecords("$(FMR)/db/FMR.db", "P=$(MYPVPREFIX)$(IOCNAME):")
+
+##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
+< $(IOCSTARTUP)/preiocinit.cmd
+
+cd ${TOP}/iocBoot/${IOC}
+iocInit
+
+##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
+< $(IOCSTARTUP)/postiocinit.cmd
