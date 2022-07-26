d17e1b03f796318013b29ff6e4cf9938feed44d9
Tom Willemsen <tomwillemsen1995@gmail.com>
2022-07-26 11:44:40 +0100

    Implement status bits

--- TRANTECH/iocBoot/iocTRANTECH-IOC-01/st-common.cmd
+++ TRANTECH/iocBoot/iocTRANTECH-IOC-01/st-common.cmd
@@ -30,6 +30,7 @@ $(IFNOTDEVSIM) $(IFNOTRECSIM) asynSetOption("$(DEVICE)",0,"ixoff","N")
 
 ## Load our record instances
 dbLoadRecords("$(TRANTECH)/db/transtechnik.db","P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=$(DEVICE),PS_ADDR=$(PS_ADDR=001),VOLT_FULLSCALE=$(VOLT_FULLSCALE=125),CURR_FULLSCALE=$(CURR_FULLSCALE=500)")
+dbLoadRecords("$(TRANTECH)/db/transtechnik_status_bit.db","P=$(MYPVPREFIX)$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=$(DEVICE)")
 
 ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
 < $(IOCSTARTUP)/preiocinit.cmd
