03beaa62050cf5e16e3e4290c41f6e2ebf735f82
David Keymer <david.keymer@stfc.ac.uk>
2022-08-23 17:38:28 +0100

    Tidied formatting

--- SCHNDR/iocBoot/iocSCHNDR-IOC-01/devices/RIKENFE.cmd
+++ SCHNDR/iocBoot/iocSCHNDR-IOC-01/devices/RIKENFE.cmd
@@ -1,26 +1,18 @@
-# drvModbusAsynConfigure(portName,
-#                        tcpPortName,
-#                        slaveAddress,
-#                        modbusFunction,
-#                        modbusStartAddress,
-#                        modbusLength,
-#                        dataType,
-#                        pollMsec,
-#                        plcType);
+# drvModbusAsynConfigure(portName, tcpPortName, slaveAddress, modbusFunction, modbusStartAddress, modbusLength, dataType, pollMsec, plcType)
 
-# IBEX PLC Specification                         
+# Sections refer to document "RIKEN PLC/IBEX Specification – Issue C"
 
 # Section 2.4 Vacuum Pump Status
-drvModbusAsynConfigure("$(PLC)vac_pump_stat", "$(PLC)", 255, 3, 4004093, 22, 0, 1000, "Schneider M580 PLC")         # IEC61131 start address: %MW4092
+drvModbusAsynConfigure("$(PLC)vac_pump_stat",         "$(PLC)", 255, 3, 4004093, 22, 0, 1000, "Schneider M580 PLC")         # IEC61131 start address: %MW4092
 
 # Section 3.1 Klixon Interlock Status
-drvModbusAsynConfigure("$(PLC)klixon_interlock_stat", "$(PLC)", 255, 3, 4004146, 35, 0, 1000, "Schneider M580 PLC")         # IEC61131 start address: %MW4145
+drvModbusAsynConfigure("$(PLC)klixon_interlock_stat", "$(PLC)", 255, 3, 4004146, 36, 0, 1000, "Schneider M580 PLC")         # IEC61131 start address: %MW4145
 
 # Section 3.2 Water Interlock Status
-drvModbusAsynConfigure("$(PLC)water_interlock_stat", "$(PLC)", 255, 3, 4004182, 38, 0, 1000, "Schneider M580 PLC")         # IEC61131 start address: %MW4181
+drvModbusAsynConfigure("$(PLC)water_interlock_stat",  "$(PLC)", 255, 3, 4004182, 38, 0, 1000, "Schneider M580 PLC")         # IEC61131 start address: %MW4181
 
 # Section 3.3 Water Flow
-drvModbusAsynConfigure("$(PLC)water_flow", "$(PLC)", 255, 3, 4004517, 78, 7, 1000, "Schneider M580 PLC")         # IEC61131 start address: %MW4516
+drvModbusAsynConfigure("$(PLC)water_flow",            "$(PLC)", 255, 3, 4004517, 78, 7, 1000, "Schneider M580 PLC")         # IEC61131 start address: %MW4516
 
 
 ## Load our record instances
