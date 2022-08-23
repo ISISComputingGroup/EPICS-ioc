aa28860d0940e4bfd47a58a3158d9392b1fc6b9e
David Keymer <david.keymer@stfc.ac.uk>
2022-08-23 12:58:03 +0100

    Added float template, addtional values, tidied templates

--- SCHNDR/iocBoot/iocSCHNDR-IOC-01/devices/RIKENFE.cmd
+++ SCHNDR/iocBoot/iocSCHNDR-IOC-01/devices/RIKENFE.cmd
@@ -11,7 +11,11 @@
 # IBEX PLC Specification                         
 
 # Section 2.4 Vacuum Pump Status
-drvModbusAsynConfigure("$(PLC)vac_pump_stat", "$(PLC)", 255, 3, 4004093, 22, 0, 1000, "Schneider M580 PLC")         # IEC61131 address: %MW4092
+drvModbusAsynConfigure("$(PLC)vac_pump_stat", "$(PLC)", 255, 3, 4004093, 22, 0, 1000, "Schneider M580 PLC")         # IEC61131 start address: %MW4092
+
+# Section 3.3 Water Flow
+drvModbusAsynConfigure("$(PLC)water_flow", "$(PLC)", 255, 3, 4004517, 78, 7, 1000, "Schneider M580 PLC")         # IEC61131 start address: %MW4516
+
 
 ## Load our record instances
 dbLoadRecords("$(TOP)/db/RIKENFE.db","P=$(MYPVPREFIX)$(IOCNAME):,PORT=$(PLC)")
