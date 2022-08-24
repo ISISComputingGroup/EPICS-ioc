d0b2f969d0c1a2e4d11643866ecafb41db201a20
David Keymer <david.keymer@stfc.ac.uk>
2022-08-24 11:07:00 +0100

    Added RBox PVs

--- SCHNDR/iocBoot/iocSCHNDR-IOC-01/devices/RIKENFE.cmd
+++ SCHNDR/iocBoot/iocSCHNDR-IOC-01/devices/RIKENFE.cmd
@@ -3,16 +3,19 @@
 # Sections refer to document "RIKEN PLC/IBEX Specification – Issue C"
 
 # Section 2.4 Vacuum Pump Status
-drvModbusAsynConfigure("$(PLC)vac_pump_stat",         "$(PLC)", 255, 3, 4004093, 22, 0, 1000, "Schneider M580 PLC")         # IEC61131 start address: %MW4092
+drvModbusAsynConfigure("$(PLC)vacuum_pump_status",      "$(PLC)", 255, 3, 4004093, 22, 0, 1000, "Schneider M580 PLC")         # IEC61131 start address: %MW4092
 
 # Section 3.1 Klixon Interlock Status
-drvModbusAsynConfigure("$(PLC)klixon_interlock_stat", "$(PLC)", 255, 3, 4004146, 36, 0, 1000, "Schneider M580 PLC")         # IEC61131 start address: %MW4145
+drvModbusAsynConfigure("$(PLC)klixon_interlock_status", "$(PLC)", 255, 3, 4004146, 36, 0, 1000, "Schneider M580 PLC")         # IEC61131 start address: %MW4145
 
 # Section 3.2 Water Interlock Status
-drvModbusAsynConfigure("$(PLC)water_interlock_stat",  "$(PLC)", 255, 3, 4004182, 38, 0, 1000, "Schneider M580 PLC")         # IEC61131 start address: %MW4181
+drvModbusAsynConfigure("$(PLC)water_interlock_status",  "$(PLC)", 255, 3, 4004182, 38, 0, 1000, "Schneider M580 PLC")         # IEC61131 start address: %MW4181
 
 # Section 3.3 Water Flow
-drvModbusAsynConfigure("$(PLC)water_flow",            "$(PLC)", 255, 3, 4004517, 78, 7, 1000, "Schneider M580 PLC")         # IEC61131 start address: %MW4516
+drvModbusAsynConfigure("$(PLC)water_flow_rate",         "$(PLC)", 255, 3, 4004517, 78, 7, 1000, "Schneider M580 PLC")         # IEC61131 start address: %MW4516
+
+# Section 3.4 R-Box Interlock Status
+drvModbusAsynConfigure("$(PLC)rbox_interlock_status",   "$(PLC)", 255, 3, 4004221, 56, 0, 1000, "Schneider M580 PLC")         # IEC61131 start address: %MW4220
 
 
 ## Load our record instances
