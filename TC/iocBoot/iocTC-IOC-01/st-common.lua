cb5533310efd9e985dba040aca9875cb938ed32c
Jack Harper <jack.harper@stfc.ac.uk>
2022-12-14 13:45:25 +0000

    getting closer

--- TC/iocBoot/iocTC-IOC-01/st-common.lua
+++ TC/iocBoot/iocTC-IOC-01/st-common.lua
@@ -1,36 +1,40 @@
 package.path = package.path .. ';' .. os.getenv("UTILITIES") .. '/lua/luaUtils.lua;'
 ibex_utils = require "luaUtils"
 
--- Check a given file exists on disk. 
-function exists(file)
-	local f=io.open(file,"r")
-    if f~=nil then io.close(f) return true else return false end
- end
  
 -- Main function called by st-common.cmd
 function twincat_stcommon_main()
 	local motor_port = "L0"
 	local pv_prefix = ibex_utils.getMacroValue{macro="MYPVPREFIX"}
-	local tpy_file = ibex_utils.getMacroValue{macro="TPY_FILE"}
+	-- local tpy_file = ibex_utils.getMacroValue{macro="TPY_FILE"}
 	local ioc_name = ibex_utils.getMacroValue{macro="IOCNAME"}
 	local plc_version = ibex_utils.getMacroValue{macro="PLC_VERSION", default="1"}
-	-- Set TcIoc's scan rate to 150ms for ADS and 300ms for EPICS - this is to avoid ADS fields not updating in channel access quickly enough. 
-	iocsh.tcSetScanRate(150, 2)
-
-	local full_tpy_path = ibex_utils.getMacroValue{macro="TWINCATCONFIG"} .. "/" .. tpy_file
-	if not exists(full_tpy_path) then
-		print("invalid TPY file given: " .. full_tpy_path)
-		iocsh.exit()
-	end
-
+	local ads_port = ibex_utils.getMacroValue(macro="ADS_PORT", default="852")
+	local num_axes = 9 -- todo: actually poll the device to get this
+	local mtrctrl = os.getenv("MTRCTRL")
 	local ioc_prefix = pv_prefix .. ioc_name .. ":"
+
+
+	-- Set TcIoc's scan rate to 150ms for ADS and 300ms for EPICS - this is to avoid ADS fields not updating in channel access quickly enough. 
+	--iocsh.tcSetScanRate(150, 2)
+
+	-- local full_tpy_path = ibex_utils.getMacroValue{macro="TWINCATCONFIG"} .. "/" .. tpy_file
+	-- if not exists(full_tpy_path) then
+	-- 	print("invalid TPY file given: " .. full_tpy_path)
+	-- 	iocsh.exit()
+	-- end
+
+
+	for axis_num=1,num_axes,1
+	do 
+		local single_axis_tc_args = string.format("P=%s,A=%s,ADSPORT=%s", ioc_prefix, axis_num, ads_port)
+		iocsh.dbLoadRecords("db/single_axis_tc.db", single_axis_tc_args)
+	end
 	
-	iocsh.tcLoadRecords(full_tpy_path, string.format("-eo -devtc -p %s", ioc_prefix))
 
 	-- count BENABLEs to determine how many Axes a Beckhoff is using
-	iocsh.countdbgrep("AXES_NUM", "*ASTAXES_*:STCONTROL-BENABLE*")
-	local num_axes = ibex_utils.getMacroValue{macro="AXES_NUM", default="8"}
-	local mtrctrl = os.getenv("MTRCTRL")
+	-- iocsh.countdbgrep("AXES_NUM", "*ASTAXES_*:STCONTROL-BENABLE*")
+
 
 	iocsh.devMotorCreateController(motor_port, "Controller", num_axes, ioc_prefix)
 	
