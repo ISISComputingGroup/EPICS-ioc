2acb53b985361e2007712088d8a920fe566a391e
Jack Harper <jack.harper@stfc.ac.uk>
2023-01-17 14:27:16 +0000

    add working FINS_01.cmd

--- FINS/exampleSettings/SANDALS_gate_valve/FINS_01.cmd
+++ FINS/exampleSettings/SANDALS_gate_valve/FINS_01.cmd
@@ -1,5 +1,5 @@
 #Init and connect
-finsUDPInit("PLC", "$(PLCIP)", "TCP", 1, "$(PLCNODE=)")
+finsUDPInit("PLC", "$(PLCIP)", "UDP", 0, "$(PLCNODE=)")
 
 ## Load our record instances
 dbLoadRecords("${TOP}/db/sandals-gate-valve.db","P=$(MYPVPREFIX),Q=FINS_VAC:,RECSIM=$(RECSIM),TIMEOUT=5.0")
