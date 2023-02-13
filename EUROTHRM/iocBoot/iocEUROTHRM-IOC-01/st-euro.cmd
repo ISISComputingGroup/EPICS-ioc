d4ab6aae994e0da23914ca904b72d126433d7f08
HannahCawley <HannahCawley@NDW1798.isis.cclrc.ac.uk>
2023-02-13 11:18:13 +0000

    Add PVs for automatic needle valve

--- EUROTHRM/iocBoot/iocEUROTHRM-IOC-01/st-euro.cmd
+++ EUROTHRM/iocBoot/iocEUROTHRM-IOC-01/st-euro.cmd
@@ -22,6 +22,9 @@ $(IFACTIVE) dbLoadRecords("$(TOP)/db/devEurothrm.db","P=$(P),Q=$(MYPVPREFIX)$(IO
 
 $(IFACTIVE) dbLoadRecords($(FILELIST)/db/calibration.db, "P=$(P), CALIB_BASE_DIR=$(CALIB_BASE_DIR), SDIR=$(SENS_DIR), CALIBLIST=SENSORFILELIST$(EURO_NUM), CONV_TO_PV=TEMP, CONV_FROM_PV=, CONV_TO_DESC=Temperature, CONV_TO_EGU=K, SP_PV=RAMP_SP")
 
+stringiftest("NEEDLE_VALVE", "$(NEEDLE_VALVE=yes)", 5, "yes")
+$(IFNEEDLE_VALVE) dbLoadRecords("$(TOP)/db/automaticNeedleValve.db", "P=$(P),GAD=$(GAD), LAD=$(LAD)")
+
 # Readascii units
 $(IFACTIVE) dbLoadRecords("$(ReadASCII)/db/get_metadata.db","DIR=$(P)TEMP,CAL=$(P)CAL:RBV,OUT=$(P)TEMP,OUTF=EGU,NAME=column1_units,DEFAULT=K,OUTPP=PP")
 $(IFACTIVE) dbLoadRecords("$(ReadASCII)/db/get_metadata.db","DIR=$(P)TEMP,CAL=$(P)CAL:RBV,OUT=$(P)TEMP:SP,OUTF=EGU,NAME=column1_units,DEFAULT=K")
