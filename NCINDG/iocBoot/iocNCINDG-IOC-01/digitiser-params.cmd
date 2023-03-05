0b5df8cde09fcd500b6fc4a8f6b99c946ab316f0
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2023-03-05 21:07:39 +0000

    Upadet for AD

--- NCINDG/iocBoot/iocNCINDG-IOC-01/digitiser-params.cmd
+++ NCINDG/iocBoot/iocNCINDG-IOC-01/digitiser-params.cmd
@@ -11,6 +11,7 @@ dbLoadRecords("$(NUCINSTDIG)/db/NucInstDigStringParam.db","EPARAM=DGTZ:PROBES:FW
 dbLoadRecords("$(NUCINSTDIG)/db/NucInstDigIntegerParam.db","EPARAM=SYS:BASECONN,DPARAM=system.baseconnected,DESC=Base detected,P=$(MYPVPREFIX),Q=$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=DIG0")
 dbLoadRecords("$(NUCINSTDIG)/db/NucInstDigIntegerParam.db","EPARAM=SYS:MEMORY,DPARAM=system.availableram,DESC=Available DAQ Ram,P=$(MYPVPREFIX),Q=$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=DIG0")
 dbLoadRecords("$(NUCINSTDIG)/db/NucInstDigIntegerParam.db","EPARAM=TRIG:RATE,DPARAM=trg.self_rate,DESC=Internal gen freq in periodic mode,EGU=Hz,P=$(MYPVPREFIX),Q=$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=DIG0")
+dbLoadRecords("$(NUCINSTDIG)/db/NucInstDigIntegerParam.db","EPARAM=TRIG:RATE,DPARAM=trg.self_rate,DESC=Internal gen freq in periodic mode,EGU=Hz,P=$(MYPVPREFIX),Q=$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=DIG0")
 
 dbLoadRecords("$(NUCINSTDIG)/db/NucInstDigRealParam.db","EPARAM=SYS:CPULOAD,DPARAM=system.cpuload,DESC=CPU Load,EGU=%,P=$(MYPVPREFIX),Q=$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=DIG0")
 dbLoadRecords("$(NUCINSTDIG)/db/NucInstDigRealParam.db","EPARAM=SYS:TSYS:CORE,DPARAM=system.Tsys.core,DESC=Zynq core temp,EGU=C,P=$(MYPVPREFIX),Q=$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=DIG0")
@@ -19,3 +20,5 @@ dbLoadRecords("$(NUCINSTDIG)/db/NucInstDigRealParam.db","EPARAM=SYS:TSYS:DGTZ,DP
 dbLoadRecords("$(NUCINSTDIG)/db/NucInstDigRealParam.db","EPARAM=SYS:TSYS:BASE,DPARAM=system.Tsys.base,DESC=Base temp,EGU=C,P=$(MYPVPREFIX),Q=$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=DIG0")
 
 dbLoadRecordsLoop("$(NUCINSTDIG)/db/NucInstDigRealParamChan.db","EPARAM=STAVE:T:PROBES:TSIPM,DPARAM=stave.T.probes.Tsipm,CHAN=\$(I),EGU=C,DESC=Temperatures of SiPM,P=$(MYPVPREFIX),Q=$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=DIG0", "I", 0, 23)
+dbLoadRecordsLoop("$(NUCINSTDIG)/db/NucInstDigRealParamChan.db","EPARAM=STAVE:BIAS:PROBES:VSIPM,DPARAM=stave.BIAS.probes.Vsipm,CHAN=\$(I),EGU=V,DESC=Voltage applied to SiPM,P=$(MYPVPREFIX),Q=$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=DIG0", "I", 0, 23)
+dbLoadRecordsLoop("$(NUCINSTDIG)/db/NucInstDigRealParamChan.db","EPARAM=STAVE:BIAS:PROBES:CORR,DPARAM=stave.BIAS.probes.correction,CHAN=\$(I),EGU=V,DESC=HV correction for SiPM,P=$(MYPVPREFIX),Q=$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=DIG0", "I", 0, 23)
--- NCINDG/iocBoot/iocNCINDG-IOC-01/st-common.cmd
+++ NCINDG/iocBoot/iocNCINDG-IOC-01/st-common.cmd
@@ -1,6 +1,9 @@
 ##ISIS## Run IOC initialisation 
 < $(IOCSTARTUP)/init.cmd
 
+epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
+asynSetMinTimerPeriod(0.001)
+
 nucInstDigConfigure("DIG0", "$(IP_ADDR_0)", $(INDEX_0))
 
 ## Load record instances
@@ -13,7 +16,10 @@ dbLoadRecords("$(NUCINSTDIG)/db/NucInstDig.db","P=$(MYPVPREFIX),Q=$(IOCNAME):,RE
 dbLoadRecords("$(NUCINSTDIG)/db/NucInstDigTrace.db","P=$(MYPVPREFIX),Q=$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=DIG0")
 dbLoadRecords("$(NUCINSTDIG)/db/NucInstDigDCSpec.db","P=$(MYPVPREFIX),Q=$(IOCNAME):,RECSIM=$(RECSIM=0),DISABLE=$(DISABLE=0),PORT=DIG0")
 
-< ${TOP}/iocBoot/${IOC}/digitiser-params.cmd
+iocshLoad "${TOP}/iocBoot/iocNCINDG-IOC-01/liveview.cmd", "LVDET=1,LVADDR=0"
+iocshLoad "${TOP}/iocBoot/iocNCINDG-IOC-01/liveview.cmd", "LVDET=2,LVADDR=1"
+
+< ${TOP}/iocBoot/iocNCINDG-IOC-01/digitiser-params.cmd
 
 ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
 < $(IOCSTARTUP)/preiocinit.cmd
@@ -26,3 +32,9 @@ iocInit
 
 ##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
 < $(IOCSTARTUP)/postiocinit.cmd
+
+# Save motor positions every 5 seconds
+create_monitor_set("$(IOCNAME)_positions.req", 5, "P=$(MYPVPREFIX)$(Q)")
+
+# Save motor settings every 30 seconds
+create_monitor_set("$(IOCNAME)_settings.req", 30, "P=$(MYPVPREFIX)$(Q)")
