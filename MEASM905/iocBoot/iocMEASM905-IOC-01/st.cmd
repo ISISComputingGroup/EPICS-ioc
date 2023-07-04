748b18339728c79a38bf4f20b4dfbbccaaa8a31d
esmithExperimentControls <evan.smith@stfc.ac.uk>
2023-07-04 11:48:51 +0100

    Added macros to get IOC to start

--- MEASM905/iocBoot/iocMEASM905-IOC-01/st.cmd
+++ MEASM905/iocBoot/iocMEASM905-IOC-01/st.cmd
@@ -3,16 +3,16 @@
 #- You may have to change MEASM905-IOC-01 to something else
 #- everywhere it appears in this file
 
-#< envPaths
+< envPaths
 
 ## Register all support components
 dbLoadDatabase("../../dbd/MEASM905-IOC-01.dbd",0,0)
 MEASM905_IOC_01_registerRecordDeviceDriver(pdbbase) 
 
 ## Load record instances
-dbLoadRecords("../../db/MEASM905-IOC-01.db","user=yyf77781")
+#dbLoadRecords("../../db/MEASM905-IOC-01.db","user=yyf77781")
 
-iocInit()
-
+## calling common command file in ioc 01 boot dir
+< st-common.cmd
 ## Start any sequence programs
 #seq sncMEASM905-IOC-01,"user=yyf77781"
