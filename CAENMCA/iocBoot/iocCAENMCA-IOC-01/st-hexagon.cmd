9d437b63c5f04d6b674aee058adaf48498acfc65
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2023-10-24 22:47:01 +0100

    Fix liveview for multiple hexagons

--- CAENMCA/iocBoot/iocCAENMCA-IOC-01/st-hexagon.cmd
+++ CAENMCA/iocBoot/iocCAENMCA-IOC-01/st-hexagon.cmd
@@ -9,5 +9,5 @@ dbLoadRecords("$(CAENMCA)/db/CAENMCAChan.db","P=$(MYPVPREFIX),Q=HEX$(ID):,PORT=L
 dbLoadRecords("$(CAENMCA)/db/CAENMCAHVChan.db","P=$(MYPVPREFIX),Q=HEX$(ID):,PORT=L$(ID),CHAN=0")
 dbLoadRecords("$(CAENMCA)/db/CAENMCAHVChan.db","P=$(MYPVPREFIX),Q=HEX$(ID):,PORT=L$(ID),CHAN=1")
 
-iocshLoad "${TOP}/iocBoot/iocCAENMCA-IOC-01/liveview.cmd", "P=$(MYPVPREFIX),Q=HEX$(ID):,PORT=L$(ID),LVDET=0,LVADDR=0"
-iocshLoad "${TOP}/iocBoot/iocCAENMCA-IOC-01/liveview.cmd", "P=$(MYPVPREFIX),Q=HEX$(ID):,PORT=L$(ID),LVDET=1,LVADDR=1"
+iocshLoad "${TOP}/iocBoot/iocCAENMCA-IOC-01/liveview.cmd", "P=$(MYPVPREFIX),Q=HEX$(ID):,PORT=L$(ID),ID=$(ID),LVDET=0,LVADDR=0"
+iocshLoad "${TOP}/iocBoot/iocCAENMCA-IOC-01/liveview.cmd", "P=$(MYPVPREFIX),Q=HEX$(ID):,PORT=L$(ID),ID=$(ID),LVDET=1,LVADDR=1"
