77902003f8df1e5f393decbd96eabf6b5bc667bd
DavidKeymer <david.keymer@stfc.ac.uk>
2023-11-17 17:05:20 +0000

    Best guess at options using new thermocouple channel function

--- PRLTHERM/iocBoot/iocPRLTHERM-IOC-01/st-daq.cmd
+++ PRLTHERM/iocBoot/iocPRLTHERM-IOC-01/st-daq.cmd
@@ -6,23 +6,24 @@ epicsEnvSet("CDAQ","$(HOST=PEARL_cDAQ_ChassisMod1)") # PEARL cDAQ chassis and mo
 ## portName - ASYN port name to create or to add to
 ## devicename - NI device name (e.g. dev1/ai0)
 ## Channelnr - Channelnr inside ASYN port (start with 0)
-## acqType - “AI” ,”AO”, “BI”, “BO” or “COUNTER”
+## acqType - “AI” ,”AO”, “BI”, “BO”, “COUNTER” or "AITC"
 ## options - see DAQmx Port Options
 
-# 
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai0",   0, "AI","OneShot TerminalDef N=1 F=0 m=-78.125000e-3 M=78.125000e-3")      # Channel  0
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai1",   1, "AI","OneShot TerminalDef N=1 F=0 m=-78.125000e-3 M=78.125000e-3")      # Channel  1
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai2",   2, "AI","OneShot TerminalDef N=1 F=0 m=-78.125000e-3 M=78.125000e-3")      # Channel  2
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai3",   3, "AI","OneShot TerminalDef N=1 F=0 m=-78.125000e-3 M=78.125000e-3")      # Channel  3
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai4",   4, "AI","OneShot TerminalDef N=1 F=0 m=-78.125000e-3 M=78.125000e-3")      # Channel  4
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai5",   5, "AI","OneShot TerminalDef N=1 F=0 m=-78.125000e-3 M=78.125000e-3")      # Channel  5
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai6",   6, "AI","OneShot TerminalDef N=1 F=0 m=-78.125000e-3 M=78.125000e-3")      # Channel  6
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai7",   7, "AI","OneShot TerminalDef N=1 F=0 m=-78.125000e-3 M=78.125000e-3")      # Channel  7
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai8",   8, "AI","OneShot TerminalDef N=1 F=0 m=-78.125000e-3 M=78.125000e-3")      # Channel  8
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai9",   9, "AI","OneShot TerminalDef N=1 F=0 m=-78.125000e-3 M=78.125000e-3")      # Channel  9
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai10", 10, "AI","OneShot TerminalDef N=1 F=0 m=-78.125000e-3 M=78.125000e-3")      # Channel 10
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai11", 11, "AI","OneShot TerminalDef N=1 F=0 m=-78.125000e-3 M=78.125000e-3")      # Channel 11
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai12", 12, "AI","OneShot TerminalDef N=1 F=0 m=-78.125000e-3 M=78.125000e-3")      # Channel 12
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai13", 13, "AI","OneShot TerminalDef N=1 F=0 m=-78.125000e-3 M=78.125000e-3")      # Channel 13
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai14", 14, "AI","OneShot TerminalDef N=1 F=0 m=-78.125000e-3 M=78.125000e-3")      # Channel 14
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai15", 15, "AI","OneShot TerminalDef N=1 F=0 m=-78.125000e-3 M=78.125000e-3")      # Channel 15
+# All channels configured for units of Kelvin and K-Type thermocouples (values from VI: m=73.15 M=1645.15)
+
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai0",   0, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel  0
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai1",   1, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel  1
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai2",   2, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel  2
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai3",   3, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel  3
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai4",   4, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel  4
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai5",   5, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel  5
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai6",   6, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel  6
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai7",   7, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel  7
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai8",   8, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel  8
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai9",   9, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel  9
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai10", 10, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel 10
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai11", 11, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel 11
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai12", 12, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel 12
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai13", 13, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel 13
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai14", 14, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel 14
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai15", 15, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel 15
