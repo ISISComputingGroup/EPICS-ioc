45476e191a39888a98bc5c5efb3ae6f5cd2a631b
DavidKeymer <david.keymer@stfc.ac.uk>
2023-11-20 15:00:24 +0000

    Corrected options for new daqmx thermocouple channel

--- PRLTHERM/iocBoot/iocPRLTHERM-IOC-01/st-daq.cmd
+++ PRLTHERM/iocBoot/iocPRLTHERM-IOC-01/st-daq.cmd
@@ -10,20 +10,23 @@ epicsEnvSet("CDAQ","$(HOST=PEARL_cDAQ_ChassisMod1)") # PEARL cDAQ chassis and mo
 ## options - see DAQmx Port Options
 
 # All channels configured for units of Kelvin and K-Type thermocouples (values from VI: m=73.15 M=1645.15)
+# Number of samples (N)=2; Sampling Frequency (F)=1; Minumum Value (m)=73.15; Maximum Value (M)=1645.15
 
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai0",   0, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel  0
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai1",   1, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel  1
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai2",   2, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel  2
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai3",   3, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel  3
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai4",   4, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel  4
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai5",   5, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel  5
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai6",   6, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel  6
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai7",   7, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel  7
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai8",   8, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel  8
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai9",   9, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel  9
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai10", 10, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel 10
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai11", 11, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel 11
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai12", 12, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel 12
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai13", 13, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel 13
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai14", 14, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel 14
-$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai15", 15, "AITC", "OneShot N=2 F=0 m=73.15 M=1645")      # Channel 15
+epicsEnvSet("DAQMX_OPTIONS", "N=2 F=1 m=73.15 M=1645.15 U=Kelvins thermocoupleType=K_Type_TC cjcSource=ConstVal cjcVal=25.0 AIADCTimingMode=HighResolution AIAutoZeroMode=Once")
+
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai0",   0, "AITC", "$(DAQMX_OPTIONS)")      # Channel  0
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai1",   1, "AITC", "$(DAQMX_OPTIONS)")      # Channel  1
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai2",   2, "AITC", "$(DAQMX_OPTIONS)")      # Channel  2
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai3",   3, "AITC", "$(DAQMX_OPTIONS)")      # Channel  3
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai4",   4, "AITC", "$(DAQMX_OPTIONS)")      # Channel  4
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai5",   5, "AITC", "$(DAQMX_OPTIONS)")      # Channel  5
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai6",   6, "AITC", "$(DAQMX_OPTIONS)")      # Channel  6
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai7",   7, "AITC", "$(DAQMX_OPTIONS)")      # Channel  7
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai8",   8, "AITC", "$(DAQMX_OPTIONS)")      # Channel  8
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai9",   9, "AITC", "$(DAQMX_OPTIONS)")      # Channel  9
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai10", 10, "AITC", "$(DAQMX_OPTIONS)")      # Channel 10
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai11", 11, "AITC", "$(DAQMX_OPTIONS)")      # Channel 11
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai12", 12, "AITC", "$(DAQMX_OPTIONS)")      # Channel 12
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai13", 13, "AITC", "$(DAQMX_OPTIONS)")      # Channel 13
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai14", 14, "AITC", "$(DAQMX_OPTIONS)")      # Channel 14
+$(IFNOTRECSIM) DAQmxConfig("$(DEVICE)", "$(CDAQ)/ai15", 15, "AITC", "$(DAQMX_OPTIONS)")      # Channel 15
