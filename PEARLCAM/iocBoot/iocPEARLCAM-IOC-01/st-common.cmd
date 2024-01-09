dc2e3963a27f16168cfaea3efc18d2a3bcbad94e
Jack Harper <jack.harper@stfc.ac.uk>
2024-01-09 15:41:07 +0000

    correct some pv names for pearlcam dbpfs

--- PEARLCAM/iocBoot/iocPEARLCAM-IOC-01/st-common.cmd
+++ PEARLCAM/iocBoot/iocPEARLCAM-IOC-01/st-common.cmd
@@ -92,15 +92,15 @@ dbpf "$(PREFIX)CAM1:GC_Height", "4000"
 dbpf "$(PREFIX)CAM1:GC_Width", "5320"
 
 # Bin so we don't accidentally saturate the network with 24mp of raw mosaiced pixel data...
-epicsEnvSet("BINNINGFACTOR", "2")
+epicsEnvSet("BINNINGFACTOR", "3")
 dbpf "$(PREFIX)CAM1:GC_BinningVertical", $(BINNINGFACTOR)
 dbpf "$(PREFIX)CAM1:GC_BinningHorizontal", $(BINNINGFACTOR)
 
 # Bin with Sum - we may need to tweak this. It seems to up exposure, presumably because it's just adding the binned pixels together
-dbpf "$(PREFIX)CAM1:GC_BinningVerticalMode", "0"
-dbpf "$(PREFIX)CAM1:GC_BinningHorizontalMode", "0"
+dbpf "$(PREFIX)CAM1:GC_BinVerticalMode", "0"
+dbpf "$(PREFIX)CAM1:GC_BinHorizontalMode", "0"
 
 # Enable constant frame rate mode, then acquire 2hz 
 dbpf "$(PREFIX)CAM1:GC_AcqFraRateEnable", "1"
-dbpf "$(PREFIX)CAM1:GC_AcqFramerate", "2"
+dbpf "$(PREFIX)CAM1:GC_Framerate", "2"
 
