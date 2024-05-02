0ddf7e5975b4b6bed79c2229ebd4a088835c9d89
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2024-05-02 16:18:57 +0100

    Update for new muon naming

--- NCINDG/iocBoot/iocNCINDG-IOC-01/postiocinit-dig.cmd
+++ NCINDG/iocBoot/iocNCINDG-IOC-01/postiocinit-dig.cmd
@@ -6,4 +6,4 @@ stringiftest("DIG0", "$(DIG=)", 5, "0")
 
 $(IFDIG0) iocshLoad "hdf5-files.cmd" "DIG=$(DIG),ADET=4,FTEMPLATE=%s%s_DC.h5"
 $(IFDIG0) iocshLoad "hdf5-files.cmd" "DIG=$(DIG),ADET=5,FTEMPLATE=%s%s_TRACE.h5"
-$(IFDIG0) iocshLoad "hdf5-files.cmd" "DIG=$(DIG),ADET=6,FTEMPLATE=%s%s.nxs_v2ext"
+$(IFDIG0) iocshLoad "hdf5-files.cmd" "DIG=$(DIG),ADET=6,FTEMPLATE=%s%s.nxsext"
