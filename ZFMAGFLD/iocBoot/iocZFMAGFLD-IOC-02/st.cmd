b7249f8795eaf99ac4f28b367edb30c71c70b372
LilithCole <lilith.cole@stfc.ac.uk>
2024-05-31 14:59:30 +0100

    add second ioc to zfmagfld, add macro for forcing scans

--- ZFMAGFLD/iocBoot/iocZFMAGFLD-IOC-02/st.cmd
+++ ZFMAGFLD/iocBoot/iocZFMAGFLD-IOC-02/st.cmd
@@ -1,6 +1,6 @@
-#!../../bin/windows-x64/ZFMAGFLD-IOC-01
+#!../../bin/windows-x64/ZFMAGFLD-IOC-02
 
-## You may have to change ZFMAGFLD-IOC-01 to something else
+## You may have to change ZFMAGFLD-IOC-02 to something else
 ## everywhere it appears in this file
 
 # Increase this if you get <<TRUNCATED>> or discarded messages warnings in your errlog output
@@ -13,8 +13,8 @@ epicsEnvSet "DEVICE" "L0"
 cd "${TOP}"
 
 ## Register all support components
-dbLoadDatabase "dbd/ZFMAGFLD-IOC-01.dbd"
-ZFMAGFLD_IOC_01_registerRecordDeviceDriver pdbbase
+dbLoadDatabase "dbd/ZFMAGFLD-IOC-02.dbd"
+ZFMAGFLD_IOC_02_registerRecordDeviceDriver pdbbase
 
 < ${TOP}/iocBoot/iocZFMAGFLD-IOC-01/st-common.cmd
 
