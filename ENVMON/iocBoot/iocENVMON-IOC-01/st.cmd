f2c145355690f243eb4030b540868c8f04b9f456
Jack Doughty <jackb.doughty@btinternet.com>
2024-07-12 15:36:08 +0100

    Setup IOC structure

--- ENVMON/iocBoot/iocENVMON-IOC-01/st.cmd
+++ ENVMON/iocBoot/iocENVMON-IOC-01/st.cmd
@@ -1,18 +1,13 @@
 #!../../bin/windows-x64/ENVMON-IOC-01
 
-#- You may have to change ENVMON-IOC-01 to something else
-#- everywhere it appears in this file
+## You may have to change ENVMON to something else
+## everywhere it appears in this file
 
-#< envPaths
+< envPaths
 
 ## Register all support components
-dbLoadDatabase "../../dbd/ENVMON-IOC-01.dbd"
-ENVMON_IOC_01_registerRecordDeviceDriver(pdbbase) 
+dbLoadDatabase "${TOP}/dbd/ENVMON-IOC-01.dbd"
+ENVMON_IOC_01_registerRecordDeviceDriver pdbbase
 
-## Load record instances
-#dbLoadRecords("../../db/ENVMON-IOC-01.db","user=nxq64494")
-
-iocInit()
-
-## Start any sequence programs
-#seq sncENVMON-IOC-01,"user=nxq64494"
+## calling common command file in ioc 01 boot dir
+< ${TOP}/iocBoot/iocENVMON-IOC-01/st-common.cmd
--- ENVMON/iocBoot/iocENVMON-IOC-02/st.cmd
+++ ENVMON/iocBoot/iocENVMON-IOC-02/st.cmd
@@ -1,18 +1,13 @@
 #!../../bin/windows-x64/ENVMON-IOC-02
 
-#- You may have to change ENVMON-IOC-02 to something else
-#- everywhere it appears in this file
+## You may have to change ENVMON to something else
+## everywhere it appears in this file
 
-#< envPaths
+< envPaths
 
 ## Register all support components
-dbLoadDatabase "../../dbd/ENVMON-IOC-02.dbd"
-ENVMON_IOC_02_registerRecordDeviceDriver(pdbbase) 
+dbLoadDatabase "${TOP}/dbd/ENVMON-IOC-02.dbd"
+ENVMON_IOC_02_registerRecordDeviceDriver pdbbase
 
-## Load record instances
-#dbLoadRecords("../../db/ENVMON-IOC-02.db","user=nxq64494")
-
-iocInit()
-
-## Start any sequence programs
-#seq sncENVMON-IOC-02,"user=nxq64494"
+## calling common command file in ioc 01 boot dir
+< ${TOP}/iocBoot/iocENVMON-IOC-01/st-common.cmd
