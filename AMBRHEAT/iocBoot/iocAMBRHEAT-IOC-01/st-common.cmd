b8c370eade8cd16fdd2f32b4824f4028898f35fe
DavidKeymer <david.keymer@stfc.ac.uk>
2025-07-03 17:26:03 +0100

    Add comment for tracemask commands

--- AMBRHEAT/iocBoot/iocAMBRHEAT-IOC-01/st-common.cmd
+++ AMBRHEAT/iocBoot/iocAMBRHEAT-IOC-01/st-common.cmd
@@ -43,5 +43,6 @@ iocInit
 ##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
 < $(IOCSTARTUP)/postiocinit.cmd
 
+## display all traffic to and from IOC:
 # asynSetTraceMask("L0", -1, 0x9)
 # asynSetTraceIOMask("L0", -1, 0x2)
