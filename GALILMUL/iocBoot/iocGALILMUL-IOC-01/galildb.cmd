58a2f0e311a4b704be5224c6d2f0051a643aa606
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2026-08-10 13:19:01 +0100

    Fix motor port in sim mode

--- GALILMUL/iocBoot/iocGALILMUL-IOC-01/galildb.cmd
+++ GALILMUL/iocBoot/iocGALILMUL-IOC-01/galildb.cmd
@@ -6,17 +6,20 @@
 dbLoadRecords("$(GALILIOC)/db/galil_motor.db", "PORT=$(GALIL_MTR_PORT),P=$(MYPVPREFIX)MOT,CCP=$(MTRCTRL)")
 
 #Load extra functionality, untop of motorRecord features for axis/motors (eg. runtime gear ratio changes between master & slaves)
-dbLoadRecords("$(GALILIOC)/db/galil_ctrl_extras.db", "P=$(MYPVPREFIX)MOT,CCP=$(MTRCTRL),PORT=$(GALIL_MTR_PORT)")
-dbLoadRecords("$(GALILIOC)/db/galil_motor_extras.db", "P=$(MYPVPREFIX)MOT,CCP=$(MTRCTRL),PVPREFIX=$(MYPVPREFIX),ASG01=$(ASG01=DEFAULT),ASG02=$(ASG02=DEFAULT),ASG03=$(ASG03=DEFAULT),ASG04=$(ASG04=DEFAULT),ASG05=$(ASG05=DEFAULT),ASG06=$(ASG06=DEFAULT),ASG07=$(ASG07=DEFAULT),ASG08=$(ASG08=DEFAULT),ENCAVNSAMP=$(ENCAVNSAMP=10),PORT=$(GALIL_MTR_PORT),IOCNAME=$(IOCNAME)")
+dbLoadRecords("$(GALILIOC)/db/galil_ctrl_extras.db", "P=$(MYPVPREFIX)MOT,CCP=$(MTRCTRL),PORT=$(GALIL_PORT)")
+dbLoadRecords("$(GALILIOC)/db/galil_motor_extras.db", "P=$(MYPVPREFIX)MOT,CCP=$(MTRCTRL),PVPREFIX=$(MYPVPREFIX),ASG01=$(ASG01=DEFAULT),ASG02=$(ASG02=DEFAULT),ASG03=$(ASG03=DEFAULT),ASG04=$(ASG04=DEFAULT),ASG05=$(ASG05=DEFAULT),ASG06=$(ASG06=DEFAULT),ASG07=$(ASG07=DEFAULT),ASG08=$(ASG08=DEFAULT),ENCAVNSAMP=$(ENCAVNSAMP=10),PORT=$(GALIL_PORT),IOCNAME=$(IOCNAME)")
 
 #Load digital IO databases
-dbLoadRecords("$(GALILIOC)/db/galil_digital_ports.db", "P=$(MYPVPREFIX)MOT,CCP=$(MTRCTRL),PORT=$(GALIL_MTR_PORT)")
+dbLoadRecords("$(GALILIOC)/db/galil_digital_ports.db", "P=$(MYPVPREFIX)MOT,CCP=$(MTRCTRL),PORT=$(GALIL_PORT),RECSIM=$(RECSIM),IFNOTSIM=$(IFNOTSIM)")
 
 #Load analog IO databases
-dbLoadRecords("$(GALILIOC)/db/galil_analog_ports.db", "P=$(MYPVPREFIX)MOT,CCP=$(MTRCTRL),PORT=$(GALIL_MTR_PORT)")
+dbLoadRecords("$(GALILIOC)/db/galil_analog_ports.db", "P=$(MYPVPREFIX)MOT,CCP=$(MTRCTRL),PORT=$(GALIL_PORT)")
 
 #Load user defined functions
-dbLoadRecords("$(GALILIOC)/db/galil_userdef_records8.db", "P=$(MYPVPREFIX)MOT,CCP=$(MTRCTRL),PORT=$(GALIL_MTR_PORT)")
+dbLoadRecords("$(GALILIOC)/db/galil_userdef_records8.db", "P=$(MYPVPREFIX)MOT,CCP=$(MTRCTRL),PORT=$(GALIL_PORT)")
 
-dbLoadRecords("$(GALILIOC)/db/galil_profileMoveController.db", "P=$(MYPVPREFIX)MOT:,CCP=$(MTRCTRL),PORT=$(GALIL_MTR_PORT)")
-dbLoadRecords("$(GALILIOC)/db/galil_profileMoveAxis.db", "P=$(MYPVPREFIX)MOT:,CCP=$(MTRCTRL),PORT=$(GALIL_MTR_PORT)")
+dbLoadRecords("$(GALILIOC)/db/galil_profileMoveController.db", "P=$(MYPVPREFIX)MOT:,CCP=$(MTRCTRL),PORT=$(GALIL_PORT)")
+dbLoadRecords("$(GALILIOC)/db/galil_profileMoveAxis.db", "P=$(MYPVPREFIX)MOT:,CCP=$(MTRCTRL),PORT=$(GALIL_PORT)")
+
+#Load homing routine records 
+dbLoadRecords("$(GALILIOC)/db/galil_homing_routines.db", "P=$(MYPVPREFIX)MOT,CCP=$(MTRCTRL),PORT=$(GALIL_PORT)")
